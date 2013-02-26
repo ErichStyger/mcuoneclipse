bc0f3295350681f7fd88ac9209d0e957a5fa5129
Erich Styger <erich.styger@hslu.ch>
2013-02-26 19:53:19 +0100

    using FreeRTOS heap management instead of library one, as otherwise running out of memory

--- Drivers/FSL_USB_Stack/Host/source/driver/mem_util.c
+++ Drivers/FSL_USB_Stack/Host/source/driver/mem_util.c
@@ -2,11 +2,14 @@
 #include "psptypes.h"
 #include "mem_util.h"
 
+#include "freeRTOS.h" /* << EST: FreeRTOS malloc */
+
 #define  USB_MEM4_ALIGN(n)          ((n) + (-(n) & 3))
 
 void* USB_mem_alloc_word_aligned(uint_32 size)
 {
-    void *p = malloc(size + 4);
+    //void *p = malloc(size + 4);
+    void *p = pvPortMalloc(size + 4); /* << EST */
     return((void*)(USB_MEM4_ALIGN((uint_32)p)));
 }
 
