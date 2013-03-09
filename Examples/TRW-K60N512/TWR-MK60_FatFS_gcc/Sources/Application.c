4f424c8ad8017947d963f427468296c2d53ea575
Erich Styger <erich.styger@hslu.ch>
2013-03-09 15:23:11 +0100

    updated project to use new shell and FatFs built-in benchmark command.

--- Examples/TRW-K60N512/TWR-MK60_FatFS_gcc/Sources/Application.c
+++ Examples/TRW-K60N512/TWR-MK60_FatFS_gcc/Sources/Application.c
@@ -2,7 +2,6 @@
  * Application.c
  *      Author: Erich Styger
  */
-
 #include "Application.h"
 #include "LED1.h"
 #include "LED2.h"
@@ -10,50 +9,20 @@
 #include "LED4.h"
 #include "FRTOS1.h"
 #include "Shell.h"
-#include "WAIT1.h"
 
 static portTASK_FUNCTION(Task1, pvParameters) {
   (void)pvParameters; /* parameter not used */
   for(;;) {
     LED1_Neg();
-    FRTOS1_vTaskDelay(100/portTICK_RATE_MS);
-  }
-}
-
-static portTASK_FUNCTION(Task2, pvParameters) {
-  (void)pvParameters; /* parameter not used */
-  for(;;) {
-    LED2_Neg();
-    FRTOS1_vTaskDelay(200/portTICK_RATE_MS);
-  }
-}
-
-static portTASK_FUNCTION(Task3, pvParameters) {
-  (void)pvParameters; /* parameter not used */
-  for(;;) {
-    LED3_Neg();
-    FRTOS1_vTaskDelay(400/portTICK_RATE_MS);
-  }
-}
-
-static portTASK_FUNCTION(Task4, pvParameters) {
-  (void)pvParameters; /* parameter not used */
-  for(;;) {
-    LED4_Neg();
-    FRTOS1_vTaskDelay(800/portTICK_RATE_MS);
+    FRTOS1_vTaskDelay(1000/portTICK_RATE_MS);
   }
 }
 
 void APP_Run(void) {
-  uint16_t i;
-
-  for(i=0;i<16;i++) {
-    LED1_Neg();
-    LED2_Neg();
-    LED3_Neg();
-    LED4_Neg();
-    WAIT1_Waitms(50);
-  }
+  LED1_Neg();
+  LED2_Neg();
+  LED3_Neg();
+  LED4_Neg();
   SHELL_Init();
   if (FRTOS1_xTaskCreate(
         Task1,  /* pointer to the task */
@@ -67,41 +36,6 @@ void APP_Run(void) {
     for(;;){}; /* error! probably out of memory */
     /*lint +e527 */
   }
-  if (FRTOS1_xTaskCreate(
-        Task2,  /* pointer to the task */
-        (signed portCHAR *)"Task2", /* task name for kernel awareness debugging */
-        configMINIMAL_STACK_SIZE, /* task stack size */
-        (void*)NULL, /* optional task startup argument */
-        tskIDLE_PRIORITY,  /* initial priority */
-        (xTaskHandle*)NULL /* optional task handle to create */
-      ) != pdPASS) {
-    /*lint -e527 */
-    for(;;){}; /* error! probably out of memory */
-    /*lint +e527 */
-  }
-  if (FRTOS1_xTaskCreate(
-        Task3,  /* pointer to the task */
-        (signed portCHAR *)"Task3", /* task name for kernel awareness debugging */
-        configMINIMAL_STACK_SIZE, /* task stack size */
-        (void*)NULL, /* optional task startup argument */
-        tskIDLE_PRIORITY,  /* initial priority */
-        (xTaskHandle*)NULL /* optional task handle to create */
-      ) != pdPASS) {
-    /*lint -e527 */
-    for(;;){}; /* error! probably out of memory */
-    /*lint +e527 */
-  }
-  if (FRTOS1_xTaskCreate(
-        Task4,  /* pointer to the task */
-        (signed portCHAR *)"Task4", /* task name for kernel awareness debugging */
-        configMINIMAL_STACK_SIZE, /* task stack size */
-        (void*)NULL, /* optional task startup argument */
-        tskIDLE_PRIORITY,  /* initial priority */
-        (xTaskHandle*)NULL /* optional task handle to create */
-      ) != pdPASS) {
-    /*lint -e527 */
-    for(;;){}; /* error! probably out of memory */
-    /*lint +e527 */
-  }
   FRTOS1_vTaskStartScheduler();
 }
+
