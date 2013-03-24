4d59b2177a8a0d3242d34001fbaecffc42abb330
Erich Styger <erich.styger@steinerberg.com>
2013-03-24 19:55:20 +0100

    Added FreeRTOS demo project for 52259

---
+++ Examples/TWR-52259/MCF52259_FreeRTOS/Sources/Events.c
@@ -0,0 +1,158 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : ProcessorExpert
+**     Processor   : MCF52259CAG80
+**     Component   : Events
+**     Compiler    : CodeWarrior MCF C Compiler
+**     Date/Time   : 2013-03-24, 19:43, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnCoreWatchdogINT - void Cpu_OnCoreWatchdogINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @date 2013-03-24, 19:43, # CodeGen: 0
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnCoreWatchdogINT (module Events)
+**
+**     Component   :  Cpu [MCF52259_144_LQFP]
+**     Description :
+**         This event is called when the OnCoreWatchdog interrupt had
+**         occurred. This event is automatically enabled when the <Mode>
+**         is set to 'Interrupt'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnCoreWatchdogINT(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationStackOverflowHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         if enabled, this hook will be called in case of a stack
+**         overflow.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**       * pxTask          - Pointer to task handle
+**       * pcTaskName      - Pointer to task name
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vApplicationStackOverflowHook(xTaskHandle *pxTask, signed portCHAR *pcTaskName)
+{
+  /* This will get called if a stack overflow is detected during the context
+     switch.  Set configCHECK_FOR_STACK_OVERFLOWS to 2 to also check for stack
+     problems within nested interrupts, but only do this for debug purposes as
+     it will increase the context switch time. */
+  (void)pxTask;
+  (void)pcTaskName;
+  taskDISABLE_INTERRUPTS();
+  /* Write your code here ... */
+  for(;;) {}
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationTickHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called by the RTOS for every
+**         tick increment.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vApplicationTickHook(void)
+{
+  /* Called for every RTOS tick. */
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationIdleHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called when the RTOS is idle.
+**         This might be a good place to go into low power mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vApplicationIdleHook(void)
+{
+  /* Called whenever the RTOS is idle (from the IDLE task).
+     Here would be a good place to put the CPU into low power mode. */
+#if 0
+  /* To put the ColdFire V2 CPU into low power WAIT mode:
+     - In the CPU component, go to 'Properties > Internal peripherals > PowerManagement > Low-power mode'
+       - Set 'Low-power stop mode' to 'Enabled'
+       - Set 'Low-power mode select' to 'WAIT'
+     - Enable this code block (change '#if 0' to '#if 1')
+     - Note: if you are using USB then putting the CPU into lower power mode might impact USB communication.
+   */
+    __asm("stop #0x2000"); /* put CPU into stop mode */
+#endif
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnCoreWatchdogINT0 (module Events)
+**
+**     Component   :  Cpu [MCF52259_144_LQFP]
+**     Description :
+**         This event is called when the OnCoreWatchdog interrupt had
+**         occurred. This event is automatically enabled when the <Mode>
+**         is set to 'Interrupt'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnCoreWatchdogINT0(void)
+{
+  /* Write your code here ... */
+}
+
+
+/* END Events */
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.0.12 [05.05]
+**     for the Freescale MCF series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/TWR-52259/MCF52259_FreeRTOS/Sources/Events.h
@@ -0,0 +1,138 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MCF52259CAG80
+**     Component   : Events
+**     Compiler    : CodeWarrior MCF C Compiler
+**     Date/Time   : 2013-03-24, 19:43, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnCoreWatchdogINT - void Cpu_OnCoreWatchdogINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @date 2013-03-24, 19:43, # CodeGen: 0
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "FRTOS1.h"
+#include "RTOSSWI1.h"
+#include "TickCntr1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "LED3.h"
+#include "LEDpin3.h"
+#include "LED4.h"
+#include "LEDpin4.h"
+#include "UTIL1.h"
+
+void Cpu_OnCoreWatchdogINT(void);
+/*
+** ===================================================================
+**     Event       :  Cpu_OnCoreWatchdogINT (module Events)
+**
+**     Component   :  Cpu [MCF52259_144_LQFP]
+**     Description :
+**         This event is called when the OnCoreWatchdog interrupt had
+**         occurred. This event is automatically enabled when the <Mode>
+**         is set to 'Interrupt'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+
+void FRTOS1_vApplicationStackOverflowHook(xTaskHandle *pxTask, signed portCHAR *pcTaskName);
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationStackOverflowHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         if enabled, this hook will be called in case of a stack
+**         overflow.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**       * pxTask          - Pointer to task handle
+**       * pcTaskName      - Pointer to task name
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void FRTOS1_vApplicationTickHook(void);
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationTickHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called by the RTOS for every
+**         tick increment.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void FRTOS1_vApplicationIdleHook(void);
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationIdleHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called when the RTOS is idle.
+**         This might be a good place to go into low power mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void Cpu_OnCoreWatchdogINT0(void);
+/*
+** ===================================================================
+**     Event       :  Cpu_OnCoreWatchdogINT0 (module Events)
+**
+**     Component   :  Cpu [MCF52259_144_LQFP]
+**     Description :
+**         This event is called when the OnCoreWatchdog interrupt had
+**         occurred. This event is automatically enabled when the <Mode>
+**         is set to 'Interrupt'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.0.12 [05.05]
+**     for the Freescale MCF series of microcontrollers.
+**
+** ###################################################################
+*/
