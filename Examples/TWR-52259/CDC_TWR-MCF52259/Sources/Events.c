5ebec5b391511906de77434a3efe6b7f2780afd3
Erich Styger <erich.styger@steinerberg.com>
2013-03-29 14:28:38 +0100

    CDC project for MCF52259

---
+++ Examples/TWR-52259/CDC_TWR-MCF52259/Sources/Events.c
@@ -0,0 +1,50 @@
+/** ###################################################################
+**     Filename    : Events.c
+**     Project     : ProcessorExpert
+**     Processor   : MCF52259CAG80
+**     Component   : Events
+**     Compiler    : CodeWarrior MCF C Compiler
+**     Date/Time   : 2012-05-31, 11:24, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnCoreWatchdogINT - void Cpu_OnCoreWatchdogINT(void);
+**
+** ###################################################################*/
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnCoreWatchdogINT (module Events)
+**
+**     Component   :  Cpu [MCF52259_144_LQFP]
+**     Description :
+**         This event is called when the OnCoreWatchdog interrupt had
+**         occurred. This event is automatically enabled when the <Mode>
+**         is set to 'Interrupt'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnCoreWatchdogINT(void)
+{
+  /* Write your code here ... */
+}
+
+/* END Events */
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 5.3 [05.01]
+**     for the Freescale MCF series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/TWR-52259/CDC_TWR-MCF52259/Sources/ProcessorExpert.c
@@ -0,0 +1,100 @@
+/** ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MCF52259CAG80
+**     Version     : Driver 01.00
+**     Compiler    : CodeWarrior MCF C Compiler
+**     Date/Time   : 2012-05-31, 11:24, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "USB1.h"
+#include "USBInit1.h"
+#include "CDC1.h"
+#include "Tx1.h"
+#include "Rx1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "LED3.h"
+#include "LEDpin3.h"
+#include "LED4.h"
+#include "LEDpin4.h"
+#include "WAIT1.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+static uint8_t cdc_buffer[USB1_DATA_BUFF_SIZE];
+static uint8_t in_buffer[USB1_DATA_BUFF_SIZE];
+
+static void CDC_Run(void) {
+  int i;
+
+  for(;;) {
+    while(CDC1_App_Task(cdc_buffer, sizeof(cdc_buffer))==ERR_BUSOFF) {
+      /* device not enumerated */
+      LED1_Neg(); LED2_Off();
+//      WAIT1_Waitms(10);
+    }
+    LED1_Off(); LED2_Neg();
+    if (CDC1_GetCharsInRxBuf()!=0) {
+      i = 0;
+      while(   i<sizeof(in_buffer)-1
+            && CDC1_GetChar(&in_buffer[i])==ERR_OK
+           )
+      {
+        i++;
+      }
+      in_buffer[i] = '\0';
+      (void)CDC1_SendString((unsigned char*)"echo: ");
+      (void)CDC1_SendString(in_buffer);
+      (void)CDC1_SendString((unsigned char*)"\r\n");
+    } else {
+//      WAIT1_Waitms(10);
+    }
+  }
+}
+
+void main(void)
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  /* For example: for(;;) { } */
+  CDC_Run();
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END ProcessorExpert */
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 5.3 [05.01]
+**     for the Freescale MCF series of microcontrollers.
+**
+** ###################################################################
+*/
