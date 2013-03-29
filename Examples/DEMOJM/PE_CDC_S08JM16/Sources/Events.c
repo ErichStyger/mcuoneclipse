3258b52ed0835c46710c957bea964078362ee341
Erich Styger <erich.styger@steinerberg.com>
2013-03-29 14:12:45 +0100

    CDC project for JM16

---
+++ Examples/DEMOJM/PE_CDC_S08JM16/Sources/Events.c
@@ -0,0 +1,34 @@
+/** ###################################################################
+**     Filename    : Events.c
+**     Project     : ProcessorExpert
+**     Processor   : MC9S08JM16CGT
+**     Component   : Events
+**     Version     : Driver 01.02
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2012-03-14, 18:20, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/* MODULE Events */
+
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/* END Events */
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 5.3 [05.01]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/DEMOJM/PE_CDC_S08JM16/Sources/Events.h
@@ -0,0 +1,48 @@
+/** ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MC9S08JM16CGT
+**     Component   : Events
+**     Version     : Driver 01.02
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2012-03-14, 18:20, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "USB1.h"
+#include "USBInit1.h"
+#include "CDC1.h"
+#include "Tx1.h"
+#include "Rx1.h"
+#include "WAIT1.h"
+
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 5.3 [05.01]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
