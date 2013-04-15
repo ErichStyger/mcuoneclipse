f2c2c1f2cab5d6b8271f9e9baea198a4cac8293e
Erich Styger <erich.styger@steinerberg.com>
2013-04-15 14:24:42 +0200

    Initial project

---
+++ Examples/FRDM-K20D50M/FRDM-K20_FreeRTOS/Sources/Events.c
@@ -0,0 +1,51 @@
+/** ###################################################################
+**     Filename    : Events.c
+**     Project     : ProcessorExpert
+**     Processor   : MK20DX128VLH5
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2013-04-14, 18:49, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT0 - void Cpu_OnNMIINT0(void);
+**
+** ###################################################################*/
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT0 (module Events)
+**
+**     Component   :  Cpu [MK20DX128EX5]
+**     Description :
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the <NMI
+**         interrrupt> property is set to 'Enabled'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnNMIINT0(void)
+{
+  /* Write your code here ... */
+}
+
+/* END Events */
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.0 [05.03]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/FRDM-K20D50M/FRDM-K20_FreeRTOS/Sources/Events.h
@@ -0,0 +1,54 @@
+/** ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MK20DX128VLH5
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2013-04-14, 18:49, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT0 - void Cpu_OnNMIINT0(void);
+**
+** ###################################################################*/
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "PE_LDD.h"
+
+void Cpu_OnNMIINT0(void);
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT0 (module Events)
+**
+**     Component   :  Cpu [MK20DX128EX5]
+**     Description :
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the <NMI
+**         interrrupt> property is set to 'Enabled'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.0 [05.03]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
