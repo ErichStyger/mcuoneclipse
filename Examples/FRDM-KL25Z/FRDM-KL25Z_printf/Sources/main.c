e76c209f878068b53decf0b87d7eb4ec0ab23b01
Erich Styger <erich.styger@steinerberg.com>
2013-04-18 15:11:00 +0200

    added blinking RED led

--- Examples/FRDM-KL25Z/FRDM-KL25Z_printf/Sources/main.c
+++ Examples/FRDM-KL25Z/FRDM-KL25Z_printf/Sources/main.c
@@ -7,14 +7,43 @@
 #include <stdio.h>
 #include "ConsoleIO.h"
 
+#define RED          (18)
+#define RED_SHIFT   (1 << 18)
+
+#define RED_OFF     (GPIOB_PSOR = RED_SHIFT)
+#define RED_ON      (GPIOB_PCOR = RED_SHIFT)
+#define RED_TOGGLE  (GPIOB_PTOR = RED_SHIFT)
+
+static void InitLED(void) {
+  /* Turn on clock to PortB module */
+  SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+
+  /* Set the PTB18 pin multiplexer to GPIO mode */
+  PORTB_PCR18 = PORT_PCR_MUX(1);
+
+  /* Set the initial output state to high */
+  GPIOB_PSOR |= RED_SHIFT;
+
+  /* Set the pins direction to output */
+  GPIOB_PDDR |= RED_SHIFT;
+}
+
+static void NegLED(void) {
+  RED_TOGGLE;
+}
+
 int main(void)
 {
 	int counter = 0;
 	
 	ConsoleIO_Init();
+	InitLED();
 	for(;;) {	   
-	   	counter++;
-	   	printf("Hello world!\r\n");
+	  counter++;
+	  printf("Hello world!\r\n");
+	  if ((counter%32)==0) {
+      NegLED();
+	  }
 	}
 	
 	return 0;
