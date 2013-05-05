b325357471faa736714d8c392c0b6ac5060cec81
Erich Styger <erich.styger@steinerberg.com>
2013-05-05 15:58:46 +0200

    Initial version of MQX USB CDC project

---
+++ Examples/FRDM-KL25Z/Freedom_CDC_MQXLite/Sources/mqx_tasks.c
@@ -0,0 +1,84 @@
+/* ###################################################################
+**     Filename    : mqx_tasks.c
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2013-05-05, 15:42, # CodeGen: 3
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Task1_task - void Task1_task(uint32_t task_init_data);
+**
+** ###################################################################*/
+/*!
+** @file mqx_tasks.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup mqx_tasks_module mqx_tasks module documentation
+**  @{
+*/         
+/* MODULE mqx_tasks */
+
+#include "Cpu.h"
+#include "Events.h"
+#include "mqx_tasks.h"
+#include "LEDR.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Task1_task (module mqx_tasks)
+**
+**     Component   :  Task1 [MQXLite_task]
+**     Description :
+**         MQX task routine. The routine is generated into mqx_tasks.c
+**         file.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         task_init_data  - 
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Task1_task(uint32_t task_init_data)
+{
+  int counter = 0;
+
+  while(1) {
+    counter++;
+
+    LEDR_Neg();
+    _time_delay_ticks(100);
+  }
+}
+
+/* END mqx_tasks */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.2 [05.06]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/FRDM-KL25Z/Freedom_CDC_MQXLite/Sources/mqx_tasks.h
@@ -0,0 +1,106 @@
+/* ###################################################################
+**     Filename    : mqx_tasks.h
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2013-05-05, 15:42, # CodeGen: 3
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Task1_task - void Task1_task(uint32_t task_init_data);
+**
+** ###################################################################*/
+/*!
+** @file mqx_tasks.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup mqx_tasks_module mqx_tasks module documentation
+**  @{
+*/         
+
+#ifndef __mqx_tasks_H
+#define __mqx_tasks_H
+/* MODULE mqx_tasks */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "LEDR.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LEDG.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LEDB.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "MQX1.h"
+#include "SystemTimer1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+
+void Task1_task(uint32_t task_init_data);
+/*
+** ===================================================================
+**     Event       :  Task1_task (module mqx_tasks)
+**
+**     Component   :  Task1 [MQXLite_task]
+**     Description :
+**         MQX task routine. The routine is generated into mqx_tasks.c
+**         file.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         task_init_data  - 
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void Task1_task(uint32_t task_init_data);
+/*
+** ===================================================================
+**     Event       :  Task1_task (module mqx_tasks)
+**
+**     Component   :  Task1 [MQXLite_task]
+**     Description :
+**         MQX task routine. The routine is generated into mqx_tasks.c
+**         file.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         task_init_data  - 
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+
+/* END mqx_tasks */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __mqx_tasks_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.2 [05.06]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
