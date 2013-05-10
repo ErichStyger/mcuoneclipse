8d08d395e76b60bdca7766530a2511117fbf2487
Erich Styger <erich.styger@steinerberg.com>
2013-05-10 17:01:14 +0200

    initial IAR + FreeRTOS project

---
+++ Examples/FRDM-KL25Z/IAR_Freedom_FreeRTOS/Sources/ProcessorExpert.c
@@ -0,0 +1,79 @@
+/** ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Version     : Driver 01.01
+**     Compiler    : IAR ARM C Compiler
+**     Date/Time   : 2013-01-31, 18:34, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "FRTOS1.h"
+#include "RTOSTICKLDD1.h"
+#include "UTIL1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+static portTASK_FUNCTION(MyTask, pvParameters) {
+  (void)pvParameters; /* not used */
+  for(;;) {
+    LED1_Neg();
+    FRTOS1_vTaskDelay(1000/portTICK_RATE_MS);
+  }
+}
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  /* For example: for(;;) { } */
+  if (FRTOS1_xTaskCreate(MyTask, (signed portCHAR *)"Task",
+		  configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL) != pdPASS) {
+    for(;;){} /* error */
+  }
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END ProcessorExpert */
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.0 [05.03]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
