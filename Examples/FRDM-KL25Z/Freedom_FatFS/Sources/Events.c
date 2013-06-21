9f7647c2feb12b33f006f76c005022bd9a1169fe
Erich Styger <erich.styger@steinerberg.com>
2013-06-21 10:44:32 +0200

    cleaned up FatFS project and removed unused events

--- Examples/FRDM-KL25Z/Freedom_FatFS/Sources/Events.c
+++ Examples/FRDM-KL25Z/Freedom_FatFS/Sources/Events.c
@@ -110,6 +110,44 @@ void FRTOS1_vApplicationTickHook(void)
   TmDt1_AddTick();
 }
 
+/*
+** ===================================================================
+**     Event       :  I2C3_OnReceiveData (module Events)
+**
+**     Component   :  I2C3 [InternalI2C]
+**     Description :
+**         This event is invoked when I2C finishes the reception of the
+**         data successfully. This event is not available for the SLAVE
+**         mode and if both RecvChar and RecvBlock are disabled. This
+**         event is enabled only if interrupts/events are enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void I2C3_OnReceiveData(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  I2C3_OnTransmitData (module Events)
+**
+**     Component   :  I2C3 [InternalI2C]
+**     Description :
+**         This event is invoked when I2C finishes the transmission of
+**         the data successfully. This event is not available for the
+**         SLAVE mode and if both SendChar and SendBlock are disabled.
+**         This event is enabled only if interrupts/events are enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void I2C3_OnTransmitData(void)
+{
+  /* Write your code here ... */
+}
+
 /* END Events */
 
 /*
