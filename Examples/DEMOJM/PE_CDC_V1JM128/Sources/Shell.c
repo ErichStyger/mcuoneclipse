626b22d8e654170a20d0caff1c652d3ca4622078
Erich Styger <erich.styger@steinerberg.com>
2013-06-29 09:32:49 +0200

    updated CDC projects for MCU10.4

--- Examples/DEMOJM/PE_CDC_V1JM128/Sources/Shell.c
+++ Examples/DEMOJM/PE_CDC_V1JM128/Sources/Shell.c
@@ -6,7 +6,7 @@
  * That way we can interact with the target and change settings using a shell implementation
  */
 #include "Shell.h"
-#include "FSSH1.h"
+#include "CLS1.h"
 #include "FRTOS1.h"
 #include <string.h> /* for strcmp() */
 #include "USB1.h"
@@ -16,18 +16,18 @@
  * \brief Prints the help text to the console
  * \param io I/O channel to be used
  */
-static void PrintHelp(const FSSH1_StdIOType *io) {
+static void PrintHelp(const CLS1_StdIOType *io) {
   /* list your local help here */
-  FSSH1_SendHelpStr((unsigned char*)"my help", (unsigned char*)"My help text\r\n", io->stdOut);
+  CLS1_SendHelpStr((unsigned char*)"my help", (unsigned char*)"My help text\r\n", io->stdOut);
 }
 
 /*!
  * \brief Prints the status text to the console
  * \param io I/O channel to be used
  */
-static void PrintStatus(const FSSH1_StdIOType *io) {
+static void PrintStatus(const CLS1_StdIOType *io) {
   /* list your local status here */
-  FSSH1_SendStatusStr((unsigned char*)"my status", (unsigned char*)"My status text\r\n", io->stdOut);
+  CLS1_SendStatusStr((unsigned char*)"my status", (unsigned char*)"My status text\r\n", io->stdOut);
 }
 
 /*!
@@ -37,18 +37,24 @@ static void PrintStatus(const FSSH1_StdIOType *io) {
  * \param io I/O stream to be used for input/output
  * \return Error code, ERR_OK if everything was fine
  */
-static uint8_t ParseCommand(const unsigned char *cmd, bool *handled, const FSSH1_StdIOType *io) {
+static uint8_t ParseCommand(const unsigned char *cmd, bool *handled, const CLS1_StdIOType *io) {
   /* handling our own commands */
-  if (strcmp((char*)cmd, (char*)FSSH1_CMD_HELP)==0) {
+  if (strcmp((char*)cmd, (char*)CLS1_CMD_HELP)==0) {
     PrintHelp(io);
     *handled = TRUE;
-  } else if (strcmp((char*)cmd, (char*)FSSH1_CMD_STATUS)==0) {
+  } else if (strcmp((char*)cmd, (char*)CLS1_CMD_STATUS)==0) {
     PrintStatus(io);
     *handled = TRUE;
   }
   /*! \todo Handle here the additional parsers */
   return ERR_OK;
 }
+
+static const CLS1_ParseCommandCallback CmdParserTable[] =
+{
+  ParseCommand,
+  NULL /* Sentinel */
+};
 
 static portTASK_FUNCTION(ShellTask, pvParameters) {
   static unsigned char cmd_buf[32];
@@ -66,13 +72,13 @@ static portTASK_FUNCTION(ShellTask, pvParameters) {
       }
     }
     LED1_Neg();
-    (void)FSSH1_ReadAndParseLine(cmd_buf, sizeof(cmd_buf), FSSH1_GetStdio(), ParseCommand);
+    (void)CLS1_ReadAndParseWithCommandTable(cmd_buf, sizeof(cmd_buf), CLS1_GetStdio(), CmdParserTable);
     FRTOS1_vTaskDelay(100/portTICK_RATE_MS);
   }
 }
 
 void SHELL_Init(void) {
-  FSSH1_Init(); /* create mutex/semaphore */
+  CLS1_Init(); /* create mutex/semaphore */
   if (FRTOS1_xTaskCreate(ShellTask, (signed portCHAR *)"Shell", configMINIMAL_STACK_SIZE+150, NULL, tskIDLE_PRIORITY, NULL) != pdPASS) {
     for(;;){} /* error */
   }
