3f170389fb237cd48cb7582164031b3100686764
Erich Styger <erich.styger@steinerberg.com>
2013-07-16 10:58:54 +0200

    updated C++ test project

--- Examples/FRDM-KL25Z/Freedom_Cpp/Sources/Events.c
+++ Examples/FRDM-KL25Z/Freedom_Cpp/Sources/Events.c
@@ -61,93 +61,107 @@ void SM1_OnBlockSent(LDD_TUserData *UserDataPtr)
   /* Write your code here ... */
 }
 
+
 /*
 ** ===================================================================
-**     Event       :  FRTOS1_vApplicationStackOverflowHook (module Events)
+**     Event       :  EVNT1_AppHandleEvent (module Events)
 **
-**     Component   :  FRTOS1 [FreeRTOS]
+**     Component   :  EVNT1 [SimpleEvents]
 **     Description :
-**         if enabled, this hook will be called in case of a stack
-**         overflow.
+**         
 **     Parameters  :
 **         NAME            - DESCRIPTION
-**       * pxTask          - Pointer to task handle
-**       * pcTaskName      - Pointer to task name
+**         event           - Event (event number) to be processed.
 **     Returns     : Nothing
 ** ===================================================================
 */
-void FRTOS1_vApplicationStackOverflowHook(xTaskHandle *pxTask, signed portCHAR *pcTaskName)
+void EVNT1_AppHandleEvent(byte event)
 {
-  /* This will get called if a stack overflow is detected during the context
-     switch.  Set configCHECK_FOR_STACK_OVERFLOWS to 2 to also check for stack
-     problems within nested interrupts, but only do this for debug purposes as
-     it will increase the context switch time. */
-  (void)pxTask;
-  (void)pcTaskName;
-  taskDISABLE_INTERRUPTS();
-  /* Write your code here ... */
-  for(;;) {}
-}
-
-/*
-** ===================================================================
-**     Event       :  FRTOS1_vApplicationTickHook (module Events)
-**
-**     Component   :  FRTOS1 [FreeRTOS]
-**     Description :
-**         If enabled, this hook will be called by the RTOS for every
-**         tick increment.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-void FRTOS1_vApplicationTickHook(void)
-{
-  /* Called for every RTOS tick. */
+  (void)event; /* only to avoid compiler warning about unused variable */
   /* Write your code here ... */
 }
 
 /*
 ** ===================================================================
-**     Event       :  FRTOS1_vApplicationIdleHook (module Events)
+**     Event       :  S191_OnSRecord (module Events)
 **
-**     Component   :  FRTOS1 [FreeRTOS]
+**     Component   :  S191 [S19]
 **     Description :
-**         If enabled, this hook will be called when the RTOS is idle.
-**         This might be a good place to go into low power mode.
-**     Parameters  : None
+**         Event called for every S-Record
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**       * returnCode      - Pointer to error code
+**         type            - type of S-Record (as character), e.g. '3'
+**         addr            - Address of data
+**       * dataBuf         - Pointer to buffer of S-Record data
+**                           (in binary form)
+**         dataBufSize     - Size in bytes of the data
+**                           buffer
+**       * data            - Pointer to private data
 **     Returns     : Nothing
 ** ===================================================================
 */
-void FRTOS1_vApplicationIdleHook(void)
+void S191_OnSRecord(byte *returnCode, byte type, dword addr, byte *dataBuf, byte dataBufSize, void *data)
 {
-  /* Called whenever the RTOS is idle (from the IDLE task).
-     Here would be a good place to put the CPU into low power mode. */
   /* Write your code here ... */
 }
 
 /*
 ** ===================================================================
-**     Event       :  FRTOS1_vApplicationMallocFailedHook (module Events)
+**     Event       :  S191_OnError (module Events)
 **
-**     Component   :  FRTOS1 [FreeRTOS]
+**     Component   :  S191 [S19]
 **     Description :
-**         If enabled, the RTOS will call this hook in case memory
-**         allocation failed.
+**         Event called in case of an error
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         errorCode       - 
+**       * iteratorInfo    - Pointer to parser info
+**                           struct
+**     Returns     : Nothing
+** ===================================================================
+*/
+void S191_OnError(S191_ErrorCode errorCode, S191_ParserStruct *iteratorInfo)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AD1_OnEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called after the measurement (which consists
+**         of <1 or more conversions>) is/are finished.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
 **     Parameters  : None
 **     Returns     : Nothing
 ** ===================================================================
 */
-void FRTOS1_vApplicationMallocFailedHook(void)
+void AD1_OnEnd(void)
 {
-  /* Called if a call to pvPortMalloc() fails because there is insufficient
-     free memory available in the FreeRTOS heap.  pvPortMalloc() is called
-     internally by FreeRTOS API functions that create tasks, queues, software
-     timers, and semaphores.  The size of the FreeRTOS heap is set by the
-     configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
-  taskDISABLE_INTERRUPTS();
   /* Write your code here ... */
-  for(;;) {}
+}
+
+/*
+** ===================================================================
+**     Event       :  AD1_OnCalibrationEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called when the calibration has been finished.
+**         User should check if the calibration pass or fail by
+**         Calibration status method./nThis event is enabled only if
+**         the <Interrupt service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AD1_OnCalibrationEnd(void)
+{
+  /* Write your code here ... */
 }
 
 /* END Events */
--- Examples/FRDM-KL25Z/Freedom_Cpp/Sources/Events.h
+++ Examples/FRDM-KL25Z/Freedom_Cpp/Sources/Events.h
@@ -29,8 +29,35 @@
 #include "TmDt1.h"
 #include "SD1.h"
 #include "SS1.h"
-#include "FRTOS1.h"
-#include "RTOSTICKLDD1.h"
+#include "SPI2.h"
+#include "Clock1.h"
+#include "BitIoLdd1.h"
+#include "Input1.h"
+#include "BitIoLdd2.h"
+#include "Output1.h"
+#include "BitIoLdd3.h"
+#include "I2C2.h"
+#include "SDA1.h"
+#include "BitIoLdd4.h"
+#include "SCL1.h"
+#include "BitIoLdd5.h"
+#include "TRG1.h"
+#include "EVNT1.h"
+#include "S191.h"
+#include "RNG1.h"
+#include "RNG2.h"
+#include "ACCEL1.h"
+#include "G11.h"
+#include "BitIoLdd6.h"
+#include "G21.h"
+#include "BitIoLdd7.h"
+#include "Sleep1.h"
+#include "BitIoLdd8.h"
+#include "AD1.h"
+#include "AdcLdd1.h"
+#include "MMA1.h"
+#include "GI2C1.h"
+#include "I2C3.h"
 #include "SM1.h"
 #include "TMOUT1.h"
 #include "PE_LDD.h"
@@ -70,60 +97,88 @@ void Cpu_OnNMIINT(void);
 /* ===================================================================*/
 void SM1_OnBlockSent(LDD_TUserData *UserDataPtr);
 
-void FRTOS1_vApplicationStackOverflowHook(xTaskHandle *pxTask, signed portCHAR *pcTaskName);
+
+void EVNT1_AppHandleEvent(byte event);
 /*
 ** ===================================================================
-**     Event       :  FRTOS1_vApplicationStackOverflowHook (module Events)
+**     Event       :  EVNT1_AppHandleEvent (module Events)
 **
-**     Component   :  FRTOS1 [FreeRTOS]
+**     Component   :  EVNT1 [SimpleEvents]
 **     Description :
-**         if enabled, this hook will be called in case of a stack
-**         overflow.
+**         
 **     Parameters  :
 **         NAME            - DESCRIPTION
-**       * pxTask          - Pointer to task handle
-**       * pcTaskName      - Pointer to task name
+**         event           - Event (event number) to be processed.
 **     Returns     : Nothing
 ** ===================================================================
 */
 
-void FRTOS1_vApplicationTickHook(void);
+void S191_OnSRecord(byte *returnCode, byte type, dword addr, byte *dataBuf, byte dataBufSize, void *data);
 /*
 ** ===================================================================
-**     Event       :  FRTOS1_vApplicationTickHook (module Events)
+**     Event       :  S191_OnSRecord (module Events)
 **
-**     Component   :  FRTOS1 [FreeRTOS]
+**     Component   :  S191 [S19]
 **     Description :
-**         If enabled, this hook will be called by the RTOS for every
-**         tick increment.
+**         Event called for every S-Record
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**       * returnCode      - Pointer to error code
+**         type            - type of S-Record (as character), e.g. '3'
+**         addr            - Address of data
+**       * dataBuf         - Pointer to buffer of S-Record data
+**                           (in binary form)
+**         dataBufSize     - Size in bytes of the data
+**                           buffer
+**       * data            - Pointer to private data
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void S191_OnError(S191_ErrorCode errorCode, S191_ParserStruct *iteratorInfo);
+/*
+** ===================================================================
+**     Event       :  S191_OnError (module Events)
+**
+**     Component   :  S191 [S19]
+**     Description :
+**         Event called in case of an error
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         errorCode       - 
+**       * iteratorInfo    - Pointer to parser info
+**                           struct
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void AD1_OnEnd(void);
+/*
+** ===================================================================
+**     Event       :  AD1_OnEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called after the measurement (which consists
+**         of <1 or more conversions>) is/are finished.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
 **     Parameters  : None
 **     Returns     : Nothing
 ** ===================================================================
 */
 
-void FRTOS1_vApplicationIdleHook(void);
+void AD1_OnCalibrationEnd(void);
 /*
 ** ===================================================================
-**     Event       :  FRTOS1_vApplicationIdleHook (module Events)
+**     Event       :  AD1_OnCalibrationEnd (module Events)
 **
-**     Component   :  FRTOS1 [FreeRTOS]
+**     Component   :  AD1 [ADC]
 **     Description :
-**         If enabled, this hook will be called when the RTOS is idle.
-**         This might be a good place to go into low power mode.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-
-void FRTOS1_vApplicationMallocFailedHook(void);
-/*
-** ===================================================================
-**     Event       :  FRTOS1_vApplicationMallocFailedHook (module Events)
-**
-**     Component   :  FRTOS1 [FreeRTOS]
-**     Description :
-**         If enabled, the RTOS will call this hook in case memory
-**         allocation failed.
+**         This event is called when the calibration has been finished.
+**         User should check if the calibration pass or fail by
+**         Calibration status method./nThis event is enabled only if
+**         the <Interrupt service/event> property is enabled.
 **     Parameters  : None
 **     Returns     : Nothing
 ** ===================================================================
