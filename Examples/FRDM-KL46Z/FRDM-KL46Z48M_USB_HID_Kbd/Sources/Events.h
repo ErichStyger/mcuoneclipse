92caa41e808bf97b8c7f78c2ae0c5a7e16a090f9
Erich Styger <erich.styger@steinerberg.com>
2013-08-27 10:25:41 +0200

    initial version of USB HID Keyboard device on the FRDM-KL46Z48M board

---
+++ Examples/FRDM-KL46Z/FRDM-KL46Z48M_USB_HID_Kbd/Sources/Events.h
@@ -0,0 +1,107 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MKL46Z256VMC4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2013-08-26, 18:37, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "USB1.h"
+#include "USB0.h"
+#include "HIDK1.h"
+#include "Tx2.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "WAIT1.h"
+#include "SW1.h"
+#include "BitIoLdd4.h"
+#include "SW3.h"
+#include "BitIoLdd5.h"
+#include "PTC.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL46Z256MC4]
+**     Description :
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the <NMI
+**         interrupt> property is set to 'Enabled'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnNMIINT(void);
+
+
+void HIDK1_OnEvent(byte error);
+/*
+** ===================================================================
+**     Event       :  HIDK1_OnEvent (module Events)
+**
+**     Component   :  HIDK1 [FSL_USB_HID_Keyboard_Device]
+**     Description :
+**         Hook called in case of an error
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         error           - Error or event code
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.2 [05.06]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/FRDM-KL46Z/FRDM-KL46Z48M_USB_HID_Kbd/readme.txt
@@ -0,0 +1,4 @@
+readme.txt
+----------
+This project demonstrates USB HID Keyboard device class on the FRDM-KL46Z48M board.
+Pressing SW1 will send a 'PrintScreen' key to the host.
