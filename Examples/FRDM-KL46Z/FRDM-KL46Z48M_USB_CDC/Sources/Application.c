60968b860d8e2de7318d31b47bf00d3d0cc11eb7
Erich Styger <erich.styger@steinerberg.com>
2013-08-27 11:21:30 +0200

    initial USB CDC project for FRDM-KL46Z

---
+++ Examples/FRDM-KL46Z/FRDM-KL46Z48M_USB_CDC/Sources/Application.c
@@ -0,0 +1,53 @@
+/*
+ * Application.c
+ *
+ *  Created on: Jul 1, 2013
+ *      Author: Erich Styger
+ */
+
+#include "Application.h"
+#include "SW1.h"
+#include "SW3.h"
+#include "UTIL1.h"
+#include "LED1.h"
+#include "LED2.h"
+#include "WAIT1.h"
+#include "USB1.h"
+#include "CDC1.h"
+
+static uint8_t cdc_buffer[USB1_DATA_BUFF_SIZE];
+static uint8_t in_buffer[USB1_DATA_BUFF_SIZE];
+
+void APP_Run(void) {
+  int i;
+  uint32_t val = 0;
+  unsigned char buf[16];
+
+  for(;;) {
+    while(CDC1_App_Task(cdc_buffer, sizeof(cdc_buffer))==ERR_BUSOFF) {
+      /* device not enumerated */
+      LED1_Neg(); LED2_Off();
+      WAIT1_Waitms(10);
+    }
+    LED2_Off(); LED1_Neg();
+    if (CDC1_GetCharsInRxBuf()!=0) {
+      i = 0;
+      while(   i<sizeof(in_buffer)-1
+            && CDC1_GetChar(&in_buffer[i])==ERR_OK
+           )
+      {
+        i++;
+      }
+      in_buffer[i] = '\0';
+      (void)CDC1_SendString((unsigned char*)"echo: ");
+      (void)CDC1_SendString(in_buffer);
+      UTIL1_strcpy(buf, sizeof(buf), (unsigned char*)"val: ");
+      UTIL1_strcatNum32u(buf, sizeof(buf), val);
+      UTIL1_strcat(buf, sizeof(buf), (unsigned char*)"\r\n");
+      (void)CDC1_SendString(buf);
+      val++;
+    } else {
+      WAIT1_Waitms(10);
+    }
+  }
+}
---
+++ Examples/FRDM-KL46Z/FRDM-KL46Z48M_USB_CDC/readme.txt
@@ -0,0 +1,3 @@
+readme.txt
+----------
+This project demonstrates USB CDC device class on the FRDM-KL46Z48M board.
