62d66d1367a3676f65c63be06bdb61adea9e1f60
Erich Styger <erich.styger@steinerberg.com>
2013-09-02 18:04:19 +0200

    trying to use rm instead of del for PEx projects

--- Examples/FRDM-KL46Z/FRDM-KL46Z_FreeRTOS/Sources/Events.c
+++ Examples/FRDM-KL46Z/FRDM-KL46Z_FreeRTOS/Sources/Events.c
@@ -144,6 +144,63 @@ void FRTOS1_vApplicationMallocFailedHook(void)
   for(;;) {}
 }
 
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vOnPreSleepProcessing (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         Used in tickless idle mode only, but required in this mode.
+**         Hook for the application to enter low power mode.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         expectedIdleTicks - expected idle
+**                           time, in ticks
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vOnPreSleepProcessing(portTickType expectedIdleTicks)
+{
+#if 1
+  /* example for Kinetis (enable SetOperationMode() in CPU component): */
+  Cpu_SetOperationMode(DOM_WAIT, NULL, NULL);
+  /* or to wait for interrupt:
+    __asm volatile("dsb");
+    __asm volatile("wfi");
+    __asm volatile("isb");
+  */
+#elif 0
+  /* example for S08/S12/ColdFire V1 (enable SetWaitMode() in CPU): */
+  Cpu_SetWaitMode();
+#elif 0
+  /* example for ColdFire V2: */
+   __asm("stop #0x2000"); */
+#else
+  #error "you *must* enter low power mode (wait for interrupt) here!"
+#endif
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vOnPostSleepProcessing (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         Event called after the CPU woke up after low power mode.
+**         This event is optional.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         expectedIdleTicks - expected idle
+**                           time, in ticks
+**     Returns     : Nothing
+** ===================================================================
+*/
+void FRTOS1_vOnPostSleepProcessing(portTickType expectedIdleTicks)
+{
+  /* Write your code here ... */
+}
+
 /* END Events */
 
 #ifdef __cplusplus
