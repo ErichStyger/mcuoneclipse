24ebbf3c565418b21f7852bf31c0e61004332a81
Erich Styger <erich.styger@steinerberg.com>
2013-09-29 17:40:07 +0200

    added optimized version with macros

--- Examples/FRDM-KL25Z/Freedom_Bits/Sources/ProcessorExpert.c
+++ Examples/FRDM-KL25Z/Freedom_Bits/Sources/ProcessorExpert.c
@@ -33,6 +33,17 @@
 #include "IO_Map.h"
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
+#define OPTIMIZE_BIT_ACCESS   1 /* set to 1 if using optimized bit access */
+#if OPTIMIZE_BIT_ACCESS
+  #define RED_Clr()     GPIO_PDD_ClearPortDataOutputMask(RED_MODULE_BASE_ADDRESS, RED_PORT_MASK)
+  #define RED_Set()     GPIO_PDD_SetPortDataOutputMask(RED_MODULE_BASE_ADDRESS, RED_PORT_MASK)
+  #define GREEN_Clr()   GPIO_PDD_ClearPortDataOutputMask(GREEN_MODULE_BASE_ADDRESS, GREEN_PORT_MASK)
+  #define GREEN_Set()   GPIO_PDD_SetPortDataOutputMask(GREEN_MODULE_BASE_ADDRESS, GREEN_PORT_MASK)
+  #define BLUE_Clr()    GPIO_PDD_ClearPortDataOutputMask(BLUE_MODULE_BASE_ADDRESS, BLUE_PORT_MASK)
+  #define BLUE_Set()    GPIO_PDD_SetPortDataOutputMask(BLUE_MODULE_BASE_ADDRESS, BLUE_PORT_MASK)
+  #define SW1_Get()     (GPIO_PDD_GetPortDataInput(SW1_MODULE_BASE_ADDRESS) & SW1_PORT_MASK)
+  #define SW2_Get()     (GPIO_PDD_GetPortDataInput(SW2_MODULE_BASE_ADDRESS) & SW2_PORT_MASK)
+#endif
 
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
@@ -44,21 +55,43 @@ int main(void)
   PE_low_level_init();
   /*** End of Processor Expert internal initialization.                    ***/
 
-  GREEN_ClrVal(NULL); /* turn on green LED */
+#if OPTIMIZE_BIT_ACCESS
+  GREEN_Clr(); /* turn on green LED */
   WAIT1_Waitms(1000);
-  GREEN_SetVal(NULL); /* turn off green LED */
+  GREEN_Set(); /* turn off green LED */
   for(;;) {
-    if (SW1_GetVal(NULL)==0) { /* button low level => pressed */
-      RED_ClrVal(NULL); /* LED cathode is connected to microcontroller pin: low level turns it on */
+    if (SW1_Get()==0) { /* button low level => pressed */
+      RED_Clr(); /* LED cathode is connected to microcontroller pin: low level turns it on */
+      BLUE_Set(); /* turn off blue led */
     } else {
-      RED_SetVal(NULL); /* turn off red led */
+      RED_Set(); /* turn off red led */
+      BLUE_Clr(); /* turn on blue led */
     }
-    if (SW2_GetVal(NULL)==0) { /* switch low level => pressed */
-       BLUE_ClrVal(NULL); /* turn on blue led */
+    if (SW2_Get()==0) { /* switch low level => pressed */
+      BLUE_Clr(); /* turn on blue led */
     } else {
-      BLUE_SetVal(NULL); /* turn off blue led */
+      BLUE_Set(); /* turn off blue led */
     }
   }
+#else
+  GREEN_ClrVal(GREEN_DeviceData); /* turn on green LED */
+  WAIT1_Waitms(1000);
+  GREEN_SetVal(GREEN_DeviceData); /* turn off green LED */
+  for(;;) {
+    if (SW1_GetVal(SW1_DeviceData)==0) { /* button low level => pressed */
+      RED_ClrVal(RED_DeviceData); /* LED cathode is connected to microcontroller pin: low level turns it on */
+      BLUE_SetVal(BLUE_DeviceData); /* turn off blue led */
+    } else {
+      RED_SetVal(RED_DeviceData); /* turn off red led */
+      BLUE_ClrVal(BLUE_DeviceData); /* turn on blue led */
+    }
+    if (SW2_GetVal(SW2_DeviceData)==0) { /* switch low level => pressed */
+      BLUE_ClrVal(BLUE_DeviceData); /* turn on blue led */
+    } else {
+      BLUE_SetVal(BLUE_DeviceData); /* turn off blue led */
+    }
+  }
+#endif
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
   #ifdef PEX_RTOS_START
---
+++ Examples/FRDM-KL25Z/Freedom_Bits/readme.txt
@@ -0,0 +1,7 @@
+readme.txt
+----------
+Example which shows how to use the BitIO_LDD with Processor Expert.
+
+See:
+http://mcuoneclipse.com/2012/11/12/tutorial-bits-and-pins-with-kinetis/
+http://mcuoneclipse.com/2013/09/29/optimized-bitio_ldd-programming-with-processor-expert/
