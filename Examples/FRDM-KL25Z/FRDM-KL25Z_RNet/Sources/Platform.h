92ee3f2d3f2992fba0773daf2987173beb53dc7f
Erich Styger <erich.styger@steinerberg.com>
2013-11-15 07:56:29 +0100

    initial RNet application

---
+++ Examples/FRDM-KL25Z/FRDM-KL25Z_RNet/Sources/Platform.h
@@ -0,0 +1,14 @@
+/*
+ * Platform.h
+ *
+ *  Created on: Nov 15, 2013
+ *      Author: tastyger
+ */
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+
+#define PL_HAS_RADIO   0
+
+#endif /* PLATFORM_H_ */
---
+++ Examples/FRDM-KL25Z/FRDM-KL25Z_RNet/readme.txt
@@ -0,0 +1,4 @@
+readme.txt
+==========
+
+This project demonstrate the usage of a simple remote/radio network stack (RNet) with the FRDM-KL25Z and the nRF24L01+.
