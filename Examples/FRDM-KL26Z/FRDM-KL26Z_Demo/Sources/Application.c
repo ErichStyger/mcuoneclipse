3c3bf7932ba36b164a63c7f988a139f4a2a5bd95
Erich Styger <erich.styger@steinerberg.com>
2013-12-29 17:22:56 +0100

    added additional functions and moved register map to header file.

--- Examples/FRDM-KL26Z/FRDM-KL26Z_Demo/Sources/Application.c
+++ Examples/FRDM-KL26Z/FRDM-KL26Z_Demo/Sources/Application.c
@@ -71,10 +71,80 @@ void APP_HandleEvent(uint8_t event) {
   }
 }
 
+static uint8_t InitAccelMagnetometer(void) {
+  uint8_t databyte;
+  
+  FX1_Init();
+  
+  if (FX1_WhoAmI(&databyte)!=ERR_OK) {
+    CLS1_SendStr((unsigned char*)"Failed!\r\n", CLS1_GetStdio()->stdErr);
+    return ERR_FAILED;
+  }
+  if (databyte!=FX1_WHO_AM_I_VAL) {
+    CLS1_SendStr((unsigned char*)"Unknown device!\r\n", CLS1_GetStdio()->stdErr);
+    return ERR_FAILED;
+  }
+  // write 0000 0000 = 0x00 to accelerometer control register 1 to place FXOS8700 into
+  // standby
+  // [7-1] = 0000 000
+  // [0]: active=0
+  if (FX1_WriteReg8(FX1_CTRL_REG_1, 0x00) != ERR_OK) {
+    CLS1_SendStr((unsigned char*)"Failed!\r\n", CLS1_GetStdio()->stdErr);
+    return ERR_FAILED;
+  }
+  // write 0001 1111 = 0x1F to magnetometer control register 1
+  // [7]: m_acal=0: auto calibration disabled
+  // [6]: m_rst=0: no one-shot magnetic reset
+  // [5]: m_ost=0: no one-shot magnetic measurement
+  // [4-2]: m_os=111=7: 8x oversampling (for 200Hz) to reduce magnetometer noise
+  // [1-0]: m_hms=11=3: select hybrid mode with accel and magnetometer active
+  if (FX1_WriteReg8(FX1_M_CTRL_REG_1, 0x1F) != ERR_OK) {
+    CLS1_SendStr((unsigned char*)"Failed!\r\n", CLS1_GetStdio()->stdErr);
+    return ERR_FAILED;
+  }
+  // write 0010 0000 = 0x20 to magnetometer control register 2
+  // [7]: reserved
+  // [6]: reserved
+  // [5]: hyb_autoinc_mode=1 to map the magnetometer registers to follow the
+  // accelerometer registers
+  // [4]: m_maxmin_dis=0 to retain default min/max latching even though not used
+  // [3]: m_maxmin_dis_ths=0
+  // [2]: m_maxmin_rst=0
+  // [1-0]: m_rst_cnt=00 to enable magnetic reset each cycle
+  if (FX1_WriteReg8(FX1_M_CTRL_REG_2, 0x20) != ERR_OK) {
+    CLS1_SendStr((unsigned char*)"Failed!\r\n", CLS1_GetStdio()->stdErr);
+    return ERR_FAILED;
+  }
+  // write 0000 0001= 0x00 to XYZ_DATA_CFG register
+  // [7]: reserved
+  // [6]: reserved
+  // [5]: reserved
+  // [4]: hpf_out=0
+  // [3]: reserved
+  // [2]: reserved
+  // [1-0]: fs=00 for accelerometer range of +/-2g range with 0.244mg/LSB
+  if (FX1_WriteReg8(FX1_XYZ_DATA_CFG, 0x00) != ERR_OK) {
+    CLS1_SendStr((unsigned char*)"Failed!\r\n", CLS1_GetStdio()->stdErr);
+    return ERR_FAILED;
+  }
+  // write 0000 1101b = 0x0D to accelerometer control register 1
+  // [7-6]: aslp_rate=00
+  // [5-3]: dr=001=1 for 200Hz data rate (when in hybrid mode)
+  // [2]: lnoise=1 for low noise mode
+  // [1]: f_read=0 for normal 16 bit reads
+  // [0]: active=1 to take the part out of standby and enable sampling
+  if (FX1_WriteReg8(FX1_CTRL_REG_1, 0x0D) != ERR_OK) {
+    CLS1_SendStr((unsigned char*)"Failed!\r\n", CLS1_GetStdio()->stdErr);
+    return ERR_FAILED;
+  }
+  // normal return
+  return ERR_OK;
+}
+
 static void AppTask(void *pvParameters) {
   uint16_t lightVal;
   
-  FX1_Init();
+  (void)InitAccelMagnetometer();
   for(;;) {
     EVNT1_HandleEvent();
     LED1_Neg();
