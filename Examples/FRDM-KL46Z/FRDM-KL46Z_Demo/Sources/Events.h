2568fde3a9911c67c8f6be14369bc3b91422e2ce
Erich Styger <erich.styger@steinerberg.com>
2014-02-25 17:49:59 +0100

    update for FreeRTOS V8.0.0

--- Examples/FRDM-KL46Z/FRDM-KL46Z_Demo/Sources/Events.h
+++ Examples/FRDM-KL46Z/FRDM-KL46Z_Demo/Sources/Events.h
@@ -88,7 +88,7 @@ extern "C" {
 void Cpu_OnNMIINT(void);
 
 
-void FRTOS1_vApplicationStackOverflowHook(xTaskHandle pxTask, signed portCHAR *pcTaskName);
+void FRTOS1_vApplicationStackOverflowHook(xTaskHandle pxTask, char *pcTaskName);
 /*
 ** ===================================================================
 **     Event       :  FRTOS1_vApplicationStackOverflowHook (module Events)
