0e1adfeb636609c064b33c6840286323c50b66a9
Erich Styger <erich.styger@steinerberg.com>
2014-03-06 14:44:50 +0100

    auto calibration added

--- Examples/FRDM-KL25Z/FRDM-KL25Z_MPC4728/Sources/Application.c
+++ Examples/FRDM-KL25Z/FRDM-KL25Z_MPC4728/Sources/Application.c
@@ -18,11 +18,10 @@
 
 typedef struct {
   uint32_t lowTicks, highTicks;
+  uint8_t lowPercent, highPercent;
 } QuadTime_t;
 
 #define NOF_SIGNALS 4
-
-static QuadTime_t signals[NOF_SIGNALS];
 
 typedef bool (*GetChannelBitFct)(void);
 
@@ -50,12 +49,14 @@ static GetChannelBitFct ChannelFct[NOF_SIGNALS] =
     Ch3Bit
 };
 
-static uint8_t Measure(uint8_t channel) {
+static uint8_t Measure(uint8_t channel, QuadTime_t *timing) {
   uint32_t timeout;
-  #define TIMEOUT_VAL 0xffff
+  #define TIMEOUT_VAL 0x0fff /* just some waiting time */
   
-  signals[channel].highTicks = 0;
-  signals[channel].lowTicks = 0;
+  timing->highTicks = 0;
+  timing->lowTicks = 0;
+  timing->lowPercent = 0;
+  timing->highPercent = 0;
   taskENTER_CRITICAL();
   /* trigger on raising edge */
   timeout = 0;
@@ -89,7 +90,7 @@ static uint8_t Measure(uint8_t channel) {
     }
   }
   /* signal is 0 now */
-  signals[channel].highTicks = TU2_GetCounterValue(TU2_DeviceData); 
+  timing->highTicks = TU2_GetCounterValue(TU2_DeviceData); 
   timeout = 0;
   TU2_ResetCounter(TU2_DeviceData);
   while(!ChannelFct[channel]()) { /* while signal is low... */
@@ -100,22 +101,98 @@ static uint8_t Measure(uint8_t channel) {
       return ERR_FAILED;
     }
   }
-  signals[channel].lowTicks = TU2_GetCounterValue(TU2_DeviceData); 
+  timing->lowTicks = TU2_GetCounterValue(TU2_DeviceData); 
   taskEXIT_CRITICAL();
+  timing->lowPercent  = (timing->lowTicks*100UL)/((uint32_t)timing->highTicks+(uint32_t)timing->lowTicks);
+  timing->highPercent = (timing->highTicks*100UL)/((uint32_t)timing->highTicks+(uint32_t)timing->lowTicks);
   return ERR_OK;
+}
+
+static uint8_t Tune(const CLS1_StdIOType *io) {
+  uint8_t channel;
+  uint16_t dac;
+  int i;
+  QuadTime_t timing;
+  uint8_t buf[32];
+  uint8_t res;
+  uint16_t dacArr[4];
+  uint8_t pd[4];
+ 
+  MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_LEFT), 10);
+  MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_RIGHT), 10);
+  for(i=0;i<4;i++) { /* init */
+    dacArr[i] = 0;
+    pd[i] = 0;
+  }
+  CLS1_SendStr((uint8_t*)"Tuning....\r\n", io->stdOut);
+  res = ERR_OK;
+  for(channel=0;channel<NOF_SIGNALS;channel++) {
+    for(i=0,dac=0;i<80 && dac<MPC4728_MAX_DAC_VAL;i++,dac += 0x10) {
+      UTIL1_strcpy(buf, sizeof(buf), (uint8_t*)"Channel: ");
+      UTIL1_strcatNum8u(buf, sizeof(buf), channel);
+      UTIL1_strcat(buf, sizeof(buf), (uint8_t*)" DAC: 0x");
+      UTIL1_strcatNum16Hex(buf, sizeof(buf), dac);
+      UTIL1_chcat(buf, sizeof(buf), ' ');
+      CLS1_SendStr(buf, io->stdOut);
+      dacArr[channel] = dac;
+      if (MPC4728_FastWriteDAC(dacArr, sizeof(dacArr), pd, sizeof(pd))!=ERR_OK) {
+        CLS1_SendStr((uint8_t*)"ERROR writing DAC/EE\r\n", io->stdErr);
+        res = ERR_FAILED;
+        break;
+      }
+      WAIT1_Waitms(10); /* wait some time to allow change */
+      if (Measure(channel, &timing)==ERR_OK) {
+        buf[0] = '\0';
+        UTIL1_strcatNum8u(buf, sizeof(buf), timing.highPercent);
+        UTIL1_strcat(buf, sizeof(buf), (uint8_t*)"% high, low ");
+        UTIL1_strcatNum8u(buf, sizeof(buf), timing.lowPercent);
+        UTIL1_strcat(buf, sizeof(buf), (uint8_t*)"%\r\n");
+        CLS1_SendStr(buf, io->stdOut);
+        if (timing.highPercent==50 || timing.lowPercent==50) {
+          CLS1_SendStr((uint8_t*)"Set!\r\n", io->stdErr);
+          break;
+        }
+      } else {
+        CLS1_SendStr((uint8_t*)"No signal\r\n", io->stdErr);
+      }
+    } /* for */
+    if (res!=ERR_OK) {
+      CLS1_SendStr((uint8_t*)"ERROR!\r\n", io->stdErr);
+      break;
+    }
+  } /* for */
+  MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_LEFT), 0);
+  MOT_SetSpeedPercent(MOT_GetMotorHandle(MOT_MOTOR_RIGHT), 0);
+  CLS1_SendStr((uint8_t*)"Writing to EEPROM...\r\n", io->stdOut);
+  for (channel=0;channel<NOF_SIGNALS;channel++) {
+    if (MPC4728_SingleWriteDACandEE(channel, dacArr[channel])!=ERR_OK) {
+      CLS1_SendStr((uint8_t*)"ERROR writing DAC/EE\r\n", io->stdErr);
+      res = ERR_FAILED;
+      break;
+    }
+    WAIT1_Waitms(500); /* give EEPROM time to write data */
+  }
+  CLS1_SendStr((uint8_t*)"finished!\r\n", io->stdOut);
+  return res;
 }
 
 static uint8_t PrintStatus(const CLS1_StdIOType *io) {
   uint8_t buf[48], buf2[16];
+  QuadTime_t timing;
   int i;
   
   CLS1_SendStatusStr((unsigned char*)"app", (unsigned char*)"\r\n", io->stdOut);
   for(i=0; i<NOF_SIGNALS; i++) {
-    if (Measure(i)==ERR_OK) {
-      UTIL1_strcpy(buf, sizeof(buf), (uint8_t*)"high: ");
-      UTIL1_strcatNum32u(buf, sizeof(buf), signals[i].highTicks);
+    if (Measure(i, &timing)==ERR_OK) {
+      buf[0] = '\0';
+      UTIL1_strcatNum8u(buf, sizeof(buf), timing.lowPercent);
+      UTIL1_strcat(buf, sizeof(buf), (uint8_t*)"% to ");
+      UTIL1_strcatNum8u(buf, sizeof(buf), timing.highPercent);
+      UTIL1_strcat(buf, sizeof(buf), (uint8_t*)"%, ");
+      UTIL1_strcat(buf, sizeof(buf), (uint8_t*)"high: ");
+      UTIL1_strcatNum32u(buf, sizeof(buf), timing.highTicks);
       UTIL1_strcat(buf, sizeof(buf), (uint8_t*)" ticks, low: ");
-      UTIL1_strcatNum32u(buf, sizeof(buf), signals[i].lowTicks);
+      UTIL1_strcatNum32u(buf, sizeof(buf), timing.lowTicks);
       UTIL1_strcat(buf, sizeof(buf), (uint8_t*)" ticks\r\n");
     } else {
       UTIL1_strcpy(buf, sizeof(buf), (uint8_t*)"TIMEOUT\r\n");
@@ -134,7 +211,7 @@ static uint8_t PrintStatus(const CLS1_StdIOType *io) {
 static uint8_t PrintHelp(const CLS1_StdIOType *io) {
   CLS1_SendHelpStr((unsigned char*)"app", (unsigned char*)"Group of application commands\r\n", io->stdOut);
   CLS1_SendHelpStr((unsigned char*)"  help|status", (unsigned char*)"Print help or status information\r\n", io->stdOut);
-  CLS1_SendHelpStr((unsigned char*)"  measure", (unsigned char*)"General Call Reset\r\n", io->stdOut);
+  CLS1_SendHelpStr((unsigned char*)"  tune", (unsigned char*)"Tune all channels\r\n", io->stdOut);
   return ERR_OK;
 }
 
@@ -145,6 +222,9 @@ byte APP_ParseCommand(const unsigned char *cmd, bool *handled, const CLS1_StdIOT
   } else if ((UTIL1_strcmp((char*)cmd, CLS1_CMD_STATUS)==0) || (UTIL1_strcmp((char*)cmd, "app status")==0)) {
     *handled = TRUE;
     return PrintStatus(io);
+  } else if ((UTIL1_strcmp((char*)cmd, "app tune")==0)) {
+    *handled = TRUE;
+    return Tune(io);
   }
   return ERR_OK;
 }
--- Examples/FRDM-KL25Z/FRDM-KL25Z_MPC4728/Sources/MPC4728.h
+++ Examples/FRDM-KL25Z/FRDM-KL25Z_MPC4728/Sources/MPC4728.h
@@ -9,6 +9,14 @@
 
 #include "CLS1.h"
 
+#define MPC4728_MAX_DAC_VAL  0xfff  /* 12bit */
+
+uint8_t MPC4728_SingleWriteDACandEE(uint8_t channel, uint16_t val);
+
+/* Fast Mode Write: updates all 4 channels, but does not affect EEPROM */
+uint8_t MPC4728_FastWriteDAC(uint16_t dac[4], size_t dacSize, uint8_t pd[4], size_t pdSize);
+
+
 byte MPC4728_ParseCommand(const unsigned char *cmd, bool *handled, const CLS1_StdIOType *io);
 
 void MPC4728_Deinit(void);
