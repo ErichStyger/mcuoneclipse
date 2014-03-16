c5574827472cac06642ade0176c6cd329e53ed10
Erich Styger <erich.styger@steinerberg.com>
2014-03-16 15:38:02 +0100

    updated with link to post and added doxygen comments

--- Examples/FRDM-KL25Z/FRDM-KL25Z_LowPower_LLS/Sources/Application.h
+++ Examples/FRDM-KL25Z/FRDM-KL25Z_LowPower_LLS/Sources/Application.h
@@ -8,10 +8,13 @@
 #ifndef APPLICATION_H_
 #define APPLICATION_H_
 
+/*! \brief callback called from the LLS wake-up interrupt */
 void APP_OnLLSWakeUpInterrupt(void);
 
+/*! \brief callback called from low power timer interrupt */
 void APP_TimerInterrupt(void);
 
+/*! \brief application main entry point */
 void APP_Run(void);
 
 #endif /* APPLICATION_H_ */
--- Examples/FRDM-KL25Z/FRDM-KL25Z_LowPower_LLS/readme.txt
+++ Examples/FRDM-KL25Z/FRDM-KL25Z_LowPower_LLS/readme.txt
@@ -1,3 +1,6 @@
 readme.txt
 ----------
-This projects shows how to use LLS low power mode with Processor Expert
+This projects shows how to use LLS low power mode with Processor Expert.
+
+See:
+http://mcuoneclipse.com/2014/03/16/starting-point-for-kinetis-low-power-lls-mode/
