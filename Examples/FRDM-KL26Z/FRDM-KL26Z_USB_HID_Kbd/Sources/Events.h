3615380cd15cde157c4103100ccef33e2eb25341
Erich Styger <erich.styger@steinerberg.com>
2014-06-29 07:08:59 +0200

    new USB HID Keyboard demo for FRDM-KL26Z

---
+++ Examples/FRDM-KL26Z/FRDM-KL26Z_USB_HID_Kbd/Sources/Events.h
@@ -0,0 +1,95 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MKL26Z128VLH4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2013-12-29, 11:41, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "LEDR.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LEDG.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LEDB.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "USB1.h"
+#include "USB0.h"
+#include "HIDK1.h"
+#include "Tx2.h"
+#include "WAIT1.h"
+#include "UTIL1.h"
+#include "CS1.h"
+#include "SW1.h"
+#include "BitIoLdd4.h"
+#include "PTC.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL26Z256MC4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void);
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/FRDM-KL26Z/FRDM-KL26Z_USB_HID_Kbd/readme.txt
@@ -0,0 +1,6 @@
+readme.txt
+----------
+This is an example project for the FRDM-KL26Z with USB HID Keyboard device class.
+
+See
+http://mcuoneclipse.com/2013/06/30/using-the-frdm-kl25z-as-usb-keyboard/
