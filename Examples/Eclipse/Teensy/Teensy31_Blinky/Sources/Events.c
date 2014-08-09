91c0d191e9e153ba216e10d4da83e48830a780fa
Erich Styger <erich.styger@steinerberg.com>
2014-08-09 21:26:51 +0200

    Blinky project for Teensy 3.1 board with Eclipse

---
+++ Examples/Eclipse/Teensy/Teensy31_Blinky/Sources/Events.c
@@ -0,0 +1,74 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : Teensy31_Blinky
+**     Processor   : MK20DX256VLL7
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-08-09, 18:55, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MK20DX256LL7]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void)
+{
+  /* Write your code here ... */
+}
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.10]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/Eclipse/Teensy/Teensy31_Blinky/readme.txt
@@ -0,0 +1,7 @@
+readme.txt
+----------
+This is a simple LED blinking program for the Teensy 3.1 board.
+Both P&D and Segger debugging is configured.
+
+See:
+http://mcuoneclipse.com/2014/08/09/hacking-the-teensy-v3-1-for-swd-debugging/
