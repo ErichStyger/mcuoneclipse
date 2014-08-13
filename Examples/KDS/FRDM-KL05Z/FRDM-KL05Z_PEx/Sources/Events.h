19bc623cf9a9f5397df9a90e05796b7a2c8ef705
Erich Styger <erich.styger@steinerberg.com>
2014-08-13 10:15:45 +0200

    initial FRDM-KL05Z project with KDS and OpenSDA connection

---
+++ Examples/KDS/FRDM-KL05Z/FRDM-KL05Z_PEx/Sources/Events.h
@@ -0,0 +1,76 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : FRDM-KL05Z_PEx
+**     Processor   : MKL05Z32VFM4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-08-13, 10:05, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL05Z32LF4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void);
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.10]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/FRDM-KL05Z/FRDM-KL05Z_PEx/readme.txt
@@ -0,0 +1,4 @@
+readme.txt
+----------
+
+This is a Processor Expert project for the FRDM-KL05Z board with Processor Expert.
