09fc0bdde2fc5aadf747add35342d3fae8fd1756
Erich Styger <erich.styger@steinerberg.com>
2014-08-23 07:37:45 +0200

    test project for re-built GNU libraries

--- Examples/Eclipse/FRDM-KL25Z/FRDM-KL25Z_Bare/sources/main.c
+++ Examples/Eclipse/FRDM-KL25Z/FRDM-KL25Z_Bare/sources/main.c
@@ -1,6 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+
 int i;
+char buf[32];
 
 int main(void) {
+  strcpy(buf, "hello");
+  strcat(buf, "world");
+  i = strlen(buf);
+  sprintf(buf, "hello");
   for(;;) {
     i++;
   }
