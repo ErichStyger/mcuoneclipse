066f29325774d5342b874a8a5d5c230d1ac801a9
Erich Styger <erich.styger@steinerberg.com>
2014-10-22 08:47:36 +0200

    updated printf() project

--- Examples/KDS/FRDM-KL25Z/FRDM-KL25Z_printf/Sources/Application.c
+++ Examples/KDS/FRDM-KL25Z/FRDM-KL25Z_printf/Sources/Application.c
@@ -7,35 +7,27 @@
 
 #include "Application.h"
 #include <stdio.h>
-//#include "LEDR.h"
-//#include "LEDG.h"
-//#include "WAIT1.h"
+#include "LEDR.h"
+#include "LEDG.h"
+#include "LEDB.h"
+#include "WAIT1.h"
 
 void APP_Run(void) {
-  int value;
+  int value=0;
   char buffer[64];
 
-#if 0
+  printf("Hello world!\r\n");
   for(;;) {
-    printf("Hello world!\r\n");
-    LEDR_On();
-    WAIT1_Waitms(500);
-    LEDR_Off();
-
     LEDG_On();
-    WAIT1_Waitms(500);
-    LEDG_Off();
-  }
-#endif
-
-  for(;;) {
-    printf("Hello world!\r\n");
     printf("Please enter a name:\n\r");
     scanf("%s", buffer);
     printf("  I have received: '%s'\r\n", buffer);
+    LEDG_Off();
 
+    LEDB_On();
     printf("Please enter a number:\r\n");
     scanf("%i", &value);
     printf("  I have received: '%i'\r\n", value);
+    LEDB_Off();
   }
 }
