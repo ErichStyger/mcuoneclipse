dd789b363e7df91a3729ede003136638da3819f0
Erich Styger <erich.styger@steinerberg.com>
2014-11-16 13:37:37 +0100

    updated ESP8266 application

--- Examples/KDS/FRDM-KL25Z/FRDM-KL25Z_ESP8266/Sources/Application.c
+++ Examples/KDS/FRDM-KL25Z/FRDM-KL25Z_ESP8266/Sources/Application.c
@@ -37,10 +37,15 @@ static uint8_t RxResponse(unsigned char *rxBuf, size_t rxBufLength, uint16_t msT
       res = ERR_OVERFLOW; /* not enough space in buffer */
       break;
     } else if (AS2_GetCharsInRxBuf() > 0) {
+#if 0
       if (AS2_RecvChar(&ch) != ERR_OK) {
         res = ERR_RXEMPTY;
         break;
       }
+#else
+      /* might get an overrun OVERRUN_ERR error here? Ignoring error for now */
+      (void)AS2_RecvChar(&ch);
+#endif
       *p++ = ch;
       *p = '\0'; /* always terminate */
       rxBufLength--;
@@ -48,7 +53,7 @@ static uint8_t RxResponse(unsigned char *rxBuf, size_t rxBufLength, uint16_t msT
           && UTIL1_strtailcmp(rxBuf, expectedTail) == 0) {
       break; /* finished */
     } else {
-      WAIT1_Waitms(1);
+      WAIT1_WaitOSms(1);
       msTimeout--;
     }
   } /* for */
@@ -71,13 +76,14 @@ uint8_t ESP_SendATCommand(uint8_t *cmd, uint8_t *rxBuf, size_t rxBufSize, uint8_
   if (AS2_SendBlock(cmd, (uint16_t)UTIL1_strlen((char*)cmd), &snt) != ERR_OK) {
     return ERR_FAILED;
   }
-  CLS1_SendStr(cmd, CLS1_GetStdio()->stdOut);
+  //CLS1_SendStr(cmd, CLS1_GetStdio()->stdOut);
   res = RxResponse(rxBuf, rxBufSize, ESP_TIMOUT_MS, expectedTailStr);
-  CLS1_SendStr(rxBuf, CLS1_GetStdio()->stdOut);
+  //CLS1_SendStr(rxBuf, CLS1_GetStdio()->stdOut);
   return res;
 }
 
 uint8_t ESP_Test(void) {
+  /* AT */
   uint8_t rxBuf[sizeof("AT\r\r\n\r\nOK\r\n")];
   uint8_t res;
 
@@ -86,20 +92,61 @@ uint8_t ESP_Test(void) {
 }
 
 uint8_t ESP_Restart(void) {
+  /* AT+RST */
   uint8_t rxBuf[sizeof("AT+RST\r\r\n\r\nOK\r\n")];
   uint8_t res;
 
-//  return ESP_SendATCommand("AT+RST\r\n", rxBuf, sizeof(rxBuf), "\r\nready\r\n");
+  /* ideally, I should wait here for 'ready' */
+  /* return ESP_SendATCommand("AT+RST\r\n", rxBuf, sizeof(rxBuf), "\r\nready\r\n"); */
   res = ESP_SendATCommand("AT+RST\r\n", rxBuf, sizeof(rxBuf), "AT+RST\r\r\n\r\nOK\r\n");
-  WAIT1_Waitms(500); /* wait after restart */
-  AS2_ClearRxBuf();
+  WAIT1_Waitms(2000); /* wait after restart */
+  AS2_ClearRxBuf(); /* clear buffer */
   return res;
+}
+
+uint8_t ESP_CheckFirmware(uint8_t *fwBuf, size_t fwBufSize) {
+  /* AT+GMR */
+  uint8_t rxBuf[32];
+  uint8_t res;
+  const unsigned char *p;
+
+  res = ESP_SendATCommand("AT+GMR\r\n", rxBuf, sizeof(rxBuf), "\r\n\r\nOK\r\n");
+  if (res==ERR_OK && UTIL1_strncmp(rxBuf, "AT+GMR\r\r\n", sizeof("AT+GMR\r\r\n")-1)==0) { /* check for beginning of response */
+    UTIL1_strCutTail(rxBuf, "\r\n\r\nOK\r\n"); /* cut tailing response */
+    p = rxBuf+sizeof("AT+GMR\r\r\n")-1;
+    UTIL1_strcpy(fwBuf, fwBufSize, p); /* copy firmware information string */
+  }
+  return res;
+}
+
+uint8_t ESP_PrintStatus(CLS1_ConstStdIOType *io) {
+  uint8_t buf[32];
+
+  CLS1_SendStatusStr("ESP8266", "\r\n", io->stdOut);
+  if (ESP_CheckFirmware(buf, sizeof(buf)) != ERR_OK) {
+    UTIL1_strcpy(buf, sizeof(buf), "FAILED\r\n");
+  } else {
+    UTIL1_strcat(buf, sizeof(buf), "\r\n");
+  }
+  CLS1_SendStatusStr("  Firmware", buf, io->stdOut);
+  return ERR_OK;
+}
+
+uint8_t ESP_ConnectWiFi(void) {
+  uint8_t buf[32];
+
+  UTIL1_strcpy(buf, sizeof(buf), "AT+CWMODE=1");
+  return ERR_OK;
 }
 
 void APP_Run(void) {
   CLS1_ConstStdIOType *io;
+  uint8_t buf[32];
 
   io = CLS1_GetStdio();
+  CLS1_SendStr("------------------------------------------\r\n", io->stdOut);
+  CLS1_SendStr("ESP8266 with FRDM-KL25Z\r\n", io->stdOut);
+  CLS1_SendStr("------------------------------------------\r\n", io->stdOut);
   if (ESP_Test()!=ERR_OK) {
     CLS1_SendStr("TEST failed!\r\n", io->stdErr);
   } else {
@@ -110,9 +157,15 @@ void APP_Run(void) {
   } else {
     CLS1_SendStr("Restart ok!\r\n", io->stdOut);
   }
+  ESP_PrintStatus(io);
+  if (ESP_ConnectWiFi()!=ERR_OK) {
+    CLS1_SendStr("connection to WiFi FAILED!\r\n", io->stdErr);
+  } else {
+    CLS1_SendStr("connected to WiFi!\r\n", io->stdErr);
+  }
   for(;;) {
-    CLS1_SendStr("Hello ESP8266!\r\n", io->stdOut);
-    ESP_Test();
+    //CLS1_SendStr("Hello ESP8266!\r\n", io->stdOut);
+    //ESP_Test();
     WAIT1_Waitms(1000);
   }
 }
--- Examples/KDS/FRDM-KL25Z/FRDM-KL25Z_ESP8266/readme.txt
+++ Examples/KDS/FRDM-KL25Z/FRDM-KL25Z_ESP8266/readme.txt
@@ -2,3 +2,7 @@ readme.txt
 ----------
 
 This project uses the ESP8266 WiFi module with the FRDM-KL25Z board, using Processor Expert and Kinetis Design Studio.
+
+See
+http://mcuoneclipse.com/2014/10/15/cheap-and-simple-wifi-with-esp8266-for-the-frdm-board/
+http://www.electrodragon.com/w/Wi07c
