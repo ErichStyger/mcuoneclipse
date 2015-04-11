0c2f9a33c91fbed62f8b7452c5fdb204755c5d27
Erich Styger <erich.styger@hslu.ch>
2015-04-11 16:52:43 +0200

    project using UART with the FRDM-KL02Z

---
+++ Examples/KDS/FRDM-KL02Z/FRDM-KL02Z_UART/Sources/Events.c
@@ -0,0 +1,74 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : FRDM-KL02Z_UART
+**     Processor   : MKL02Z32VFM4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-04-11, 14:05, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL02Z32FM4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void)
+{
+  /* Write your code here ... */
+}
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.11]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/FRDM-KL02Z/FRDM-KL02Z_UART/readme.txt
@@ -0,0 +1,3 @@
+readme.txt
+----------
+This project demonstrates UART trough OpenSDA with the FRDM-KL02Z Board and Kintetis Design Studio.
