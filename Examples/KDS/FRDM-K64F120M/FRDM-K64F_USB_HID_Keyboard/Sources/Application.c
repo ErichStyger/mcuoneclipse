5f1b28258b1e6ec30a4f71c0c561333ca7d0f894
Erich Styger <erich.styger@hslu.ch>
2015-06-13 18:47:41 +0200

    new USB HID Keyboard project for FRDM-K64F

---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_USB_HID_Keyboard/Sources/Application.c
@@ -0,0 +1,51 @@
+/*
+ * Application.c
+ *
+ *  Created on: Jul 1, 2013
+ *      Author: Erich Styger
+ */
+
+#include "Application.h"
+#include "SW2.h"
+#include "LED1.h"
+#include "LED2.h"
+#include "LED3.h"
+#include "WAIT1.h"
+#include "HIDK1.h"
+
+void APP_Run(void) {
+  int cnt=0; /* counter to slow down LED blinking */
+
+  for(;;) {
+    WAIT1_Waitms(10);
+    cnt++;
+    if (SW2_GetVal()==0) { /* button pressed */
+      WAIT1_Waitms(100); /* wait for debouncing */
+      if (SW2_GetVal()==0) { /* still pressed */
+#if 0 /* send a string */
+        (void)HIDK1_SendStr((unsigned char*)"SW2 press "); /* send a string */
+#endif
+#if 1 /* send print screen */
+        HIDK1_Send(MODIFERKEYS_NONE, KEY_PRINTSCREEN);
+        HIDK1_Send(MODIFERKEYS_NONE, KEY_NONE); /* release key */
+#endif
+#if 0 /* send CTRL+ALT+DELETE */
+        HIDK1_Send(MODIFERKEYS_LEFT_CTRL|MODIFERKEYS_RIGHT_ALT, KEY_DELETE);
+        HIDK1_Send(MODIFERKEYS_NONE, KEY_NONE); /* release key */
+#endif
+      }
+      while(SW2_GetVal()==0) {} /* wait until button is released */
+    }
+    if (HIDK1_App_Task()==ERR_OK) { /* run the USB application task: this will send the buffer */
+      if ((cnt%100)==0) {
+        LED1_Off();
+        LED2_Neg(); /* blink green LED if connected */
+      }
+    } else {
+      if ((cnt%200)==0) {
+        LED2_Off();
+        LED1_Neg(); /* blink red LED if not connected */
+      }
+    }
+  }
+}
