54d4fe543fd29518e10471d3318951ff099a9fe0
Erich Styger <erich.styger@hslu.ch>
2015-07-30 14:53:21 +0200

    first DMA transfers triggered by FTM0 :-)

--- Examples/KDS/FRDM-K64F120M/FRDM-K64F_NeoPixel_SDK_PEx/Sources/Application.c
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_NeoPixel_SDK_PEx/Sources/Application.c
@@ -10,6 +10,14 @@
 #include "osa1.h"
 #include <stdbool.h>
 
+static volatile bool dmaDone = false;
+
+#define NEO_NOF_PIXEL       1
+#define NEO_NOF_BITS_PIXEL 24
+static const uint8_t OneValue = 0xFF; /* value to clear or set the port bits */
+static uint8_t transmitBuf[] = {0x11,0x20,0x31,0x40,0x51,0x60,0x71,0x80};
+
+
 static const ftm_pwm_param_t flexTimer1_ChnConfig0 = {
   .mode = kFtmEdgeAlignedPWM,
   .edgeMode = kFtmHighTrue,
@@ -18,7 +26,7 @@ static const ftm_pwm_param_t flexTimer1_ChnConfig0 = {
   .uFirstEdgeDelayPercent = 0U,
 };
 
-static const ftm_pwm_param_t flexTimer1_ChnConfig1 = {
+static const ftm_pwm_param_t flexTimer0_ChnConfig0 = {
   .mode = kFtmEdgeAlignedPWM,
   .edgeMode = kFtmHighTrue,
   .uFrequencyHZ = 1000U,
@@ -26,7 +34,7 @@ static const ftm_pwm_param_t flexTimer1_ChnConfig1 = {
   .uFirstEdgeDelayPercent = 0U,
 };
 
-static const ftm_user_config_t flexTimer1_InitConfig0 = {
+static const ftm_user_config_t flexTimer0_InitConfig0 = {
   .tofFrequency      = 0U,
   .isWriteProtection = false,
   .BDMMode           = kFtmBdmMode_00,
@@ -36,48 +44,17 @@ static const ftm_user_config_t flexTimer1_InitConfig0 = {
 static void InitFlexTimer(uint32_t instance) {
   FTM_Type *ftmBase = g_ftmBase[instance];
 
-  FTM_DRV_Init(instance, &flexTimer1_InitConfig0);
+  FTM_DRV_Init(instance, &flexTimer0_InitConfig0);
   FTM_DRV_SetClock(instance, kClock_source_FTM_SystemClk, kFtmDividedBy1);
-  FTM_DRV_PwmStart(instance, (ftm_pwm_param_t*)&flexTimer1_ChnConfig0, 0U); /* this will start the clock! */
-  /* stop clock, so we can init second channel */
-  //FTM_DRV_CounterStop(instance);
-  FTM_HAL_SetClockSource(ftmBase, kClock_source_FTM_None);
-  FTM_DRV_PwmStart(instance, (ftm_pwm_param_t*)&flexTimer1_ChnConfig1, 1U); /* this will start the clock! */
-  /* stop it again */
-  //FTM_DRV_CounterStop(instance);
-  FTM_HAL_SetClockSource(ftmBase, kClock_source_FTM_None);
-
+  FTM_DRV_PwmStart(instance, (ftm_pwm_param_t*)&flexTimer0_ChnConfig0, 0U); /* this will start the clock! */
+  FTM_DRV_CounterStop(instance); /* stop clock, so we can init second channel */
+  FTM_DRV_PwmStart(instance, (ftm_pwm_param_t*)&flexTimer0_ChnConfig0, 1U); /* this will start the clock! */
+  FTM_DRV_CounterStop(instance); /* stop it again */
+  FTM_HAL_SetChnDmaCmd(ftmBase, 0, true); /* enable DMA request */
+  FTM_HAL_EnableChnInt(ftmBase, 0); /* enable channel interrupt: need to have both DMA and CHnIE set for DMA transfers! See RM 40.4.23 */
   FTM_DRV_SetTimeOverflowIntCmd(instance, false); /* disable interrupt */
   FTM_DRV_SetFaultIntCmd(instance, false); /* disable interrupt */
 }
-
-static void StartTransfer(uint32_t instance) {
-  FTM_Type *ftmBase = g_ftmBase[instance];
-
-  /*! \todo signals need to be low, are random now */
-  /* enable DMA */
-  FTM_HAL_SetChnDmaCmd(ftmBase, 0, true);
-  //FTM_HAL_SetChnDmaCmd(ftmBase, 1, true);
-  /* clock it */
-  FTM_HAL_SetClockSource(ftmBase, kClock_source_FTM_SystemClk);
-  OSA_TimeDelay(100);
-  FTM_HAL_SetClockSource(ftmBase, kClock_source_FTM_None);
-  /* disable DMA */
-  FTM_HAL_SetChnDmaCmd(ftmBase, 0, false);
-  //FTM_HAL_SetChnDmaCmd(ftmBase, 1, false);
-}
-
-//#define BUFFER_SIZE               16/*! Total transfer size */
-//#define EDMA_TRANSFER_SIZE        2 /*! Transfer size on basic loop */
-//#define EDMA_CHAIN_LENGTH         1 /*! Number of srcSG and destSG */
-//#define EDMA_WARTERMARK_LEVEL     8 /*! number of bytes transfered on each EDMA request(minor loop) */
-
-static volatile bool dmaDone = false;
-
-#define NEO_NOF_PIXEL 1
-#define NEO_NOF_BITS_PIXEL 24
-static const uint8_t OneValue = 0xFF; /* value to clear or set the port bits */
-static uint8_t transmitBuf[] = {0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88};
 
 void EDMA_Callback(void *param, edma_chn_status_t chanStatus) {
   dmaDone = true;
@@ -127,7 +104,6 @@ static void StartStopFTM(uint32_t instance, bool startIt) {
 
 static void DoDMA(void) {
   /* configure single end descriptor chain. */
-#if 1
   EDMA_DRV_ConfigScatterGatherTransfer(
           &chnState, /* channel information */
           &stcd, /* transfer control descriptor */
@@ -137,18 +113,6 @@ static void DoDMA(void) {
           &srcSG, &destSG, /* source and destination scatter-gather */
           1 /* number of scatter-gather in chain */
           );
-#else
-  EDMA_DRV_ConfigLoopTransfer(
-      &chnState, /* channel information */
-      &stcd, /* transfer control descriptor */
-      kEDMAMemoryToPeripheral, /* perform memory to peripheral operation */
-      (uint32_t)&transmitBuf[0], //srcAddr,
-      (uint32_t)&GPIO_PDOR_REG(PTD_BASE_PTR), //dstAddr,
-      1, //size ==> transferSize,
-      1, //eachrequest,
-      1, //totallength,
-      1); //number);
-#endif
   /* Initialize transfer. */
   dmaDone = false;
   EDMA_DRV_StartChannel(&chnState); /* enable DMA */
@@ -158,15 +122,11 @@ static void DoDMA(void) {
   } while(!dmaDone);
   StartStopFTM(FTM0_IDX, false); /* stop timer */
   EDMA_DRV_StopChannel(&chnState); /* stop DMA channel */
-  /* Release channel */
-  //EDMA_DRV_ReleaseChannel(&chnState);
 }
 
 static bool start = false;
 
 void APP_Run(void) {
-  int i;
-
   GPIO_PCOR_REG(PTD_BASE_PTR) = (1<<0); /* Port Clear Output PTD0 */
   InitFlexTimer(FTM0_IDX);
   InitDMA();
@@ -178,14 +138,13 @@ void APP_Run(void) {
       DoDMA();
       //GPIO_PTOR_REG(PTD_BASE_PTR) = (1<<0); /* Port Toggle Output PTD0 */
       //GPIO_PSOR_REG(PTD_BASE_PTR) = (1<<0); /* Port Set Output PTD0 */
-//      GPIO_PTOR_REG(PTE_BASE_PTR) = (1<<26); /* toggle PTE26 (GREEN RGB) */
+      //GPIO_PTOR_REG(PTE_BASE_PTR) = (1<<26); /* toggle PTE26 (GREEN RGB) */
     }
   }
 }
 
 /*! @brief EDMA ERROR IRQ handler with the same name in the startup code*/
-void DMA_Error_IRQHandler(void)
-{
-    EDMA_DRV_ErrorIRQHandler(0);
+void DMA_Error_IRQHandler(void) {
+  EDMA_DRV_ErrorIRQHandler(0);
 }
 
