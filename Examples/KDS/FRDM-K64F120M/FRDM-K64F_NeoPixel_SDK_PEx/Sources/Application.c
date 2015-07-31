6ede3541a62fbded07ea6d5ceb4cdef65027d8c4
Erich Styger <erich.styger@hslu.ch>
2015-07-31 09:42:22 +0200

    moved global variables

--- Examples/KDS/FRDM-K64F120M/FRDM-K64F_NeoPixel_SDK_PEx/Sources/Application.c
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_NeoPixel_SDK_PEx/Sources/Application.c
@@ -18,20 +18,19 @@ static volatile bool dmaDone = false;
 static const uint8_t OneValue[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; /* value to clear or set the port bits */
 static uint8_t transmitBuf[] = {0x11,0x20,0x31,0x40,0x51,0x60,0x71,0x80}; /* bit values */
 
-static const ftm_user_config_t flexTimer0_InitConfig0 = {
-  .tofFrequency      = 0U,
-  .isWriteProtection = false,
-  .BDMMode           = kFtmBdmMode_00,
-  .syncMethod        = (uint32_t)(kFtmUseSoftwareTrig)
-};
-
 static void InitFlexTimer(uint32_t instance) {
-  static const ftm_pwm_param_t flexTimer0_ChnConfig0 = { /* FTM0, channel 0 */
+  ftm_pwm_param_t flexTimer0_ChnConfig0 = { /* FTM channel configuration */
     .mode = kFtmEdgeAlignedPWM,
     .edgeMode = kFtmHighTrue,
     .uFrequencyHZ = 1000U,
     .uDutyCyclePercent = 10U,
     .uFirstEdgeDelayPercent = 0U,
+  };
+  ftm_user_config_t flexTimer0_InitConfig0 = {
+    .tofFrequency      = 0U,
+    .isWriteProtection = false, /* FTM is not write protected */
+    .BDMMode           = kFtmBdmMode_00, /* default mode for debug: timer will be stopped, can modify registers */
+    .syncMethod        = (uint32_t)(kFtmUseSoftwareTrig) /* using software synchronization */
   };
   uint8_t channel;
   FTM_Type *ftmBase = g_ftmBase[instance];
@@ -39,7 +38,8 @@ static void InitFlexTimer(uint32_t instance) {
   FTM_DRV_Init(instance, &flexTimer0_InitConfig0);
   FTM_DRV_SetTimeOverflowIntCmd(instance, false); /* disable interrupt */
   FTM_DRV_SetFaultIntCmd(instance, false); /* disable interrupt */
-  FTM_DRV_SetClock(instance, kClock_source_FTM_SystemClk, kFtmDividedBy1);
+  FTM_DRV_SetClock(instance, kClock_source_FTM_SystemClk, kFtmDividedBy1); /* use system clock with a divider of 1 */
+
   /* configure timer */
   FTM_HAL_ClearTimerOverflow(ftmBase); /* clear timer overflow */
 
@@ -49,10 +49,10 @@ static void InitFlexTimer(uint32_t instance) {
   /* Based on Ref manual, in PWM mode CNTIN is to be set 0*/
   FTM_HAL_SetCounterInitVal(ftmBase, 0);
 
-  FTM_HAL_SetMod(ftmBase, 0x5000);
-  FTM_HAL_SetChnCountVal(ftmBase, 0, 0x1000);
-  FTM_HAL_SetChnCountVal(ftmBase, 1, 0x1500);
-  FTM_HAL_SetChnCountVal(ftmBase, 2, 0x2000);
+  FTM_HAL_SetMod(ftmBase, 0x500);
+  FTM_HAL_SetChnCountVal(ftmBase, 0, 0x100);
+  FTM_HAL_SetChnCountVal(ftmBase, 1, 0x200);
+  FTM_HAL_SetChnCountVal(ftmBase, 2, 0x300);
 
   /* reset all values */
   FTM_HAL_SetCounter(ftmBase, 0); /* reset counter */
@@ -68,13 +68,11 @@ void EDMA_Callback(void *param, edma_chn_status_t chanStatus) {
   dmaDone = true;
 }
 
-static edma_software_tcd_t stcd0, stcd1, stcd2; /* transfer control descriptor */
 static edma_chn_state_t    chn0State, chn1State, chn2State;
-static edma_state_t        edmaState;
-static edma_user_config_t  edmaUserConfig;
-static edma_scatter_gather_list_t src0SG, src1SG, src2SG, dest0SG, dest1SG, dest2SG;
 
 static void InitDMA(void) {
+  edma_user_config_t  edmaUserConfig;
+  edma_state_t        edmaState;
   uint8_t res;
 
   /* Initialize eDMA modules. */
@@ -97,22 +95,6 @@ static void InitDMA(void) {
     for(;;); /* ups!?! */
   }
 
-  /* Configure EDMA channels */
-  src0SG.address  = (uint32_t)&OneValue[0];
-  dest0SG.address = (uint32_t)&GPIO_PSOR_REG(PTD_BASE_PTR);
-  src0SG.length   = 8; /* number of total bytes to read */
-  dest0SG.length  = 8; /* number of total bytes to write */
-
-  src1SG.address  = (uint32_t)&transmitBuf[0];//&OneValue;
-  dest1SG.address = (uint32_t)&GPIO_PDOR_REG(PTD_BASE_PTR);
-  src1SG.length   = 8; /* number of total bytes to read */
-  dest1SG.length  = 8; /* number of total bytes to write */
-
-  src2SG.address  = (uint32_t)&OneValue[0];
-  dest2SG.address = (uint32_t)&GPIO_PCOR_REG(PTD_BASE_PTR);
-  src2SG.length   = 8; /* number of total bytes to read */
-  dest2SG.length  = 8; /* number of total bytes to write */
-
   /* Install callback for eDMA handler on channel 2 */
   EDMA_DRV_InstallCallback(&chn2State, EDMA_Callback, NULL);
 }
@@ -128,6 +110,24 @@ static void StartStopFTM(uint32_t instance, bool startIt) {
 }
 
 static void DoDMA(void) {
+  edma_software_tcd_t stcd0, stcd1, stcd2; /* transfer control descriptor */
+  edma_scatter_gather_list_t src0SG, src1SG, src2SG, dest0SG, dest1SG, dest2SG;
+
+  /* Configure EDMA src and destination descriptors */
+  src0SG.address  = (uint32_t)&OneValue[0];
+  dest0SG.address = (uint32_t)&GPIO_PSOR_REG(PTD_BASE_PTR);
+  src0SG.length   = 8; /* number of total bytes to read */
+  dest0SG.length  = 8; /* number of total bytes to write */
+
+  src1SG.address  = (uint32_t)&transmitBuf[0];//&OneValue;
+  dest1SG.address = (uint32_t)&GPIO_PDOR_REG(PTD_BASE_PTR);
+  src1SG.length   = 8; /* number of total bytes to read */
+  dest1SG.length  = 8; /* number of total bytes to write */
+
+  src2SG.address  = (uint32_t)&OneValue[0];
+  dest2SG.address = (uint32_t)&GPIO_PCOR_REG(PTD_BASE_PTR);
+  src2SG.length   = 8; /* number of total bytes to read */
+  dest2SG.length  = 8; /* number of total bytes to write */
   /* configure single end descriptor chain */
   EDMA_DRV_ConfigScatterGatherTransfer(
           &chn0State, /* channel information */
@@ -179,7 +179,7 @@ void APP_Run(void) {
   InitDMA();
   for(;;) {
     GPIO_DRV_TogglePinOutput(LEDRGB_BLUE);
-    OSA_TimeDelay(100);
+    OSA_TimeDelay(10);
     if (start) {
       GPIO_PCOR_REG(PTD_BASE_PTR) = (1<<0); /* Port Clear Output PTD0 */
       DoDMA();
