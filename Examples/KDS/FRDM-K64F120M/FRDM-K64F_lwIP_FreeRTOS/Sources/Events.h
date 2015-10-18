4bf8becdd64928da6a63e985652452fbc253d59f
Erich Styger <erich.styger@hslu.ch>
2015-10-18 18:01:29 +0200

    initial lwIP project

---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_lwIP_FreeRTOS/Sources/Events.h
@@ -0,0 +1,61 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : FRDM-K64F_lwIP_FreeRTOS
+**     Processor   : MK64FN1M0VLL12
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-10-18, 16:12, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "fsl_device_registers.h"
+#include "clockMan1.h"
+#include "pin_init.h"
+#include "osa1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_lwIP_FreeRTOS/readme.txt
@@ -0,0 +1,1 @@
+Project for Kinetis Design Studio, showing using lwIP with Kinetis SDK v1.3.0 and FreeRTOS.
