44aabdc6700ea1774681921bb408fb0d1cafa59e
Erich Styger <erich.styger@hslu.ch>
2016-04-10 11:37:02 +0200

    USB HID Mouse example for FRDM-K22F

---
+++ Examples/KDS/FRDM-K22F/FRDM-K22F_USB_HID_Mouse/Sources/Events.h
@@ -0,0 +1,96 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : FRDM-K22F_USB_HID_Mouse
+**     Processor   : MK22FN512VDC12
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2016-04-10, 11:19, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Contents    :
+**         Cpu_OnNMI - void Cpu_OnNMI(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "Pins1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LED3.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "WAIT1.h"
+#include "UTIL1.h"
+#include "CS1.h"
+#include "USB1.h"
+#include "HIDM1.h"
+#include "Tx2.h"
+#include "KSDK1.h"
+#include "SW2.h"
+#include "BitIoLdd4.h"
+#include "SW3.h"
+#include "BitIoLdd5.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMI (module Events)
+**
+**     Component   :  Cpu [MK22FN512DC12]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMI(void);
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/FRDM-K22F/FRDM-K22F_USB_HID_Mouse/readme.txt
@@ -0,0 +1,4 @@
+readme.txt
+----------
+
+This project implements USB HID Mouse for the FRDM-K22F board, using Processor Expert and Kinetis Design Studio.
