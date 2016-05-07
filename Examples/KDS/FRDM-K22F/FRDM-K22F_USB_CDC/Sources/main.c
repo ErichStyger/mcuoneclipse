d0f99ca66684120b916c9bf402d8acc95429093b
Erich Styger <erich.styger@hslu.ch>
2016-05-07 20:50:29 +0200

    added timout to USB CDC and CDC1_SendDataBlock()

--- Examples/KDS/FRDM-K22F/FRDM-K22F_USB_CDC/Sources/main.c
+++ Examples/KDS/FRDM-K22F/FRDM-K22F_USB_CDC/Sources/main.c
@@ -48,6 +48,10 @@
 #include "Tx1.h"
 #include "Rx1.h"
 #include "KSDK1.h"
+#include "TMOUT1.h"
+#include "TI1.h"
+#include "TimerIntLdd1.h"
+#include "TU1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
