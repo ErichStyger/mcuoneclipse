8206ff28ec6b04fb8a2088f808d1788d0e4502f9
Erich Styger <erich.styger@hslu.ch>
2016-07-20 12:37:01 +0200

    added USB CDC Project with Processor Expert and SDK V1.3

---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_USB_CDC_SDK_V1_3/Sources/main.c
@@ -0,0 +1,115 @@
+/* ###################################################################
+**     Filename    : main.c
+**     Project     : test_pex_sdk1.3
+**     Processor   : MK64FN1M0VLL12
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2016-03-09, 16:35, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file main.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup main_module main module documentation
+**  @{
+*/         
+/* MODULE main */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "clockMan1.h"
+#include "pin_init.h"
+#include "osa1.h"
+#include "USB1.h"
+#include "CDC1.h"
+#include "Tx1.h"
+#include "Rx1.h"
+#include "KSDK1.h"
+#include "CS1.h"
+#include "TMOUT1.h"
+#include "WAIT1.h"
+#if CPU_INIT_CONFIG
+  #include "Init_Config.h"
+#endif
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+static uint8_t cdc_buffer[USB1_DATA_BUFF_SIZE];
+static uint8_t in_buffer[USB1_DATA_BUFF_SIZE];
+
+static void CDC_Run(void) {
+  int i;
+
+  for(;;) {
+    while(CDC1_App_Task(cdc_buffer, sizeof(cdc_buffer))==ERR_BUSOFF) {
+      /* device not enumerated */
+      WAIT1_Waitms(10);
+    }
+    if (CDC1_GetCharsInRxBuf()!=0) {
+      i = 0;
+      while(   i<sizeof(in_buffer)-1
+            && CDC1_GetChar(&in_buffer[i])==ERR_OK
+           )
+      {
+        i++;
+      }
+      in_buffer[i] = '\0';
+      (void)CDC1_SendString((unsigned char*)"echo: ");
+      (void)CDC1_SendString(in_buffer);
+      (void)CDC1_SendString((unsigned char*)"\r\n");
+    } else {
+      WAIT1_Waitms(10);
+    }
+  }
+}
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  INT_SYS_DisableIRQGlobal();
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  /* For example: for(;;) { } */
+  INT_SYS_EnableIRQGlobal();
+  CDC_Run();
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_USB_CDC_SDK_V1_3/readme.txt
@@ -0,0 +1,5 @@
+readme.txt
+----------
+This project demonstrates using USB CDC on the FRDM-K64F board with McuOnEclipse Processor Expert components.
+
+The USB CDC uses a virtual COM with 38400 baud.
