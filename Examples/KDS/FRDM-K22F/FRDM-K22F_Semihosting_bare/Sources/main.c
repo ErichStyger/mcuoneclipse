56fcf3d91947c1caff5c22a52c76dda1f503d57b
Erich Styger <erich.styger@hslu.ch>
2016-08-26 19:55:51 +0200

    added example project for semihosting and FRDM-K22F

---
+++ Examples/KDS/FRDM-K22F/FRDM-K22F_Semihosting_bare/Sources/main.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "MK22F51212.h"
+#include <stdio.h>
+
+#if 0 /* might be needed dependin on GNU version */
+void *_sbrk ( uint32_t delta )
+{
+extern char _end; /* Defined by the linker */
+static char *heap_end;
+char *prev_heap_end;
+
+  if (heap_end == 0) {
+    heap_end = &_end;
+  }
+
+  prev_heap_end = heap_end;
+  heap_end += delta;
+  return (void *) prev_heap_end;
+}
+#endif
+
+static int i = 0;
+
+static void delay(int cntr) {
+  while(cntr>0) {
+    __asm("nop \n");
+    cntr--;
+  }
+}
+
+int main(void)
+{
+
+    /* Write your code here */
+
+    /* This for loop should be replaced. By default this loop allows a single stepping. */
+    for (;;) {
+        i++;
+        printf("hello world\r\n");
+        delay(10000);
+    }
+    /* Never leave main */
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////
+// EOF
+////////////////////////////////////////////////////////////////////////////////
---
+++ Examples/KDS/FRDM-K22F/FRDM-K22F_Semihosting_bare/readme.txt
@@ -0,0 +1,4 @@
+readme.txt
+----------
+
+This project uses simple semihosting (printf()) with the FRDM-K22F, KDS V3.2.0 and the Segger and P&E debug connection.
