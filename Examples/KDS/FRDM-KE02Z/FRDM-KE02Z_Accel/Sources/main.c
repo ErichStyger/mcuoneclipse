df4162a7cfccb467c0ad7d186b1cbaff765cb058
Erich Styger <erich.styger@hslu.ch>
2016-09-27 19:54:54 +0200

    initial project of accelerometer demo for FRDM-KE02Z40M

---
+++ Examples/KDS/FRDM-KE02Z/FRDM-KE02Z_Accel/Sources/main.c
@@ -0,0 +1,109 @@
+/* ###################################################################
+**     Filename    : main.c
+**     Project     : FRDM-KE02Z_Accel
+**     Processor   : MKE02Z32VLH4
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2016-09-27, 07:14, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file main.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup main_module main module documentation
+**  @{
+*/         
+/* MODULE main */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "KIN1.h"
+#include "UTIL1.h"
+#include "WAIT1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LED3.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "KSDK1.h"
+#include "CS1.h"
+#include "CLS1.h"
+#include "AS1.h"
+#include "ASerialLdd1.h"
+#include "GI2C1.h"
+#include "MMA1.h"
+#include "CI2C1.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+  uint8_t buf[16];
+  uint16_t x, y, z;
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  /* For example: for(;;) { } */
+  for(;;) {
+    x = MMA1_GetXmg();
+    CLS1_SendStr("\r\nX: ", CLS1_GetStdio()->stdOut);
+    CLS1_SendNum16u(x, CLS1_GetStdio()->stdOut);
+    y = MMA1_GetYmg();
+    CLS1_SendStr(" Y: ", CLS1_GetStdio()->stdOut);
+    CLS1_SendNum16u(y, CLS1_GetStdio()->stdOut);
+    z = MMA1_GetZmg();
+    CLS1_SendStr(" Z: ", CLS1_GetStdio()->stdOut);
+    CLS1_SendNum16u(z, CLS1_GetStdio()->stdOut);
+    WAIT1_Waitms(100);
+  }
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/FRDM-KE02Z/FRDM-KE02Z_Accel/readme.txt
@@ -0,0 +1,4 @@
+readme.txt
+----------
+Example project for the FRDM-KE02Z with Kinetis Design Studio and McuOnEclipse components,
+demonstrating the onboard accelerometer.
