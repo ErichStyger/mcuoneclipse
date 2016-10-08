56abd1ab340a3c2d227a995ba7777eb11736adaf
Erich Styger <erich.styger@hslu.ch>
2016-10-08 11:05:32 +0200

    initial version for TWR-KL43Z48M

---
+++ Examples/KDS/TWR-KL43Z48M/TWR-KL43Z_Blinky_SDK_V1.3/Sources/Events.h
@@ -0,0 +1,65 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : TWR-KL43Z_Blinky_SDK_V1.3
+**     Processor   : MKL43Z256VLH4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2016-10-08, 10:37, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "fsl_device_registers.h"
+#include "clockMan1.h"
+#include "pin_init.h"
+#include "osa1.h"
+#include "gpio1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "KSDK1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/TWR-KL43Z48M/readme.txt
@@ -0,0 +1,3 @@
+readme.txt
+----------
+This folder contains projects for the TWR-KL43Z48M board and the Kinetis Design Studio.
