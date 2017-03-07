cf05b1f7a305d54c6c077759fd5981d0d2f292cb
Erich Styger <erich.styger@hslu.ch>
2017-03-07 15:49:20 +0100

    added accelerator and bare metal shell

--- Examples/KDS/FRDM-KL02Z/FRDM-KL02Z_UART/Sources/main.c
+++ Examples/KDS/FRDM-KL02Z/FRDM-KL02Z_UART/Sources/main.c
@@ -32,21 +32,27 @@
 #include "Events.h"
 #include "WAIT1.h"
 #include "LED1.h"
-#include "LEDpin1.h"
-#include "BitIoLdd1.h"
+#include "Inhr1.h"
+#include "PwmLdd1.h"
 #include "LED2.h"
-#include "LEDpin2.h"
-#include "BitIoLdd2.h"
+#include "Inhr2.h"
+#include "PwmLdd2.h"
 #include "LED3.h"
-#include "LEDpin3.h"
-#include "BitIoLdd3.h"
+#include "Inhr3.h"
+#include "PwmLdd3.h"
 #include "CLS1.h"
 #include "UTIL1.h"
 #include "CS1.h"
 #include "AS1.h"
 #include "ASerialLdd1.h"
-#include "KSDK1.h"
 #include "XF1.h"
+#include "MCUC1.h"
+#include "MMA1.h"
+#include "GI2C1.h"
+#include "CI2C1.h"
+#include "IntI2cLdd1.h"
+#include "TU1.h"
+#include "TU0.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -55,29 +61,44 @@
 /* User includes (#include below this line is not maintained by Processor Expert) */
 static unsigned char buffer[64];
 
+static const CLS1_ConstParseCommandCallback cmdTable[] =
+{
+    CLS1_ParseCommand,
+    LED1_ParseCommand,
+    LED2_ParseCommand,
+    LED3_ParseCommand,
+    MMA1_ParseCommand,
+    NULL/* sentinel */
+};
+
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
 {
   /* Write your local variable definition here */
+  int i;
 
   /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
   PE_low_level_init();
   /*** End of Processor Expert internal initialization.                    ***/
 
-  /* Write your code here */
-  /* For example: for(;;) { } */
+  MMA1_Init();
+  for(i=0;i<5;i++) {
+    LED1_On();
+    LED2_On();
+    LED3_On();
+    WAIT1_Waitms(200);
+    LED2_Off();
+    LED3_Off();
+    LED1_Off();
+    WAIT1_Waitms(100);
+  }
   buffer[0] = '\0'; /* initialize buffer for ReadLine() */
+  CLS1_SendStr("\r\nType 'help' with CR or LF at the end...\r\n", CLS1_GetStdio()->stdOut);
   for(;;) {
-    CLS1_SendStr("Type in some text with CR or LF at the end...\r\n", CLS1_GetStdio()->stdOut);
-    LED1_Neg();
-    WAIT1_Waitms(1000);
-    if (CLS1_ReadLine(buffer, buffer, sizeof(buffer), CLS1_GetStdio())) {
-      /* line read */
-      CLS1_SendStr("You entered:\r\n", CLS1_GetStdio()->stdOut);
-      CLS1_SendStr(buffer, CLS1_GetStdio()->stdOut);
-      buffer[0] = '\0';
-    }
+    //LED1_Neg();
+    WAIT1_Waitms(200);
+    (void)CLS1_ReadAndParseWithCommandTable(buffer, sizeof(buffer), CLS1_GetStdio(), cmdTable);
   }
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
