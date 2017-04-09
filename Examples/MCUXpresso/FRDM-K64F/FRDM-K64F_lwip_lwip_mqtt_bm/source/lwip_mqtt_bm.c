5c912c74264a82c45d6547ce327da2f92a96e11a
Erich Styger <erich.styger@hslu.ch>
2017-04-09 14:53:15 +0200

    update and cleanup

--- Examples/MCUXpresso/FRDM-K64F/FRDM-K64F_lwip_lwip_mqtt_bm/source/lwip_mqtt_bm.c
+++ Examples/MCUXpresso/FRDM-K64F/FRDM-K64F_lwip_lwip_mqtt_bm/source/lwip_mqtt_bm.c
@@ -34,8 +34,6 @@
 
 #include "lwip/opt.h"
 
-#if LWIP_IPV4 && LWIP_RAW
-
 #include "ping/ping.h"
 #include "lwip/timeouts.h"
 #include "lwip/init.h"
@@ -47,9 +45,65 @@
 #include "fsl_device_registers.h"
 #include "pin_mux.h"
 #include "clock_config.h"
+
+#include "mqtt.h"
+#include "sys.h"
+
+#define USE_HSLU   0 /* work or home address */
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+#if !USE_HSLU /* using fixed home network configuration for local broker */
+  /* IP address configuration. */
+  #define configIP_ADDR0 192
+  #define configIP_ADDR1 168
+  #define configIP_ADDR2 0
+  #define configIP_ADDR3 75
+
+  /* Netmask configuration. */
+  #define configNET_MASK0 255
+  #define configNET_MASK1 255
+  #define configNET_MASK2 255
+  #define configNET_MASK3 0
+
+  /* Gateway address configuration. */
+  #define configGW_ADDR0 192
+  #define configGW_ADDR1 168
+  #define configGW_ADDR2 0
+  #define configGW_ADDR3 1
+
+  /* MQTT broker address configuration. */
+  #define configBroker_ADDR0 192
+  #define configBroker_ADDR1 168
+  #define configBroker_ADDR2 0
+  #define configBroker_ADDR3 111
+#else /* using fixed work IP address */
+  /* IP address configuration for board. */
+  #define configIP_ADDR0 10
+  #define configIP_ADDR1 9
+  #define configIP_ADDR2 4
+  #define configIP_ADDR3 99
+
+  /* Netmask configuration. */
+  #define configNET_MASK0 255
+  #define configNET_MASK1 255
+  #define configNET_MASK2 254
+  #define configNET_MASK3 0
+
+  /* Gateway address configuration. */
+  #define configGW_ADDR0 10
+  #define configGW_ADDR1 9
+  #define configGW_ADDR2 5
+  #define configGW_ADDR3 254
+
+  /* MQTT broker address configuration. */
+  /* https://test.mosquitto.org/: 37.184.106.16 */
+  #define configBroker_ADDR0 10
+  #define configBroker_ADDR1 9
+  #define configBroker_ADDR2 4
+  #define configBroker_ADDR3 26
+#endif
+
 typedef enum {
 	MQTT_STATE_INIT,
     MQTT_STATE_IDLE,
@@ -59,218 +113,136 @@ typedef enum {
 	MQTT_STATE_DO_PUBLISH,
 	MQTT_STATE_DO_DISCONNECT
 } MQTT_StateT;
+
 static MQTT_StateT MQTT_state = MQTT_STATE_INIT;
 
-#define USE_PING   0 /* << EST */
-#define USE_DHCP   0 /* << EST issue with requesting semaphore in sys_mutex_lock() while in interrupt */
-#define USE_MQTT   1 /* << EST */
-#define USE_HSLU   0 /* << EST: fixed address */
-#define USE_HOME   1
+static mqtt_client_t mqtt_client;
+static ip4_addr_t broker_ipaddr;
 
-#if USE_MQTT
-  #include "mqtt.h"
-#endif
+/* The idea is to demultiplex topic and create some reference to be used in data callbacks
+   Example here uses a global variable, better would be to use a member in arg
+   If RAM and CPU budget allows it, the easiest implementation might be to just take a copy of
+   the topic string and use it in mqtt_incoming_data_cb
+*/
+static int inpub_id;
+static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len) {
+  printf("Incoming publish at topic %s with total length %u\n", topic, (unsigned int)tot_len);
 
-#if USE_HOME
-	/* IP address configuration. */
-	#define configIP_ADDR0 192
-	#define configIP_ADDR1 168
-	#define configIP_ADDR2 0
-	#define configIP_ADDR3 75
-
-	/* Netmask configuration. */
-	#define configNET_MASK0 255
-	#define configNET_MASK1 255
-	#define configNET_MASK2 255
-	#define configNET_MASK3 0
-
-	/* Gateway address configuration. */
-	#define configGW_ADDR0 192
-	#define configGW_ADDR1 168
-	#define configGW_ADDR2 0
-	#define configGW_ADDR3 1
-
-	/* MQTT broker address configuration. */
-	#define configBroker_ADDR0 192
-	#define configBroker_ADDR1 168
-	#define configBroker_ADDR2 0
-	#define configBroker_ADDR3 111
-#elif USE_HSLU
-	/* IP address configuration for board. */
-	#define configIP_ADDR0 10
-	#define configIP_ADDR1 9
-	#define configIP_ADDR2 4
-	#define configIP_ADDR3 99
-
-	/* Netmask configuration. */
-	#define configNET_MASK0 255
-	#define configNET_MASK1 255
-	#define configNET_MASK2 254
-	#define configNET_MASK3 0
-
-	/* Gateway address configuration. */
-	#define configGW_ADDR0 10
-	#define configGW_ADDR1 9
-	#define configGW_ADDR2 5
-	#define configGW_ADDR3 254
-
-	/* MQTT broker address configuration. */
-  /* https://test.mosquitto.org/: 37.184.106.16 */
-   /* EST machine: */
-	#define configBroker_ADDR0 10
-	#define configBroker_ADDR1 9
-	#define configBroker_ADDR2 4
-	#define configBroker_ADDR3 26
-#else
-	/* IP address configuration. */
-	#define configIP_ADDR0 192
-	#define configIP_ADDR1 168
-	#define configIP_ADDR2 0
-	#define configIP_ADDR3 150
-
-	/* Netmask configuration. */
-	#define configNET_MASK0 255
-	#define configNET_MASK1 255
-	#define configNET_MASK2 255
-	#define configNET_MASK3 0
-
-	/* Gateway address configuration. */
-	#define configGW_ADDR0 192
-	#define configGW_ADDR1 168
-	#define configGW_ADDR2 0
-	#define configGW_ADDR3 1
-#endif
-#if USE_MQTT
-  static mqtt_client_t mqtt_client;
-  static ip4_addr_t broker_ipaddr;
-
-  /* The idea is to demultiplex topic and create some reference to be used in data callbacks
-     Example here uses a global variable, better would be to use a member in arg
-     If RAM and CPU budget allows it, the easiest implementation might be to just take a copy of
-     the topic string and use it in mqtt_incoming_data_cb
-  */
-  static int inpub_id;
-  static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len) {
-    printf("Incoming publish at topic %s with total length %u\n", topic, (unsigned int)tot_len);
-
-    /* Decode topic string into a user defined reference */
-    if(strcmp(topic, "print_payload") == 0) {
-      inpub_id = 0;
-    } else if(topic[0] == 'A') {
-      /* All topics starting with 'A' might be handled at the same way */
-      inpub_id = 1;
-    } else {
-      /* For all other topics */
-      inpub_id = 2;
-    }
+  /* Decode topic string into a user defined reference */
+  if(strcmp(topic, "print_payload") == 0) {
+    inpub_id = 0;
+  } else if(topic[0] == 'A') {
+    /* All topics starting with 'A' might be handled at the same way */
+    inpub_id = 1;
+  } else {
+    /* For all other topics */
+    inpub_id = 2;
   }
+}
 
-  static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags) {
-    printf("Incoming publish payload with length %d, flags %u\n", len, (unsigned int)flags);
+static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags) {
+  printf("Incoming publish payload with length %d, flags %u\n", len, (unsigned int)flags);
 
-    if(flags & MQTT_DATA_FLAG_LAST) {
-      /* Last fragment of payload received (or whole part if payload fits receive buffer
-         See MQTT_VAR_HEADER_BUFFER_LEN)  */
+  if(flags & MQTT_DATA_FLAG_LAST) {
+    /* Last fragment of payload received (or whole part if payload fits receive buffer
+       See MQTT_VAR_HEADER_BUFFER_LEN)  */
 
-      /* Call function or do action depending on reference, in this case inpub_id */
-      if(inpub_id == 0) {
-        /* Don't trust the publisher, check zero termination */
-        if(data[len-1] == 0) {
-          printf("mqtt_incoming_data_cb: %s\n", (const char *)data);
-        }
-      } else if(inpub_id == 1) {
-        /* Call an 'A' function... */
-      } else {
-        printf("mqtt_incoming_data_cb: Ignoring payload...\n");
+    /* Call function or do action depending on reference, in this case inpub_id */
+    if(inpub_id == 0) {
+      /* Don't trust the publisher, check zero termination */
+      if(data[len-1] == 0) {
+        printf("mqtt_incoming_data_cb: %s\n", (const char *)data);
       }
+    } else if(inpub_id == 1) {
+      /* Call an 'A' function... */
     } else {
-      /* Handle fragmented payload, store in buffer, write to file or whatever */
+      printf("mqtt_incoming_data_cb: Ignoring payload...\n");
     }
+  } else {
+    /* Handle fragmented payload, store in buffer, write to file or whatever */
   }
+}
 
-  static void mqtt_sub_request_cb(void *arg, err_t result) {
-    /* Just print the result code here for simplicity,
-       normal behaviour would be to take some action if subscribe fails like
-       notifying user, retry subscribe or disconnect from server */
-    printf("Subscribe result: %d\n", result);
-  }
+static void mqtt_sub_request_cb(void *arg, err_t result) {
+  /* Just print the result code here for simplicity,
+     normal behaviour would be to take some action if subscribe fails like
+     notifying user, retry subscribe or disconnect from server */
+  printf("Subscribe result: %d\n", result);
+}
 
-  static void example_do_connect(mqtt_client_t *client);
+static void example_do_connect(mqtt_client_t *client); /* forward declaration */
 
-  static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status) {
-    err_t err;
-    if(status == MQTT_CONNECT_ACCEPTED) {
-      printf("mqtt_connection_cb: Successfully connected\n");
+static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status) {
+  err_t err;
 
-      /* Setup callback for incoming publish requests */
-      mqtt_set_inpub_callback(client, mqtt_incoming_publish_cb, mqtt_incoming_data_cb, arg);
+  if(status == MQTT_CONNECT_ACCEPTED) {
+    printf("mqtt_connection_cb: Successfully connected\n");
 
-      /* Subscribe to a topic named "subtopic" with QoS level 1, call mqtt_sub_request_cb with result */
-      err = mqtt_subscribe(client, "subtopic", 1, mqtt_sub_request_cb, arg);
+    /* Setup callback for incoming publish requests */
+    mqtt_set_inpub_callback(client, mqtt_incoming_publish_cb, mqtt_incoming_data_cb, arg);
 
-      if(err != ERR_OK) {
-        printf("mqtt_subscribe return: %d\n", err);
-      }
-    } else {
-      printf("mqtt_connection_cb: Disconnected, reason: %d\n", status);
+    /* Subscribe to a topic named "subtopic" with QoS level 1, call mqtt_sub_request_cb with result */
+    err = mqtt_subscribe(client, "subtopic", 1, mqtt_sub_request_cb, arg);
 
-      /* Its more nice to be connected, so try to reconnect */
-      example_do_connect(client);
-    }
-  }
-
-  static void example_do_connect(mqtt_client_t *client) {
-    struct mqtt_connect_client_info_t ci;
-    err_t err;
-
-    IP4_ADDR(&broker_ipaddr, configBroker_ADDR0, configBroker_ADDR1, configBroker_ADDR2, configBroker_ADDR3);
-
-    /* Setup an empty client info structure */
-    memset(&ci, 0, sizeof(ci));
-
-    /* Minimal amount of information required is client identifier, so set it here */
-    ci.client_id = "lwip_hslu";
-
-    /* Initiate client and connect to server, if this fails immediately an error code is returned
-       otherwise mqtt_connection_cb will be called with connection result after attempting
-       to establish a connection with the server.
-       For now MQTT version 3.1.1 is always used */
-
-    err = mqtt_client_connect(client, &broker_ipaddr, MQTT_PORT, mqtt_connection_cb, 0, &ci);
-
-    /* For now just print the result code if something goes wrong */
     if(err != ERR_OK) {
-      printf("mqtt_connect return %d\n", err);
+      printf("mqtt_subscribe return: %d\n", err);
     }
-  }
+  } else {
+    printf("mqtt_connection_cb: Disconnected, reason: %d\n", status);
 
-  /* Called when publish is complete either with sucess or failure */
-  static void mqtt_pub_request_cb(void *arg, err_t result) {
-    if(result != ERR_OK) {
-      printf("Publish result: %d\n", result);
-    }
+    /* Its more nice to be connected, so try to reconnect */
+    example_do_connect(client);
   }
+}
 
-  static void example_publish(mqtt_client_t *client, void *arg)
-  {
-    const char *pub_payload= "abcd";
-    err_t err;
-    u8_t qos = 2; /* 0 1 or 2, see MQTT specification */
-    u8_t retain = 0; /* No don't retain such crappy payload... */
-    err = mqtt_publish(client, "HSLU/test", pub_payload, strlen(pub_payload), qos, retain, mqtt_pub_request_cb, arg);
-    if(err != ERR_OK) {
-      printf("Publish err: %d\n", err);
-    }
+static void example_do_connect(mqtt_client_t *client) {
+  struct mqtt_connect_client_info_t ci;
+  err_t err;
+
+  IP4_ADDR(&broker_ipaddr, configBroker_ADDR0, configBroker_ADDR1, configBroker_ADDR2, configBroker_ADDR3);
+
+  /* Setup an empty client info structure */
+  memset(&ci, 0, sizeof(ci));
+
+  /* Minimal amount of information required is client identifier, so set it here */
+  ci.client_id = "lwip_hslu";
+
+  /* Initiate client and connect to server, if this fails immediately an error code is returned
+     otherwise mqtt_connection_cb will be called with connection result after attempting
+     to establish a connection with the server.
+     For now MQTT version 3.1.1 is always used */
+
+  err = mqtt_client_connect(client, &broker_ipaddr, MQTT_PORT, mqtt_connection_cb, 0, &ci);
+
+  /* For now just print the result code if something goes wrong */
+  if(err != ERR_OK) {
+    printf("mqtt_connect return %d\n", err);
   }
+}
 
-#endif
+/* Called when publish is complete either with sucess or failure */
+static void mqtt_pub_request_cb(void *arg, err_t result) {
+  if(result != ERR_OK) {
+    printf("Publish result: %d\n", result);
+  }
+}
+
+static void example_publish(mqtt_client_t *client, void *arg) {
+  const char *pub_payload= "abcd";
+  err_t err;
+  u8_t qos = 2; /* 0 1 or 2, see MQTT specification */
+  u8_t retain = 0; /* No don't retain such crappy payload... */
+  err = mqtt_publish(client, "HSLU/test", pub_payload, strlen(pub_payload), qos, retain, mqtt_pub_request_cb, arg);
+  if(err != ERR_OK) {
+    printf("Publish err: %d\n", err);
+  }
+}
 
 /*!
  * @brief Interrupt service for SysTick timer.
  */
-void SysTick_Handler(void)
-{
-    time_isr();
+void SysTick_Handler(void) {
+  time_isr();
 }
 
 static void MqttDoStateMachine(void) {
@@ -308,77 +280,68 @@ static void MqttDoStateMachine(void) {
 }
 
 static void DoMQTT(struct netif *netifp) {
-  bool doConnect = false;
-  bool doPublish = false;
-  bool doDisconnect = false;
+  uint32_t timeStampMs, diffTimeMs;
+  #define CONNECT_DELAY_MS   1000 /* after 1 second, connect */
+  #define PUBLISH_PERIOD_MS  2000 /* publish every 2 seconds */
 
   MQTT_state = MQTT_STATE_IDLE;
+  timeStampMs = sys_now(); /* get time in milli seconds */
   for(;;) {
-    if (MQTT_state==MQTT_STATE_CONNECTED && doConnect) {
-      MQTT_state = MQTT_STATE_DO_CONNECT; doConnect = false;
+    diffTimeMs = sys_now()-timeStampMs;
+    if (MQTT_state==MQTT_STATE_IDLE && diffTimeMs>CONNECT_DELAY_MS) {
+      MQTT_state = MQTT_STATE_DO_CONNECT; /* connect after 1 second */
+      timeStampMs = sys_now(); /* get time in milli seconds */
     }
-    if (MQTT_state==MQTT_STATE_CONNECTED && doPublish) {
-      MQTT_state = MQTT_STATE_DO_PUBLISH; doPublish = false;
-    }
-    if (MQTT_state==MQTT_STATE_CONNECTED && doDisconnect) {
-      MQTT_state = MQTT_STATE_DO_DISCONNECT; doDisconnect = false;
+    if (MQTT_state==MQTT_STATE_CONNECTED && diffTimeMs>=PUBLISH_PERIOD_MS) {
+      MQTT_state = MQTT_STATE_DO_PUBLISH; /* publish */
+      timeStampMs = sys_now(); /* get time in milli seconds */
     }
     MqttDoStateMachine(); /* process state machine */
     /* Poll the driver, get any outstanding frames */
     ethernetif_input(netifp);
+    sys_check_timeouts(); /* Handle all system timeouts for all core protocols */
   }
 }
 
 /*!
  * @brief Main function.
  */
-int main(void)
-{
-    struct netif fsl_netif0;
-    ip4_addr_t fsl_netif0_ipaddr, fsl_netif0_netmask, fsl_netif0_gw;
+int main(void) {
+  struct netif fsl_netif0;
+  ip4_addr_t fsl_netif0_ipaddr, fsl_netif0_netmask, fsl_netif0_gw;
 
-    SYSMPU_Type *base = SYSMPU;
-    BOARD_InitPins();
-    BOARD_BootClockRUN();
-    BOARD_InitDebugConsole();
-    /* Disable SYSMPU. */
-    base->CESR &= ~SYSMPU_CESR_VLD_MASK;
+  SYSMPU_Type *base = SYSMPU;
+  BOARD_InitPins();
+  BOARD_BootClockRUN();
+  BOARD_InitDebugConsole();
+  /* Disable SYSMPU. */
+  base->CESR &= ~SYSMPU_CESR_VLD_MASK;
 
-    time_init();
+  time_init();
 
-    IP4_ADDR(&fsl_netif0_ipaddr, configIP_ADDR0, configIP_ADDR1, configIP_ADDR2, configIP_ADDR3);
-    IP4_ADDR(&fsl_netif0_netmask, configNET_MASK0, configNET_MASK1, configNET_MASK2, configNET_MASK3);
-    IP4_ADDR(&fsl_netif0_gw, configGW_ADDR0, configGW_ADDR1, configGW_ADDR2, configGW_ADDR3);
+  IP4_ADDR(&fsl_netif0_ipaddr, configIP_ADDR0, configIP_ADDR1, configIP_ADDR2, configIP_ADDR3);
+  IP4_ADDR(&fsl_netif0_netmask, configNET_MASK0, configNET_MASK1, configNET_MASK2, configNET_MASK3);
+  IP4_ADDR(&fsl_netif0_gw, configGW_ADDR0, configGW_ADDR1, configGW_ADDR2, configGW_ADDR3);
 
-    lwip_init();
+  lwip_init();
 
-    netif_add(&fsl_netif0, &fsl_netif0_ipaddr, &fsl_netif0_netmask, &fsl_netif0_gw, NULL, ethernetif_init,
-              ethernet_input);
-    netif_set_default(&fsl_netif0);
-    netif_set_up(&fsl_netif0);
+  netif_add(&fsl_netif0, &fsl_netif0_ipaddr, &fsl_netif0_netmask, &fsl_netif0_gw, NULL, ethernetif_init, ethernet_input);
+  netif_set_default(&fsl_netif0);
+  netif_set_up(&fsl_netif0);
 
-    PRINTF("\r\n************************************************\r\n");
-    PRINTF(" PING BM example\r\n");
-    PRINTF("************************************************\r\n");
-    PRINTF(" IPv4 Address     : %u.%u.%u.%u\r\n", ((u8_t *)&fsl_netif0_ipaddr)[0], ((u8_t *)&fsl_netif0_ipaddr)[1],
-           ((u8_t *)&fsl_netif0_ipaddr)[2], ((u8_t *)&fsl_netif0_ipaddr)[3]);
-    PRINTF(" IPv4 Subnet mask : %u.%u.%u.%u\r\n", ((u8_t *)&fsl_netif0_netmask)[0], ((u8_t *)&fsl_netif0_netmask)[1],
-           ((u8_t *)&fsl_netif0_netmask)[2], ((u8_t *)&fsl_netif0_netmask)[3]);
-    PRINTF(" IPv4 Gateway     : %u.%u.%u.%u\r\n", ((u8_t *)&fsl_netif0_gw)[0], ((u8_t *)&fsl_netif0_gw)[1],
-           ((u8_t *)&fsl_netif0_gw)[2], ((u8_t *)&fsl_netif0_gw)[3]);
-    PRINTF("************************************************\r\n");
-#if USE_MQTT
-    DoMQTT(&fsl_netif0);
-#elif USE_PING
-    ping_init();
-#endif
+  PRINTF("\r\n************************************************\r\n");
+  PRINTF(" MQTT Bare Metal example\r\n");
+  PRINTF("************************************************\r\n");
+  PRINTF(" IPv4 Address     : %u.%u.%u.%u\r\n", ((u8_t *)&fsl_netif0_ipaddr)[0], ((u8_t *)&fsl_netif0_ipaddr)[1],
+         ((u8_t *)&fsl_netif0_ipaddr)[2], ((u8_t *)&fsl_netif0_ipaddr)[3]);
+  PRINTF(" IPv4 Subnet mask : %u.%u.%u.%u\r\n", ((u8_t *)&fsl_netif0_netmask)[0], ((u8_t *)&fsl_netif0_netmask)[1],
+         ((u8_t *)&fsl_netif0_netmask)[2], ((u8_t *)&fsl_netif0_netmask)[3]);
+  PRINTF(" IPv4 Gateway     : %u.%u.%u.%u\r\n", ((u8_t *)&fsl_netif0_gw)[0], ((u8_t *)&fsl_netif0_gw)[1],
+         ((u8_t *)&fsl_netif0_gw)[2], ((u8_t *)&fsl_netif0_gw)[3]);
+  PRINTF("************************************************\r\n");
 
-    while (1)
-    {
-        /* Poll the driver, get any outstanding frames */
-        ethernetif_input(&fsl_netif0);
-
-        sys_check_timeouts(); /* Handle all system timeouts for all core protocols */
-    }
+  DoMQTT(&fsl_netif0);
+  for(;;) {}
+  return 0;
 }
-#endif
+
