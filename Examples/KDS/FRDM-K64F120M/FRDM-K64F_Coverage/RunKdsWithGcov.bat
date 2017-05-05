6144db4601d9d11e0baf67ef6925242583ddb48d
Erich Styger <erich.styger@hslu.ch>
2017-05-05 15:05:36 +0200

    added more tools to make sure it runs with gcov

--- Examples/KDS/FRDM-K64F120M/FRDM-K64F_Coverage/RunKdsWithGcov.bat
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_Coverage/RunKdsWithGcov.bat
@@ -6,6 +6,8 @@ IF NOT EXIST "%KDS_PATH%\Toolchain\bin\addr2line.exe" COPY "%KDS_PATH%\Toolchain
 IF NOT EXIST "%KDS_PATH%\Toolchain\bin\nm.exe"        COPY "%KDS_PATH%\Toolchain\bin\arm-none-eabi-nm.exe"        "%KDS_PATH%\Toolchain\bin\nm.exe"
 IF NOT EXIST "%KDS_PATH%\Toolchain\bin\c++filt.exe"   COPY "%KDS_PATH%\Toolchain\bin\arm-none-eabi-c++filt.exe"   "%KDS_PATH%\Toolchain\bin\c++filt.exe"
 IF NOT EXIST "%KDS_PATH%\Toolchain\bin\strings.exe"   COPY "%KDS_PATH%\Toolchain\bin\arm-none-eabi-strings.exe"   "%KDS_PATH%\Toolchain\bin\strings.exe"
+IF NOT EXIST "%KDS_PATH%\Toolchain\bin\gprof.exe"     COPY "%KDS_PATH%\Toolchain\bin\arm-none-eabi-gprof.exe"     "%KDS_PATH%\Toolchain\bin\gprof.exe"
+IF NOT EXIST "%KDS_PATH%\Toolchain\bin\gdb.exe"       COPY "%KDS_PATH%\Toolchain\bin\arm-none-eabi-gdb.exe"       "%KDS_PATH%\Toolchain\bin\gdb.exe"
 
 ECHO Extending PATH
 SET PATH=%PATH%;%KDS_PATH%\Toolchain\bin
