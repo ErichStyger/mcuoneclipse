94a3240aef66352fdcc3fa5928e9526312bb08c6
Erich Styger <erich.styger@hslu.ch>
2017-06-10 18:00:22 +0200

    updated projects for latest components

--- Examples/KDS/FRDM-KE02Z/FRDM-KE02Z_FatFS/Sources/main.c
+++ Examples/KDS/FRDM-KE02Z/FRDM-KE02Z_FatFS/Sources/main.c
@@ -32,7 +32,6 @@
 #include "Events.h"
 #include "KIN1.h"
 #include "UTIL1.h"
-#include "KSDK1.h"
 #include "WAIT1.h"
 #include "LED1.h"
 #include "LEDpin1.h"
@@ -59,6 +58,7 @@
 #include "AS1.h"
 #include "ASerialLdd1.h"
 #include "XF1.h"
+#include "MCUC1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
