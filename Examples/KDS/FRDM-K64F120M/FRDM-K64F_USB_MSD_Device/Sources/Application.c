0824d261d842538d2d8cb20d8ea3f66bd6983700
Erich Styger <erich.styger@hslu.ch>
2017-06-13 06:38:57 +0200

    updated projects

--- Examples/KDS/FRDM-K64F120M/FRDM-K64F_USB_MSD_Device/Sources/Application.c
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_USB_MSD_Device/Sources/Application.c
@@ -6,12 +6,31 @@
 #include "FRTOS1.h"
 #include "Shell.h"
 #include "PORT_PDD.h"
+#include "Inhr1.h"
+#include "USB1.h"
+#include "USB0.h"
+#include "FATM1.h"
+
+static void AppTask(void *pvParameters) {
+  (void)pvParameters; /* not used */
+  vTaskDelay(1000/portTICK_RATE_MS);
+  FATM1_disk_initialize(0);
+  USB1_Init();
+  for(;;) {
+    Inhr1_App_Task();
+    vTaskDelay(50/portTICK_RATE_MS);
+  }
+}
+
 
 void APP_Run(void) {
   /* SD card detection: PTE6 with pull-down! */
   PORT_PDD_SetPinPullSelect(PORTE_BASE_PTR, 6, PORT_PDD_PULL_DOWN);
   PORT_PDD_SetPinPullEnable(PORTE_BASE_PTR, 6, PORT_PDD_PULL_ENABLE);
   SHELL_Init();
+  if (xTaskCreate(AppTask, (signed portCHAR *)"App", configMINIMAL_STACK_SIZE+400, NULL, tskIDLE_PRIORITY+2, NULL) != pdPASS) {
+    for(;;){} /* error */
+  }
   vTaskStartScheduler();
 }
 
--- Examples/KDS/FRDM-K64F120M/FRDM-K64F_USB_MSD_Device/Sources/Shell.c
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_USB_MSD_Device/Sources/Shell.c
@@ -10,8 +10,7 @@
 #include "LEDR.h"
 #include "LEDG.h"
 #include "FAT1.h"
-
-
+#include "RTT1.h"
 
 static const CLS1_ParseCommandCallback CmdParserTable[] =
 {
@@ -37,25 +36,47 @@ static const CLS1_ParseCommandCallback CmdParserTable[] =
   NULL /* sentinel */
 };
 
+typedef struct {
+  CLS1_ConstStdIOType *stdio;
+  unsigned char *buf;
+  size_t bufSize;
+} SHELL_IODesc;
+
+#define PL_CONFIG_HAS_SHELL_EXTRA_RTT  1  /* RTT communication channel */
+
+static const SHELL_IODesc ios[] =
+{
+  {&CLS1_stdio, CLS1_DefaultShellBuffer, sizeof(CLS1_DefaultShellBuffer)},
+#if PL_CONFIG_HAS_SHELL_EXTRA_RTT
+  {&RTT1_stdio, RTT1_DefaultShellBuffer, sizeof(RTT1_DefaultShellBuffer)},
+#endif
+};
+
 static void ShellTask(void *pvParameters) {
+  unsigned int i;
   bool cardMounted = FALSE;
   static FAT1_FATFS fileSystemObject;
   unsigned char buf[48];
 
   (void)pvParameters; /* not used */
+  for(i=0;i<sizeof(ios)/sizeof(ios[0]);i++) {
+    ios[i].buf[0] = '\0';
+  }
   buf[0] = '\0';
   (void)CLS1_ParseWithCommandTable((unsigned char*)CLS1_CMD_HELP, CLS1_GetStdio(), CmdParserTable);
   for(;;) {
     (void)FAT1_CheckCardPresence(&cardMounted,
         0 /* volume */, &fileSystemObject, CLS1_GetStdio());
-    (void)CLS1_ReadAndParseWithCommandTable(buf, sizeof(buf), CLS1_GetStdio(), CmdParserTable);
+    /* process all I/Os */
+    for(i=0;i<sizeof(ios)/sizeof(ios[0]);i++) {
+      (void)CLS1_ReadAndParseWithCommandTable(ios[i].buf, ios[i].bufSize, ios[i].stdio, CmdParserTable);
+    }
     LEDG_Neg();
-    FRTOS1_vTaskDelay(50/portTICK_RATE_MS);
+    vTaskDelay(50/portTICK_RATE_MS);
   }
 }
 
 void SHELL_Init(void) {
-  CLS1_Init();
   if (xTaskCreate(ShellTask, (signed portCHAR *)"Shell", configMINIMAL_STACK_SIZE+400, NULL, tskIDLE_PRIORITY+1, NULL) != pdPASS) {
     for(;;){} /* error */
   }
