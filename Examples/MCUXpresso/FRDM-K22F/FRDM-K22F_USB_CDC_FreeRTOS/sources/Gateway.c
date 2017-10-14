896662236cff44c776cb9e316d3eeb7f5c5be840
Erich Styger <erich.styger@hslu.ch>
2017-10-14 08:55:16 +0200

    more cleanup

--- Examples/MCUXpresso/FRDM-K22F/FRDM-K22F_USB_CDC_FreeRTOS/sources/Gateway.c
+++ Examples/MCUXpresso/FRDM-K22F/FRDM-K22F_USB_CDC_FreeRTOS/sources/Gateway.c
@@ -12,57 +12,55 @@
 #include "fsl_debug_console.h"
 #include "McuUtility.h"
 
-#define UART_USED    UART2  /* UART for GPS */
 #define UART_ENABLE_GPS_RX  (1) /* 1 to enable Rx emulation (Rx from GPS) */
 #define UART_ENABLE_GPS_TX  (1) /* 1 to enable Tx emulation (Tx to GPS) */
 
 static uint8_t rxRingBuffer[512]; /* ring buffer for rx data */
 
 typedef struct {
-  UART_Type *uart;
-  uart_handle_t g_uartHandle;
+  UART_Type *uart; /* pointer to UART hardware used */
+  uart_handle_t *sdk_handle; /* SDK UART device handle */
+  /* following fields are used for communication handshaking with the user callback */
   bool rxBufferEmpty;
   bool txBufferFull;
   bool txOnGoing;
   bool rxOnGoing;
   int nofRxOverrunErrors;
   int nofRxRingBufferOverrunErrors;
-} UART_UserDataDesc;
-
-static UART_UserDataDesc UART_UserData; /* user data structure, used for status */
-static uart_handle_t g_uartHandle; /* UART device handle struct */
+} UART_MyDeviceDesc;
 
 static void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, void *userData) {
-  UART_UserDataDesc *data = (UART_UserDataDesc*)userData;
+  UART_MyDeviceDesc *device = (UART_MyDeviceDesc*)userData;
 
-  if (userData==NULL) {
+  if (userData==NULL || handle->userData!=userData) {
     return; /* something is really wrong here? */
   }
   if (status==kStatus_UART_RxHardwareOverrun) {
-    data->nofRxOverrunErrors++;
-    UART_ClearStatusFlags(UART_USED, kUART_RxOverrunFlag); /* clear error */
-    data->rxOnGoing = FALSE;
+    device->nofRxOverrunErrors++;
+    UART_ClearStatusFlags(device->uart, kUART_RxOverrunFlag); /* clear error */
+    device->rxOnGoing = FALSE;
   }
   if (status==kStatus_UART_RxRingBufferOverrun) { /* overrun of ring buffer */
-    data->nofRxRingBufferOverrunErrors++;
-    UART_TransferAbortReceive(UART_USED, handle); /* abort the current transfer */
-    data->rxOnGoing = FALSE;
+    device->nofRxRingBufferOverrunErrors++;
+    UART_TransferAbortReceive(device->uart, handle); /* abort the current transfer */
+    device->rxOnGoing = FALSE;
   }
   if (status==kStatus_UART_TxIdle) { /* hardware finished Tx */
-    data->txBufferFull = false; /* indicate that Tx buffer is ready for new data */
-    data->txOnGoing = false; /* reset Tx flag */
+    device->txBufferFull = false; /* indicate that Tx buffer is ready for new data */
+    device->txOnGoing = false; /* reset Tx flag */
   }
   if (status==kStatus_UART_RxIdle)   { /* hardware finished Rx */
-    data->rxBufferEmpty = false; /* indicate that we have something in the Rx buffer */
-    data->rxOnGoing = false; /* reset Rx flag */
+    device->rxBufferEmpty = false; /* indicate that we have something in the Rx buffer */
+    device->rxOnGoing = false; /* reset Rx flag */
   }
 }
 
-static void InitUART(UART_UserDataDesc *userData) {
+static void InitUART(uart_handle_t *sdkHandle, UART_MyDeviceDesc *userData) {
   uart_config_t config; /* user configuration */
 
   /* initialize user data descriptor */
-  userData->uart = UART_USED;
+  userData->uart = UART2;
+  userData->sdk_handle = sdkHandle;
   userData->rxBufferEmpty = true;
   userData->txBufferFull = false;
   userData->txOnGoing = false;
@@ -87,20 +85,22 @@ static void InitUART(UART_UserDataDesc *userData) {
   config.enableRx = true;
   UART_Init(userData->uart, &config, CLOCK_GetFreq(SYS_CLK)/2);
 
-  UART_TransferCreateHandle(userData->uart, &g_uartHandle, UART_UserCallback, userData);
-  UART_TransferStartRingBuffer(userData->uart, &g_uartHandle, rxRingBuffer, sizeof(rxRingBuffer));
+  UART_TransferCreateHandle(userData->uart, userData->sdk_handle, UART_UserCallback, userData);
+  UART_TransferStartRingBuffer(userData->uart, userData->sdk_handle, rxRingBuffer, sizeof(rxRingBuffer));
 }
 
-static void UartSendStr(UART_UserDataDesc *userData, uart_transfer_t *txTransfer, uint8_t *txBuf, size_t txBufSize, const uint8_t *str) {
+static void UartSendStr(UART_MyDeviceDesc *device, uart_transfer_t *txTransfer, uint8_t *txBuf, size_t txBufSize, const uint8_t *str) {
   McuUtility_strcpy(txBuf, txBufSize, str); /* copy string into buffer */
   /* setup transfer descriptor */
   txTransfer->data = txBuf; /* pointer to data */
   txTransfer->dataSize = McuUtility_strlen((char*)txBuf); /* number of bytes */
-  UART_UserData.txOnGoing = true; /* flag will be reset by callback */
-  UART_TransferSendNonBlocking(userData->uart, &g_uartHandle, txTransfer);
+  device->txOnGoing = true; /* flag will be reset by callback */
+  UART_TransferSendNonBlocking(device->uart, device->sdk_handle, txTransfer);
 }
 
 static void UartTask(void *pvParams) {
+  static UART_MyDeviceDesc myDevice; /* user data structure, used for status */
+  static uart_handle_t sdkUartHandle; /* SDK UART device handle struct */
   status_t status;
   uint32_t receivedBytes;
   uint32_t counterMs = 0;
@@ -123,18 +123,19 @@ static void UartTask(void *pvParams) {
   txTransfer.data = &txBuf[0];
   txTransfer.dataSize = sizeof(txBuf);
 
+  InitUART(&sdkUartHandle, &myDevice);
   for(;;) {
 #if UART_ENABLE_GPS_RX
-    if (!UART_UserData.rxOnGoing) { /* call UART_TransferReceiveNonBlocking() only if there is no other transfer pending */
+    if (!myDevice.rxOnGoing) { /* call UART_TransferReceiveNonBlocking() only if there is no other transfer pending */
       /* check if we have something already in the Rx buffer */
-      if (UART_UserData.rxBufferEmpty) {
+      if (myDevice.rxBufferEmpty) {
         /* buffer is empty: start new transfer and receive new data into buffer */
-        UART_UserData.rxOnGoing = true;
+        myDevice.rxOnGoing = true;
         /* start transaction for a single byte */
-         status = UART_TransferReceiveNonBlocking(UART_USED, &g_uartHandle, &rxTransfer, &receivedBytes); /* this enables the UART interrupts and triggers receiving data */
+         status = UART_TransferReceiveNonBlocking(myDevice.uart, myDevice.sdk_handle, &rxTransfer, &receivedBytes); /* this enables the UART interrupts and triggers receiving data */
          if (status==kStatus_UART_RxBusy) {
-           UART_TransferAbortReceive(UART_USED, &g_uartHandle); /* abort the current transfer */
-           UART_UserData.rxOnGoing = FALSE;
+           UART_TransferAbortReceive(myDevice.uart, myDevice.sdk_handle); /* abort the current transfer */
+           myDevice.rxOnGoing = FALSE;
          }
       } else {
          /* rxBuffer is full. Get the number of bytes stored in the transfer buffer */
@@ -146,15 +147,15 @@ static void UartTask(void *pvParams) {
           DbgConsole_Putchar(rxTransfer.data[i]);
         }
         /* initiate a new transfer */
-        UART_UserData.rxBufferEmpty = true; /* ready to get new data */
-        status = UART_TransferReceiveNonBlocking(UART_USED, &g_uartHandle, &rxTransfer, &receivedBytes);
+        myDevice.rxBufferEmpty = true; /* ready to get new data */
+        status = UART_TransferReceiveNonBlocking(myDevice.uart, myDevice.sdk_handle, &rxTransfer, &receivedBytes);
         /* stay inside while loop as long as we get more data */
       }
    }
 #endif
 #if UART_ENABLE_GPS_TX
     if ((counterMs%1000)==0) { /* every second */
-      if (!UART_UserData.txOnGoing) {
+      if (!myDevice.txOnGoing) {
         switchBaudCntr++;
 #if 0
         if (switchBaudCntr==10) {
@@ -164,8 +165,8 @@ static void UartTask(void *pvParams) {
            /* setup transfer */
            txTransfer.data = &txBuf[0];
            txTransfer.dataSize = McuUtility_strlen((char*)txBuf);
-           UART_UserData.txOnGoing = true; /* flag will be reset by callback */
-           UART_TransferSendNonBlocking(UART_USED, &g_uartHandle, &txTransfer);
+           myDevice.txOnGoing = true; /* flag will be reset by callback */
+           UART_TransferSendNonBlocking(myDevice.uart, &g_uartHandle, &txTransfer);
         } else if (switchBaudCntr==20) {
           /* send data to the GPS UART */
            DbgConsole_Printf("Tx to GPS...\n");
@@ -173,8 +174,8 @@ static void UartTask(void *pvParams) {
            /* setup transfer */
            txTransfer.data = &txBuf[0];
            txTransfer.dataSize = McuUtility_strlen((char*)txBuf);
-           UART_UserData.txOnGoing = true; /* flag will be reset by callback */
-           UART_TransferSendNonBlocking(UART_USED, &g_uartHandle, &txTransfer);
+           myDevice.txOnGoing = true; /* flag will be reset by callback */
+           UART_TransferSendNonBlocking(myDevice.uart, &g_uartHandle, &txTransfer);
            switchBaudCntr = 0; /* reset */
         } else {
 #endif
@@ -184,8 +185,8 @@ static void UartTask(void *pvParams) {
           /* setup transfer */
           txTransfer.data = &txBuf[0];
           txTransfer.dataSize = McuUtility_strlen((char*)txBuf);
-          UART_UserData.txOnGoing = true; /* flag will be reset by callback */
-          UART_TransferSendNonBlocking(UART_USED, &g_uartHandle, &txTransfer);
+          myDevice.txOnGoing = true; /* flag will be reset by callback */
+          UART_TransferSendNonBlocking(myDevice.uart, myDevice.sdk_handle, &txTransfer);
  //       }
       }
     }
@@ -196,7 +197,6 @@ static void UartTask(void *pvParams) {
 }
 
 void GW_Init(void) {
-  InitUART(&UART_UserData);
   if (xTaskCreate(UartTask,                         /* pointer to the task                      */
                   "UartTask",                       /* task name for kernel awareness debugging */
                   1024L/sizeof(portSTACK_TYPE),  /* task stack size                          */
