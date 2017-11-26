fd66e3b98e50a215f4f7264fcbd200219f9105a1
Erich Styger <erich.styger@hslu.ch>
2017-11-26 08:14:37 +0100

    added demo project for TSL2561 ambient light sensor

---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_TSL2561/Sources/Events.h
@@ -0,0 +1,128 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : FRDM-K64F_TSL2561
+**     Processor   : MK64FN1M0VLL12
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2017-11-15, 14:34, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Contents    :
+**         Cpu_OnNMI - void Cpu_OnNMI(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "TSL1.h"
+#include "GI2C1.h"
+#include "MCUC1.h"
+#include "WAIT1.h"
+#include "UTIL1.h"
+#include "I2C1.h"
+#include "SDA1.h"
+#include "BitIoLdd1.h"
+#include "SCL1.h"
+#include "BitIoLdd2.h"
+#include "CI2C1.h"
+#include "IntI2cLdd1.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd3.h"
+#include "LED2.h"
+#include "LEDpin2.h"
+#include "BitIoLdd4.h"
+#include "LED3.h"
+#include "LEDpin3.h"
+#include "BitIoLdd5.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMI (module Events)
+**
+**     Component   :  Cpu [MK64FN1M0LQ12]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMI(void);
+
+
+/*
+** ===================================================================
+**     Event       :  CI2C1_OnReceiveData (module Events)
+**
+**     Component   :  CI2C1 [InternalI2C]
+**     Description :
+**         This event is invoked when I2C finishes the reception of the
+**         data successfully. This event is not available for the SLAVE
+**         mode and if both RecvChar and RecvBlock are disabled. This
+**         event is enabled only if interrupts/events are enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void CI2C1_OnReceiveData(void);
+
+/*
+** ===================================================================
+**     Event       :  CI2C1_OnTransmitData (module Events)
+**
+**     Component   :  CI2C1 [InternalI2C]
+**     Description :
+**         This event is invoked when I2C finishes the transmission of
+**         the data successfully. This event is not available for the
+**         SLAVE mode and if both SendChar and SendBlock are disabled.
+**         This event is enabled only if interrupts/events are enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void CI2C1_OnTransmitData(void);
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_TSL2561/readme.txt
@@ -0,0 +1,3 @@
+readme.txt
+----------
+Demo program using the TSL2561 ambient light sensor with I2C and Kinetis Design Studio 3.2.0.
