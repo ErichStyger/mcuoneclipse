df64fff59e152114f3c0ebd0dc0b45c0bed2813e
Erich Styger <erich.styger@hslu.ch>
2018-06-25 11:59:24 -0700

    updated and changed linker file

--- Examples/KDS/tinyK22/tinyK22_MPU/Sources/Application.c
+++ Examples/KDS/tinyK22/tinyK22_MPU/Sources/Application.c
@@ -9,6 +9,29 @@
 #include "FRTOS1.h"
 #include "LED1.h"
 
+#if configUSE_MPU_SUPPORT
+BaseType_t xPortRaisePrivilege( void ) {
+
+}
+
+
+void vPortStoreTaskMPUSettings( xMPU_SETTINGS *xMPUSettings, const struct xMEMORY_REGION * const xRegions, StackType_t *pxBottomOfStack, uint32_t ulStackDepth ) PRIVILEGED_FUNCTION {
+  if (xRegions==NULL) {
+    int ul;
+    /* no MPU regions are defined, so allow access to everything */
+    xMPUSettings->xRegion[0].ulRegionBaseAddress  = 0x08000000;
+    xMPUSettings->xRegion[0].ulRegionAttribute = portMPU_REGION_READ_WRITE;
+    /* Invalidate all other regions.   */
+    for(ul = 1; ul < portNUM_CONFIGURABLE_REGIONS; ul++) {
+      xMPUSettings->xRegion[ul].ulRegionBaseAddress = 0x00000000UL;
+      xMPUSettings->xRegion[ul].ulRegionAttribute = 0UL;
+    }
+   } else {
+
+  }
+}
+#endif
+
 static void AppTask(void *param) {
   (void)param;
   for(;;) {
---
+++ Examples/KDS/tinyK22/tinyK22_MPU/readme.txt
@@ -0,0 +1,8 @@
+readme.txt
+----------
+
+This project implements FreeRTOS with MPU for the FRDM-K64F board (ARM Cortex-M4).
+
+
+Note:
+priviledged_data is placed in a special way in the linker file.
