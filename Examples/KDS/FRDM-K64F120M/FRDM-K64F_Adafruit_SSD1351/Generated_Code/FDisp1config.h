f19c799cc54184c9b1e184bd977745351f587526
Erich Styger <erich.styger@hslu.ch>
2018-07-26 14:53:39 +0200

    initial state for porting

---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_Adafruit_SSD1351/Generated_Code/FDisp1config.h
@@ -0,0 +1,6 @@
+#ifndef __FDisp1_CONFIG_H
+#define __FDisp1_CONFIG_H
+
+/* no configuration supported yet */
+
+#endif /* __FDisp1_CONFIG_H */
---
+++ Examples/KDS/FRDM-K64F120M/FRDM-K64F_Adafruit_SSD1351/Generated_Code/UTIL1config.h
@@ -0,0 +1,13 @@
+/**
+ * \file
+ * \brief Configuration header file for Utility
+ *
+ * This header file is used to configure settings of the Utility module.
+ */
+
+#ifndef __UTIL1_CONFIG_H
+#define __UTIL1_CONFIG_H
+
+/* no configuration supported yet */
+
+#endif /* __UTIL1_CONFIG_H */
