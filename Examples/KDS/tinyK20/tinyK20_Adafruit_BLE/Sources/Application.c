816b897348de62c04e9ff222fee4e03c19833097
Erich Styger <erich.styger@hslu.ch>
2018-10-17 14:11:43 +0200

    turning on UART Rx pull-up resistors if having a tinyK20 with OpenSDA firmware on the other side: the OpenSDA will not pull up the line so the line will be floating. Thus might inject noise into the Rx line if not having a defined logic level.

--- Examples/KDS/tinyK20/tinyK20_Adafruit_BLE/Sources/Application.c
+++ Examples/KDS/tinyK20/tinyK20_Adafruit_BLE/Sources/Application.c
@@ -12,8 +12,12 @@
 #if PL_CONFIG_BLEUART_CMDMODE_ENABLED
   #include "bleuart_cmdmode.h"
 #endif
+#include "PORT_PDD.h"
 
 void APP_Run(void) {
+  /* need pull-up on UART Rx pin (PTC3) */
+  PORT_PDD_SetPinPullSelect(PORTC_BASE_PTR, 3, PORT_PDD_PULL_UP);
+  PORT_PDD_SetPinPullEnable(PORTC_BASE_PTR, 3, PORT_PDD_PULL_ENABLE);
   SHELL_Init();
 #if PL_CONFIG_BLEUART_CMDMODE_ENABLED
   BLEUART_CMDMODE_Init();
