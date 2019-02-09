419f7426cb3c51c44d9c3dfb1d98cc56dcd57f72
Erich Styger <erich.styger@hslu.ch>
2019-02-09 07:08:02 +0100

    added UART project

---
+++ Examples/MCUXpresso/tinyK22/tinyK22_UART_SDK/source/main.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2016-2018 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+/**
+ * @file    MK22FN512xxx12_Project_Test.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK22F51212.h"
+#include "fsl_debug_console.h"
+/* TODO: insert other include files here. */
+
+/* TODO: insert other definitions and declarations here. */
+#define USE_UART_POLLING (1)
+
+#if USE_UART_POLLING
+  #include "fsl_lpuart.h"
+
+  static uint8_t txbuff[] = "Lpuart polling example\r\nBoard will send back received characters\r\n";
+  static uint8_t rxbuff[20] = {0};
+  uint8_t ch;
+  lpuart_config_t config;
+  #define BOARD_LUART_BAUDRATE   (115200)
+  #define DEMO_LPUART LPUART0
+  #define DEMO_LPUART_CLKSRC    kCLOCK_PllFllSelClk
+  #define DEMO_LPUART_CLK_FREQ  CLOCK_GetFreq(kCLOCK_PllFllSelClk)
+#endif
+
+/*
+ * @brief   Application entry point.
+ */
+int main(void) {
+  	/* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+
+    BOARD_InitDebugConsole(); /* Init FSL debug console. used for printf() */
+    PRINTF("Hello World\n");
+
+#if USE_UART_POLLING
+    CLOCK_SetLpuartClock(1U); /* 1: MCGFLLCLK , or MCGPLLCLK , or IRC48M clock as selected by SOPT2[PLLFLLSEL]. */
+    /*
+     * config.baudRate_Bps = 115200U;
+     * config.parityMode = kLPUART_ParityDisabled;
+     * config.stopBitCount = kLPUART_OneStopBit;
+     * config.txFifoWatermark = 0;
+     * config.rxFifoWatermark = 0;
+     * config.enableTx = false;
+     * config.enableRx = false;
+     */
+    LPUART_GetDefaultConfig(&config);
+    config.baudRate_Bps = BOARD_LUART_BAUDRATE;
+    config.enableTx = true;
+    config.enableRx = true;
+
+    LPUART_Init(DEMO_LPUART, &config, DEMO_LPUART_CLK_FREQ);
+
+    LPUART_WriteBlocking(DEMO_LPUART, txbuff, sizeof(txbuff) - 1);
+    while (1)
+    {
+        LPUART_ReadBlocking(DEMO_LPUART, &ch, 1);
+        LPUART_WriteBlocking(DEMO_LPUART, &ch, 1);
+    }
+#endif
+    /* Force the counter to be placed into memory. */
+    volatile static int i = 0 ;
+    /* Enter an infinite loop, just incrementing a counter. */
+    while(1) {
+        i++ ;
+    }
+    return 0 ;
+}
