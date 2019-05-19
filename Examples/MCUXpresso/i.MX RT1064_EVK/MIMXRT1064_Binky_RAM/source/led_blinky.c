e75e72d830908df3e37ca3d0c9ba8a553ca63aed
Erich Styger <erich.styger@hslu.ch>
2019-05-19 09:02:42 +0200

    added two more RT1064 projects

---
+++ Examples/MCUXpresso/i.MX RT1064_EVK/MIMXRT1064_Binky_RAM/source/led_blinky.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "board.h"
+#include "fsl_gpio.h"
+
+#include "pin_mux.h"
+#include "system_MIMXRT1064.h"
+#include "clock_config.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define EXAMPLE_LED_GPIO BOARD_USER_LED_GPIO
+#define EXAMPLE_LED_GPIO_PIN BOARD_USER_LED_GPIO_PIN
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+volatile uint32_t g_systickCounter;
+/* The PIN status */
+volatile bool g_pinSet = false;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+void SysTick_Handler(void)
+{
+    if (g_systickCounter != 0U)
+    { 
+        g_systickCounter--;
+    }
+}
+
+void SysTick_DelayTicks(uint32_t n)
+{
+    g_systickCounter = n;
+    while(g_systickCounter != 0U)
+    {
+    }
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    /* Define the init structure for the output LED pin*/
+    gpio_pin_config_t led_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
+
+    /* Board pin init */
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+    /* Update the core clock */
+    SystemCoreClockUpdate();
+
+    /* Init output LED GPIO. */
+    GPIO_PinInit(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, &led_config);
+
+    /* Set systick reload value to generate 1ms interrupt */
+    if(SysTick_Config(SystemCoreClock / 1000U))
+    {
+        while(1)
+        {
+        }
+    }
+
+    while (1)
+    {
+        /* Delay 1000 ms */
+        SysTick_DelayTicks(1000U);
+        if (g_pinSet)
+        {
+            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 0U);
+            g_pinSet = false;
+        }
+        else
+        {
+            GPIO_PinWrite(EXAMPLE_LED_GPIO, EXAMPLE_LED_GPIO_PIN, 1U);
+            g_pinSet = true;
+        }
+    }
+}
---
+++ Examples/MCUXpresso/i.MX RT1064_EVK/MIMXRT1064_Blinky_FLASH/doc/readme.txt
@@ -0,0 +1,35 @@
+Overview
+========
+The LED Blinky demo application provides a sanity check for the new SDK build environments and board bring up. The LED Blinky demo 
+uses the systick interrupt to realize the function of timing delay. The example takes turns to shine the LED. The purpose of this 
+demo is to provide a simple project for debugging and further development.
+
+Toolchain supported
+===================
+- Keil MDK 5.25
+- IAR embedded Workbench 8.30.1
+- GCC ARM Embedded 7-2017-q4-major
+- MCUXpresso10.2.1
+
+Hardware requirements
+=====================
+- Mini/micro USB cable
+- EVK-MIMXRT1064 board
+- Personal Computer
+
+Board settings
+==============
+No special settings are required.
+
+Prepare the Demo
+================
+1. Connect a USB cable between the host PC and the OpenSDA USB port on the target board. 
+2. Download the program to the target board.
+3. Either press the reset button on your board or launch the debugger in your IDE to begin running the demo.
+
+Running the demo
+================
+When the demo runs successfully, you will find the LED is blinking.
+Customization options
+=====================
+
