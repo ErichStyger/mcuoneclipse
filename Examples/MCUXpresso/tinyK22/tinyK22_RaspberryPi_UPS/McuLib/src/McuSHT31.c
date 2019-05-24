923ca68d2204ecfeda78ad854abb515a4a1a316b
Erich Styger <erich.styger@hslu.ch>
2019-05-24 11:14:46 +0200

    making SHT31 driver common

--- Examples/MCUXpresso/tinyK22/tinyK22_RaspberryPi_UPS/McuLib/src/McuSHT31.c
+++ Examples/MCUXpresso/tinyK22/tinyK22_RaspberryPi_UPS/McuLib/src/McuSHT31.c
@@ -5,21 +5,12 @@
  *      Author: Erich Styger
  */
 
-#ifndef SOURCES_SHT31_C_
-#define SOURCES_SHT31_C_
-
-
-#include "platform.h"
-#if PL_CONFIG_USE_SHT31
-#include "SHT31.h"
+#include "McuSHT31config.h"
+#include "McuSHT31.h"
 #include "McuGenericI2C.h"
 #include "McuUtility.h"
 #include <stddef.h> /* for NULL */
 
-#define SHT31_I2C_ADDR  (0x44) /* default I2C address with ADDR pin pulled LOW (default on Adafruit board) */
-//#define SHT31_I2C_ADDR  (0x45) /* I2C address with ADDR pin pulled HIGH: pull ADR pin to VIN */
-
-#define SHT31_DEFAULT_ADDR           0x44
 #define SHT31_MEAS_HIGHREP_STRETCH 0x2C06
 #define SHT31_MEAS_MEDREP_STRETCH  0x2C0D
 #define SHT31_MEAS_LOWREP_STRETCH  0x2C10
@@ -37,14 +28,14 @@
 #define SHT31_STATUS_HEATER_ON          (1<<13) /* 0: heater OFF, 1: heater ON */
 #define SHT31_STATUS_RH_TRACKING_ALERT  (1<<11) /* 0: no RH tracking alert, 1: alert */
 #define SHT31_STATUS_T_TRACKING_ALERT   (1<<10) /* 0: no T tracking alert, 1: alert */
-#define SHT31_STATUS_SYSTEM_RESET       (1<<4)  /* 0: no reset detected sincd last clear status register command, 1: reset detected (hard reset, soft reset or supply fail */
+#define SHT31_STATUS_SYSTEM_RESET       (1<<4)  /* 0: no reset detected since last clear status register command, 1: reset detected (hard reset, soft reset or supply fail */
 #define SHT31_STATUS_CMD_STATUS         (1<<1)  /* 0: last command executed successfully; 1: last command not processed, it was either invalid, failed the internal command checksum */
 #define SHT31_STATUS_WRITE_CRC_STATUS   (1<<0)  /* 0: checksum of last write transfer was correct, 1: checksum of last write transfer failed */
 
 #define SHT31_CRC8_POLYNOMIAL  0x31    /* Seed for CRC polynomial */
 #define SHT31_CRC8_INIT        0xFF    /* Init value for CRC */
 
-uint8_t SHT31_GenerateCRC(uint8_t *data, uint8_t datalen) {
+static uint8_t SHT31_GenerateCRC(uint8_t *data, uint8_t datalen) {
   /* calculates 8-Bit checksum with given polynomial */
   uint8_t crc = SHT31_CRC8_INIT;
   uint8_t b;
@@ -54,7 +45,7 @@ uint8_t SHT31_GenerateCRC(uint8_t *data, uint8_t datalen) {
     crc ^= data[i];
     for (b=0; b<8; b++) {
       if (crc & 0x80) {
-        crc = (crc << 1) ^ SHT31_CRC8_POLYNOMIAL;
+        crc = (crc<<1)^SHT31_CRC8_POLYNOMIAL;
       } else {
         crc <<= 1;
       }
@@ -63,12 +54,12 @@ uint8_t SHT31_GenerateCRC(uint8_t *data, uint8_t datalen) {
   return crc;
 }
 
-uint8_t SHT31_WriteCommand(uint16_t cmd) {
+static uint8_t SHT31_WriteCommand(uint16_t cmd) {
   uint8_t buf[2];
 
   buf[0] = cmd>>8;
   buf[1] = cmd&0xff;
-  return McuGenericI2C_WriteAddress(SHT31_I2C_ADDR, buf, sizeof(buf), NULL, 0);
+  return McuGenericI2C_WriteAddress(McuSHT31_I2C_ADDR, buf, sizeof(buf), NULL, 0);
 }
 
 uint8_t SHT31_ReadStatus(uint16_t *status) {
@@ -79,7 +70,7 @@ uint8_t SHT31_ReadStatus(uint16_t *status) {
 
   cmd[0] = SHT31_READSTATUS>>8;
   cmd[1] = SHT31_READSTATUS&0xff;
-  res = McuGenericI2C_ReadAddress(SHT31_I2C_ADDR, cmd, sizeof(cmd), stat, sizeof(stat));
+  res = McuGenericI2C_ReadAddress(McuSHT31_I2C_ADDR, cmd, sizeof(cmd), stat, sizeof(stat));
   if (res!=ERR_OK) {
     *status = 0;
     return res;
@@ -92,15 +83,15 @@ uint8_t SHT31_ReadStatus(uint16_t *status) {
   return res;
 }
 
-uint8_t SHT31_Heater(bool on) {
+uint8_t McuSHT31_Heater(bool on) {
   return SHT31_WriteCommand(on?SHT31_HEATEREN:SHT31_HEATERDIS);
 }
 
-uint8_t SHT31_Reset(void) {
+uint8_t McuSHT31_Reset(void) {
   return SHT31_WriteCommand(SHT31_SOFTRESET);
 }
 
-uint8_t SHT31_ReadTempHum(float *temperature, float *humidity) {
+uint8_t McuSHT31_ReadTempHum(float *temperature, float *humidity) {
   double stemp, shum;
   uint16_t ST, SRH;
   uint8_t readbuffer[6];
@@ -109,7 +100,7 @@ uint8_t SHT31_ReadTempHum(float *temperature, float *humidity) {
 
   cmd[0] = SHT31_MEAS_HIGHREP>>8;
   cmd[1] = SHT31_MEAS_HIGHREP&0xff;
-  res = McuGenericI2C_ReadAddressWait(SHT31_I2C_ADDR, cmd, sizeof(cmd), 20, readbuffer, sizeof(readbuffer));
+  res = McuGenericI2C_ReadAddressWait(McuSHT31_I2C_ADDR, cmd, sizeof(cmd), 20, readbuffer, sizeof(readbuffer));
   if (res!=ERR_OK) {
     return res;
   }
@@ -137,14 +128,14 @@ uint8_t SHT31_ReadTempHum(float *temperature, float *humidity) {
   return ERR_OK;
 }
 
-#if SHT31_CONFIG_PARSE_COMMAND_ENABLED
+#if MCUSHT31_CONFIG_PARSE_COMMAND_ENABLED
 static uint8_t PrintStatus(const McuShell_StdIOType *io) {
   uint8_t buf[32];
   float temperature, humidity;
 
   McuShell_SendStatusStr((unsigned char*)"SHT31", (unsigned char*)"\r\n", io->stdOut);
 
-  if(SHT31_ReadTempHum(&temperature, &humidity)==ERR_OK) {
+  if(McuSHT31_ReadTempHum(&temperature, &humidity)==ERR_OK) {
     McuUtility_NumFloatToStr(buf, sizeof(buf), temperature, 2);
     McuUtility_chcat(buf, sizeof(buf), '\xB0'); /* add '°' (ASCII 0xB0) it as single char, as with UTF-8 it might end up in two bytes inside a string */
     McuUtility_strcat(buf, sizeof(buf), (unsigned char*)"C\r\n");
@@ -164,11 +155,10 @@ static uint8_t PrintHelp(const McuShell_StdIOType *io) {
   McuShell_SendHelpStr((unsigned char*)"  help|status", (unsigned char*)"Print help or status information\r\n", io->stdOut);
   return ERR_OK;
 }
-#endif /* SHT31_CONFIG_PARSE_COMMAND_ENABLED */
+#endif /* MCUSHT31_CONFIG_PARSE_COMMAND_ENABLED */
 
-
-#if SHT31_CONFIG_PARSE_COMMAND_ENABLED
-uint8_t SHT31_ParseCommand(const unsigned char* cmd, bool *handled, const McuShell_StdIOType *io) {
+#if MCUSHT31_CONFIG_PARSE_COMMAND_ENABLED
+uint8_t McuSHT31_ParseCommand(const unsigned char* cmd, bool *handled, const McuShell_StdIOType *io) {
   uint8_t res = ERR_OK;
 
   if (McuUtility_strcmp((char*)cmd, McuShell_CMD_HELP) == 0
@@ -187,14 +177,10 @@ uint8_t SHT31_ParseCommand(const unsigned char* cmd, bool *handled, const McuShe
 }
 #endif /* SHT31_CONFIG_PARSE_COMMAND_ENABLED */
 
-void SHT31_Init(void) {
+void McuSHT31_Init(void) {
   /* noting to do */
 }
 
-void SHT31_Deinit(void) {
+void McuSHT31_Deinit(void) {
   /* nothing to do */
 }
-
-#endif /* PL_CONFIG_HAS_SHT31 */
-
-#endif /* SOURCES_SHT31_C_ */
---
+++ Examples/MCUXpresso/tinyK22/tinyK22_RaspberryPi_UPS/McuLib/src/McuSHT31.h
@@ -0,0 +1,29 @@
+/*
+ * SHT31.h
+ *
+ *  Created on: 08.10.2018
+ *      Author: Erich Styger
+ *  Driver for the SHT31 Temperature/Humidity sensor
+ */
+
+#ifndef SOURCES_MCUSHT31_H_
+#define SOURCES_MCUSHT31_H_
+
+#include "McuSHT31config.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#if MCUSHT31_CONFIG_PARSE_COMMAND_ENABLED
+  #include "McuShell.h"
+  uint8_t McuSHT31_ParseCommand(const unsigned char* cmd, bool *handled, const McuShell_StdIOType *io);
+#endif
+
+uint8_t McuSHT31_Reset(void);
+uint8_t McuSHT31_Heater(bool on);
+uint8_t SHT31_ReadStatus(uint16_t *status);
+uint8_t McuSHT31_ReadTempHum(float *temperature, float *humidity);
+
+void McuSHT31_Deinit(void);
+void McuSHT31_Init(void);
+
+#endif /* SOURCES_MCUSHT31_H_ */
