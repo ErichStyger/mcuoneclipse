8ffd133703d1f588d43119d8bf8faba70c522352
Erich Styger <erich.styger@hslu.ch>
2019-12-13 17:05:35 +0100

    updated flash application

--- Examples/MCUXpresso/i.MX RT1052 Seeed Arch Mix/MIMXRT1052_Seeed_ArchMix_BlinkyFLASH/readme.txt
+++ Examples/MCUXpresso/i.MX RT1052 Seeed Arch Mix/MIMXRT1052_Seeed_ArchMix_BlinkyFLASH/readme.txt
@@ -3,9 +3,12 @@ readme.txt
 Project for a 'blinky' on the Seeed Arch Mix (i.MX RT1052) board, running from FLASH.
 
 Notes:
-- only works with a NXP LPC-Link! Segger J-Link (Mini EDU) fails to properly debug the target.
+- works out of the box with a NXP LPC-Link.
+- for Segger J-Link (Mini EDU), see https://mcuoneclipse.com/2019/10/27/using-segger-j-link-with-qspi-flash-on-nxp-i-mx-boards/
 
 Links:
+- https://community.nxp.com/community/mcuxpresso/mcuxpresso-ide/blog/2017/12/07/overview-of-using-the-mimxrt1050-evk-with-mcuxpresso-ide
 - https://mcuoneclipse.com/2019/07/25/seeed-studio-arch-mix-nxp-i-mx-rt1052-board/
 - https://mcuoneclipse.com/2019/07/27/debug-and-execute-code-from-flash-on-the-seeed-arch-mix-nxp-i-mx-rt1052-board/
 - https://mcuoneclipse.com/2019/08/01/restoring-default-firmware-on-seeed-arch-mix-nxp-i-mx-rt1052-board/
+- https://mcuoneclipse.com/2019/10/27/using-segger-j-link-with-qspi-flash-on-nxp-i-mx-boards/
--- Examples/MCUXpresso/i.MX RT1052 Seeed Arch Mix/MIMXRT1052_Seeed_ArchMix_BlinkyFLASH/xip/evkbimxrt1050_sdram_ini_dcd.h
+++ Examples/MCUXpresso/i.MX RT1052 Seeed Arch Mix/MIMXRT1052_Seeed_ArchMix_BlinkyFLASH/xip/evkbimxrt1050_sdram_ini_dcd.h
@@ -31,40 +31,17 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <xip/evkmimxrt1050_flexspi_nor_config.h>
+#ifndef __EVKBIMXRT1050_DCD_SDRAM_INIT__
+#define __EVKBIMXRT1050_DCD_SDRAM_INIT__
 
-/*******************************************************************************
- * Code
- ******************************************************************************/
-#if defined(XIP_BOOT_HEADER_ENABLE) && (XIP_BOOT_HEADER_ENABLE == 1)
-#if defined(__CC_ARM) || defined(__GNUC__)
-__attribute__((section(".boot_hdr.conf")))
-#elif defined(__ICCARM__)
-#pragma location = ".boot_hdr.conf"
-#endif
+#include <stdint.h>
 
-const flexspi_nor_config_t qspiflash_config = {
-    .memConfig =
-        {
-            .tag = FLEXSPI_CFG_BLK_TAG,
-            .version = FLEXSPI_CFG_BLK_VERSION,
-            .readSampleClkSrc = kFlexSPIReadSampleClk_LoopbackFromDqsPad,
-            .csHoldTime = 3u,
-            .csSetupTime = 3u,
-            // Enable DDR mode, Wordaddassable, Safe configuration, Differential clock
-            .sflashPadType = kSerialFlash_4Pads,
-            .serialClkFreq = kFlexSpiSerialClk_100MHz,
-            .sflashA1Size = 8u * 1024u * 1024u,
-            .lookupTable =
-                {
-                    // Read LUTs
-                    FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0xEB, RADDR_SDR, FLEXSPI_4PAD, 0x18),
-                    FLEXSPI_LUT_SEQ(DUMMY_SDR, FLEXSPI_4PAD, 0x06, READ_SDR, FLEXSPI_4PAD, 0x04),
-                },
-        },
-    .pageSize = 256u,
-    .sectorSize = 4u * 1024u,
-    .blockSize = 256u * 1024u,
-    .isUniformBlockSize = false,
-};
-#endif /* XIP_BOOT_HEADER_ENABLE */
+/*************************************
+ *  DCD Data
+ *************************************/
+#define DCD_TAG_HEADER (0xD2)
+#define DCD_TAG_HEADER_SHIFT (24)
+#define DCD_VERSION (0x40)
+#define DCD_ARRAY_SIZE 1
+
+#endif /* __EVKBIMXRT1050_DCD_SDRAM_INIT__ */
