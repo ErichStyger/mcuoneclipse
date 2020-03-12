f9db58e613897ca39537e4d4a63c594d67ff59df
Erich Styger <erich.styger@hslu.ch>
2020-03-12 07:23:09 +0100

    cleanup

--- Examples/MCUXpresso/tinyK22/tinyK22_WS2812B_Ring/source/IncludeMcuLibConfig.h
+++ Examples/MCUXpresso/tinyK22/tinyK22_WS2812B_Ring/source/IncludeMcuLibConfig.h
@@ -28,10 +28,10 @@ Instructions:
 #define INCLUDEMCULIBCONFIG_H_
 
 /* ------------------- SDK/Library ---------------------------*/
-#define McuLib_CONFIG_SDK_VERSION_USED              McuLib_CONFIG_SDK_MCUXPRESSO_2_0
-#define McuLib_CONFIG_CPU_IS_LPC        (0)  /* LPC845 */
-#define McuLib_CONFIG_CORTEX_M          (4)  /* K22 i Cortex-M4 */
-#define McuLib_CONFIG_CPU_IS_KINETIS    (1)
+#define McuLib_CONFIG_SDK_VERSION_USED   McuLib_CONFIG_SDK_MCUXPRESSO_2_0
+#define McuLib_CONFIG_CPU_IS_LPC         (0)  /* LPC845 */
+#define McuLib_CONFIG_CORTEX_M           (4)  /* K22 is a Cortex-M4 */
+#define McuLib_CONFIG_CPU_IS_KINETIS     (1)
 
 /* ------------------- RTOS ---------------------------*/
 #define McuLib_CONFIG_SDK_USE_FREERTOS              (1)
