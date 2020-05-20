7c5c4feae32ebba33a138e7658d9238afdd32618
Erich Styger <erich.styger@hslu.ch>
2020-05-20 15:50:36 +0200

    extended platform for USB

--- Examples/MCUXpresso/FRDM-K22F/FRDM-K22F_FatFS_SD_Card/source/platform.h
+++ Examples/MCUXpresso/FRDM-K22F/FRDM-K22F_FatFS_SD_Card/source/platform.h
@@ -7,15 +7,18 @@
 #ifndef PLATFORM_H_
 #define PLATFORM_H_
 
+/* Features */
+#define PL_CONFIG_USE_SD_CARD       (1) /* if using FatFS with SD card */
+#define PL_CONFIG_USE_MININI        (1 && PL_CONFIG_USE_SD_CARD) /* if using minIni */
+#define PL_CONFIG_USE_USB_MSD       (1) /* if using FatFS with USB MSD */
 
 /* Shell */
 #define PL_CONFIG_USE_SHELL         (1) /* if using shell */
 #define PL_CONFIG_USE_SHELL_UART    (1) /* if using (OpenSDA) UART */
 #define PL_CONFIG_USE_RTT           (1) /* if using RTT */
 #define PL_CONFIG_USE_USB_CDC       (0) /* if using USB CDC */
-#define PL_CONFIG_USE_SD_CARD       (1) /* if using SD card */
-#define PL_CONFIG_USE_MININI        (1 && PL_CONFIG_USE_SD_CARD)
 
+/* platform initialization */
 void PL_Init(void);
 
 #endif /* PLATFORM_H_ */
