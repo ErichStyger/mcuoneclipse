d463154b4d5d5419862d1aedc36ae860b0907bc9
Erich Styger <erich.styger@hslu.ch>
2021-02-01 11:35:45 +0100

    adding gcov support

--- Examples/MCUXpresso/FRDM-K64F/FRDM-K64F_gcov_bm/source/main.c
+++ Examples/MCUXpresso/FRDM-K64F/FRDM-K64F_gcov_bm/source/main.c
@@ -41,32 +41,36 @@
 #include "fsl_debug_console.h"
 /* TODO: insert other include files here. */
 
-/* TODO: insert other definitions and declarations here. */
-
+#include "../gcov/gcov_support.h"
 /*
  * @brief   Application entry point.
  */
 int main(void) {
-
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
+  gcov_init();  /* initialize library */
+  if (!gcov_check()) { /* optional: check if semihosting file writing works */
+    printf("WARNING: writing coverage does not work! Wrong library used?\n");
+  }
+  /* Init board hardware. */
+  BOARD_InitBootPins();
+  BOARD_InitBootClocks();
+  BOARD_InitBootPeripherals();
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
+  /* Init FSL debug console. */
+  BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+  PRINTF("Hello World\n");
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+  gcov_write(); /* write coverage files, might take a while depending how many files are covered */
+
+  /* Force the counter to be placed into memory. */
+  volatile static int i = 0 ;
+  /* Enter an infinite loop, just incrementing a counter. */
+  while(1) {
+      i++ ;
+      /* 'Dummy' NOP to allow source level single stepping of
+          tight while() loop */
+      __asm volatile ("nop");
+  }
+  return 0 ;
 }
