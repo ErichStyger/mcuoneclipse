53fb7ad54641e404b09e066dc42237f31939e577
Erich Styger <erich.styger@hslu.ch>
2021-05-04 08:06:50 +0200

    removing generated files

--- Examples/VisualStudioCode/FRDM-K22F/FRDM-K22F_Simple/build/CMakeFiles/FRDM-K22F_Blinky.elf.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for FRDM-K22F_Blinky.elf.
-# This may be replaced when dependencies are built.
--- Examples/VisualStudioCode/FRDM-K22F/FRDM-K22F_Simple/build/CMakeFiles/FRDM-K22F_Blinky.elf.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for FRDM-K22F_Blinky.elf.
