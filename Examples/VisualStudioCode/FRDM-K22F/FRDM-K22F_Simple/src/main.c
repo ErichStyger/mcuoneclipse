ccdfe0300d1d17b23c81eaf59cd922fd40f58c0a
Erich Styger <erich.styger@hslu.ch>
2021-05-10 13:27:43 +0200

    added debug options and generated debugger files

--- Examples/VisualStudioCode/FRDM-K22F/FRDM-K22F_Simple/src/main.c
+++ Examples/VisualStudioCode/FRDM-K22F/FRDM-K22F_Simple/src/main.c
@@ -1,5 +1,19 @@
 #include "main.h"
- 
+#include <stdint.h>
+
+static void bar(int *p) {
+    (*p)++;
+}
+
+int32_t global = 0;
+
 int main(void) {
+    int local = 0;
+
+    for(;;) {
+        global++;
+        local += 2;
+        bar(&global);
+    }
     return 0;
 }
