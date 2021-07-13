0d3b036f616485f34a8a2839773399c5999a8d47
Erich Styger <erich.styger@hslu.ch>
2021-07-13 14:16:09 +0200

    simplified more and using SDK API

--- Examples/MCUXpresso/LPC804/LPC804_Blinky/readme.txt
+++ Examples/MCUXpresso/LPC804/LPC804_Blinky/readme.txt
@@ -10,5 +10,3 @@ The LPC804.xml file is the data file for the NXP PLU Tool.
 Training/Tutorial:
 https://www.nxp.com/video/creative-ways-to-leverage-the-lpc804-mcus-part-2:LPC80X-LEVERAGE-PLU
 https://www.nxp.com/video/learn-more-about-lpc804-mcus-development-board-and-the-programmable-logic-unit-feature:LPC804-DEMO-VIDEO
-
-
--- Examples/MCUXpresso/LPC804/LPC804_Blinky/source/ws2812b.c
+++ Examples/MCUXpresso/LPC804/LPC804_Blinky/source/ws2812b.c
@@ -1,55 +1,46 @@
 /*
-  ws2812b.c
-
-  PLU will be configured as SPI client. 
-  SPI clock should 800kHz: SPI clock div for the main clk can be 24 or 23
-  
-  PLU configuration:
-    SPI Clock: PIO0_9;
-    SPI MOSI: PIO0_11;
-*/
+ * Copyright (c) 2021, Erich Styger
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 #include "platform.h"
 #include "ws2812b.h"
-#include "LPC8xx.h"
-#include "syscon.h"
-#include "spi.h"
+#include "fsl_spi.h"
 #include "McuWait.h"
 #include "plu_setup.h"
 
-static void spi_init(void) {
-  Enable_Periph_Clock(CLK_SPI0);
-  LPC_SYSCON->SPI0CLKSEL = FCLKSEL_MAIN_CLK;	
-  LPC_SPI0->DIV = 24;	/* 22 still seems to work */
-  LPC_SPI0->CFG = SPI_CFG_ENABLE | SPI_CFG_MASTER;
+static void SPI_Init(void) {
+  CLOCK_EnableClock(kCLOCK_Spi0); /* enable peripheral clock */
+  SYSCON->SPI0CLKSEL = SYSCON_SPI0CLKSEL_SEL(1); /* use main clock */
+  SPI0->DIV = 24; /* divider for reaching 500'000 baud at 15 MHz clock rate */
+  SPI0->CFG = SPI_CFG_ENABLE_MASK | SPI_CFG_MASTER_MASK;
 }
 
 void WS2812B_spi_out(uint8_t *data, size_t cnt) {
-  LPC_SPI0->TXCTL = 
-      SPI_CTL_RXIGNORE | 		/* do not read data from MISO */
-      SPI_CTL_LEN(8) | 			/* send 8 bits */
-      SPI_TXDATCTL_SSELN(3); 	/* do not use any slave select */
+  SPI0->TXCTL =
+      SPI_TXDATCTL_RXIGNORE_MASK |    /* do not read data from MISO */
+      SPI_TXDATCTL_LEN(7) |           /* send 8 bits */
+      SPI_TXDATCTL_TXSSEL0_N(3);      /* do not use any slave select */
   for(int i=0; i<cnt; i++) {
-    /* wait until the tx register can accept further data */
-    while( (LPC_SPI0->STAT & SPI_STAT_TXRDY) == 0 ) {}
-    /* ensure, that the SCK goes to low after the byte transfer: */
-    /* Set the EOT flag at the end of the transfer */
-    if (i+1 == cnt) {
-      LPC_SPI0->TXCTL |= SPI_CTL_EOT;
+    while( (SPI0->STAT & SPI_STAT_TXRDY_MASK) == 0 ) {
+      /* block until there is space in the TX register for receiving the data */
     }
-    LPC_SPI0->TXDAT = data[i]; /* transfer one byte via SPI */
+    if (i+1 == cnt) {
+      /* need to make sure that the CLK line goes LOW after sending: send EOT */
+      SPI0->TXCTL |= SPI_TXCTL_EOT(1);
+    }
+    SPI0->TXDAT = data[i]; /* send one byte  */
   }
 }
 
 void WS2812B_SendPixels(uint8_t *data, size_t cnt) {
   WS2812B_spi_out(data, cnt);
-  /* LPC804 has might have up to two bytes in the queue, wait for their transmission */
-  McuWait_Waitus(24+24);
-  /* write the new values into the LEDs */
-  McuWait_Waitus(50);
+  McuWait_Waitus(2*24); /* LPC804 has might have up to two bytes in the queue, wait for their transmission */
+  McuWait_Waitus(50); /* write the new values into the LEDs: signal low for 50us to latch the data */
 }
 
 void WS2812B_Init(void) {
-  PLU_setup(); /*  will enable SWM clock */
-  spi_init();
+  SPI_Init(); /* initialize and enable SPI */
+  PLU_setup(); /*  initialize the PLU and configure clocks */
 }
