8a15db4cf691d8c41a991735379b1728391066fd
Erich Styger <erich.styger@hslu.ch>
2021-07-18 10:49:50 +0200

    fresh new project

---
+++ Examples/VisualStudioCode/FRDM-K22F/HelloWorld/libs/adder/include/add/add.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2021 Liviu Ionescu. All rights reserved.
+ *
+ * This Software is provided under the terms of the MIT License.
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT/.
+ */
+
+// ----------------------------------------------------------------------------
+
+#ifndef ADD_H_
+#define ADD_H_
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif // defined(__cplusplus)
+
+// ----------------------------------------------------------------------------
+
+int
+add (int a, int b);
+
+// ----------------------------------------------------------------------------
+
+#if defined(__cplusplus)
+}
+#endif // defined(__cplusplus)
+
+#endif // ADD_H_
+
+// ----------------------------------------------------------------------------
