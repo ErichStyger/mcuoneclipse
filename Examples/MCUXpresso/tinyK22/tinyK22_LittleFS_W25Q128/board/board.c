8bcf5928662f41f4490e07c9cc5911c532abd3a7
Erich Styger <erich.styger@hslu.ch>
2021-12-26 15:25:35 +0100

    initial version, starting point from the SDK

---
+++ Examples/MCUXpresso/tinyK22/tinyK22_LittleFS_W25Q128/board/board.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdint.h>
+#include "fsl_common.h"
+#include "fsl_debug_console.h"
+#include "board.h"
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/* Initialize debug console. */
+void BOARD_InitDebugConsole(void)
+{
+    #if (defined BOARD_USE_VIRTUALCOM)
+        DbgConsole_Init(0, 0, DEBUG_CONSOLE_DEVICE_TYPE_USBCDC, 0);
+    #endif    
+}
---
+++ Examples/MCUXpresso/tinyK22/tinyK22_LittleFS_W25Q128/readme.txt
@@ -0,0 +1,5 @@
+readme.txt
+----------
+Project for the tinyK22 (NXP K22FN512) siliar to the project presented in
+https://mcuoneclipse.com/2019/01/06/driver-and-shell-for-winbond-w25q128-16mbyte-serial-flash-device/
+using littleFS with the Winbond W25Q128 SPI flash.
