5abde74c51aa8fc7e150c7e938f6d119cf722c71
Erich Styger <erich.styger@hslu.ch>
2022-03-09 10:12:58 +0100

    using common McuLib

--- Examples/MCUXpresso/LPC55S69-EVK/LPC55S69_McuLib_Blinky/source/IncludeMcuLibConfig.h
+++ Examples/MCUXpresso/LPC55S69-EVK/LPC55S69_McuLib_Blinky/source/IncludeMcuLibConfig.h
@@ -12,6 +12,7 @@
 
 #define McuLib_CONFIG_CPU_IS_LPC        (1)
 #define McuLib_CONFIG_CPU_IS_LPC55xx    (1)
+#define McuLib_CONFIG_CPU_VARIANT       McuLib_CONFIG_CPU_VARIANT_NXP_LPC55S69
 #define McuLib_CONFIG_CPU_IS_KINETIS    (0)
 #define McuLib_CONFIG_CORTEX_M          (33)
 #define McuLib_CONFIG_SDK_VERSION_USED  McuLib_CONFIG_SDK_MCUXPRESSO_2_0
@@ -49,7 +50,7 @@
 #define McuShell_CONFIG_PROJECT_NAME_STRING "LPC55S69-EVK"
 /* -----------------------------------------------------*/
 /* McuSWO */
-#define McuSWO_CONFIG_HAS_SWO         (1) /* enable SWO support */
+#define McuSWO_CONFIG_HAS_SWO         (0) /* enable SWO support */
 //#define McuSWO_CONFIG_SPEED_BAUD      (5625000) /* J-Link supports up to 5625 kHz */
 #define McuSWO_CONFIG_SPEED_BAUD      (96000000) /* needed for LinkServer and MCUXpresso IDE 11.5.0 */
 
