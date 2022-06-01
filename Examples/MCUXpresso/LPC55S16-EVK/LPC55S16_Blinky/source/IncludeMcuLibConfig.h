58de8b01bc5d94ccb7dbbcf6bee0e34dd400679d
Erich Styger <erich.styger@hslu.ch>
2022-06-01 16:24:58 +0200

    cosmetic

--- Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_Blinky/source/IncludeMcuLibConfig.h
+++ Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_Blinky/source/IncludeMcuLibConfig.h
@@ -31,7 +31,7 @@ Instructions:
 #define INCLUDEMCULIBCONFIG_H_
 
 /* global platform configuration */
-#define PL_CONFIG_USE_LORA_SHIELD (0) /* if using the LoRa Shield developed by Corsin Obrist */
+#define PL_CONFIG_USE_LORA_SHIELD         (1) /* if using the LoRa Shield developed by Corsin Obrist */
 
 /* ------------------- SDK/Library ---------------------------*/
 #define McuLib_CONFIG_SDK_VERSION_USED  McuLib_CONFIG_SDK_MCUXPRESSO_2_0
