36db1e7a09a22f108e04168760ecc08f55f58270
Erich Styger <erich.styger@hslu.ch>
2023-01-08 17:41:41 +0100

    added udp_server (not implemented yet)

---
+++ Examples/RaspberryPiPico/pico_Heidelberg/src/udp_server.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023, Erich Styger
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SRC_UDP_SERVER_H_
+#define SRC_UDP_SERVER_H_
+
+void UdpServer_Init(void);
+
+#endif /* SRC_UDP_SERVER_H_ */
