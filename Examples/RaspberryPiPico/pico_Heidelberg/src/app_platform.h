dea80b60ffeac6ac38f2ac23c71610f20c4e4f4a
Erich Styger <erich.styger@hslu.ch>
2023-01-09 18:09:08 +0100

    rearranged config

--- Examples/RaspberryPiPico/pico_Heidelberg/src/app_platform.h
+++ Examples/RaspberryPiPico/pico_Heidelberg/src/app_platform.h
@@ -38,15 +38,14 @@
 #define PL_CONFIG_USE_I2C               (1 && (PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_1 || PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_2 || PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_3)) /* if using I2C */
 #define PL_CONFIG_USE_GUI               (1) /* if using lvgl GUI or not */
 #define PL_CONFIG_USE_GUI_KEY_NAV       (1 && PL_CONFIG_USE_GUI && PL_CONFIG_USE_BUTTONS) /* if using navigation keys for GUI */
-#define PL_CONFIG_USE_OLED              (1 & PL_CONFIG_USE_I2C && !PL_CONFIG_USE_GUI) /* if using OLED module (simple GUI, not lvgl!) */
-#define PL_CONFIG_USE_OLED_CLOCK        (1 && PL_CONFIG_USE_OLED) /* if showing clock/time/date on OLED. Note: no true RTC yet! */
 #define PL_CONFIG_USE_USB_CDC           (0) /* caution, because issues while debugging! In Termite, need to connect with RTS/CTS! Putty works fine */
 #define PL_CONFIG_USE_RTT               (1) /* if using SEGGER RTT */
-#define PL_CONFIG_USE_SHELL_UART        (0) /* NYI, using the physical UART */
 #define PL_CONFIG_USE_NVMC              (1) /* if using NVMC in Flash, needs to be turned on too */
 #define PL_CONFIG_USE_MINI              (1 && PL_CONFIG_USE_NVMC)
+
 #define PL_CONFIG_USE_EXT_FLASH         (0 && !PL_CONFIG_USE_BUTTONS) /* if using externals SPI flash (uses nav switch button pins!) */
 #define PL_CONFIG_USE_LITTLE_FS         (0 && PL_CONFIG_USE_EXT_FLASH) /* if using littleFS as file system, not supported yet! */
+#define PL_CONFIG_USE_SHELL_UART        (0) /* NYI, using the physical UART */
 
 #define PL_CONFIG_USE_NEO_PIXEL_HW      (0 && (PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_1 || PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_2 || PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_3 || PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_4)) /* using WS2812B */
 #define PL_CONFIG_USE_PWM_LED           (0 && (PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_5)) /* using 3 RGB LEDs with PWM */
@@ -54,7 +53,8 @@
 #define PL_CONFIG_USE_SHT31             (0 & PL_CONFIG_USE_I2C) /* if using SHT31 sensor */
 #define PL_CONFIG_USE_SHT40             (0 & PL_CONFIG_USE_I2C && PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_5) /* if using SHT31 sensor */
 #define PL_CONFIG_USE_ROAD              (0 && (PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_3 || PL_CONFIG_HW_ACTIVE_HW_VERSION==PL_CONFIG_HW_VERSION_0_5)) /* if having support for the magnetic/coil road */
-
+#define PL_CONFIG_USE_OLED              (0 & PL_CONFIG_USE_I2C && !PL_CONFIG_USE_GUI) /* if using OLED module (simple GUI, not lvgl!) */
+#define PL_CONFIG_USE_OLED_CLOCK        (0 && PL_CONFIG_USE_OLED) /* if showing clock/time/date on OLED. Note: no true RTC yet! */
 
 #define PL_CONFIG_USE_LIGHTS            (1 && (PL_CONFIG_USE_PWM_LED|PL_CONFIG_USE_NEO_PIXEL_HW)) /* simple VHS house light application with neopixels */
 #define PL_CONFIG_USE_LED_COUNTER_APP   (0 && !PL_CONFIG_USE_LIGHTS && PL_CONFIG_USE_NEO_PIXEL_HW && PL_CONFIG_USE_BUTTONS) /* application to control LEDs with nav switch */
