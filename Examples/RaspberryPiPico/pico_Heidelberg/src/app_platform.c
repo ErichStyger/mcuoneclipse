23fd1654fbd3336b34741d83b3b0037a3a0d3154
Erich Styger <erich.styger@hslu.ch>
2023-01-12 13:17:55 +0100

    added lights module

--- Examples/RaspberryPiPico/pico_Heidelberg/src/app_platform.c
+++ Examples/RaspberryPiPico/pico_Heidelberg/src/app_platform.c
@@ -85,6 +85,9 @@
 #if PL_CONFIG_USE_GUI
   #include "gui.h"
 #endif
+#if PL_CONFIG_USE_LIGHTS
+  #include "lights.h"
+#endif
 
 /* \todo need to have it globally defined, as not present anywhere else */
 uint32_t SystemCoreClock = 120000000;
