338637e1d4d022c263ca31b2dfbe7a85a22265f1
Erich Styger <erich.styger@hslu.ch>
2023-01-15 14:38:05 +0100

    refactoring

--- Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_hello_world_swo/source/hello_world_swo.c
+++ Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_hello_world_swo/source/hello_world_swo.c
@@ -31,106 +31,88 @@ void BOARD_InitDebugConsoleSWO(uint32_t port, uint32_t baudrate);
 /*******************************************************************************
  * Variables
  ******************************************************************************/
-volatile bool g_userPress = false;
-volatile bool g_timeOut = false;
-volatile uint32_t g_systickCounter = 1000U;
-/*******************************************************************************
- * Code
- ******************************************************************************/
-extern volatile bool g_userPress;
+static volatile bool g_userPress = false;
+static volatile bool g_timeOut = false;
+static volatile uint32_t g_systickCounter = 1000U;
 
-void pint_intr_callback(pint_pin_int_t pintr, uint32_t pmatch_status)
-{
-    g_userPress = true;
+void pint_intr_callback(pint_pin_int_t pintr, uint32_t pmatch_status) {
+  g_userPress = true;
 }
 
-void BOARD_InitKey(void)
-{
+void BOARD_InitKey(void) {
 #if defined(FSL_FEATURE_SYSCON_HAS_PINT_SEL_REGISTER) && FSL_FEATURE_SYSCON_HAS_PINT_SEL_REGISTER
-    /* Connect trigger sources to PINT */
-    SYSCON_AttachSignal(SYSCON, kPINT_PinInt0, kINPUTMUX_GpioPort0Pin5ToPintsel);
+  /* Connect trigger sources to PINT */
+  SYSCON_AttachSignal(SYSCON, kPINT_PinInt0, kINPUTMUX_GpioPort0Pin5ToPintsel);
 #else
-    /* Connect trigger sources to PINT */
-    INPUTMUX_Init(INPUTMUX);
-    INPUTMUX_AttachSignal(INPUTMUX, kPINT_PinInt0, kINPUTMUX_GpioPort0Pin5ToPintsel);
-    /* Turnoff clock to inputmux to save power. Clock is only needed to make changes */
-    INPUTMUX_Deinit(INPUTMUX);
+  /* Connect trigger sources to PINT */
+  INPUTMUX_Init(INPUTMUX);
+  INPUTMUX_AttachSignal(INPUTMUX, kPINT_PinInt0, kINPUTMUX_GpioPort0Pin5ToPintsel);
+  /* Turnoff clock to inputmux to save power. Clock is only needed to make changes */
+  INPUTMUX_Deinit(INPUTMUX);
 #endif /* FSL_FEATURE_SYSCON_HAS_PINT_SEL_REGISTER */
 
-    /* Initialize PINT */
-    PINT_Init(PINT);
+  /* Initialize PINT */
+  PINT_Init(PINT);
 
-    /* Setup Pin Interrupt 0 for rising edge */
-    PINT_PinInterruptConfig(PINT, kPINT_PinInt0, kPINT_PinIntEnableRiseEdge, pint_intr_callback);
+  /* Setup Pin Interrupt 0 for rising edge */
+  PINT_PinInterruptConfig(PINT, kPINT_PinInt0, kPINT_PinIntEnableRiseEdge, pint_intr_callback);
 
-    /* Enable callbacks for PINT */
-    PINT_EnableCallback(PINT);
+  /* Enable callbacks for PINT */
+  PINT_EnableCallback(PINT);
 }
 
 
-void BOARD_InitDebugConsoleSWO(uint32_t port, uint32_t baudrate)
-{
-    SystemCoreClockUpdate();
-    uint32_t clkSrcFreq = SystemCoreClock;
+void BOARD_InitDebugConsoleSWO(uint32_t port, uint32_t baudrate) {
+  SystemCoreClockUpdate();
+  uint32_t clkSrcFreq = SystemCoreClock;
 
-    DbgConsole_Init(port, baudrate, kSerialPort_Swo, clkSrcFreq);
+  DbgConsole_Init(port, baudrate, kSerialPort_Swo, clkSrcFreq);
 }
 
- void SysTick_Handler(void)
-{
-    if (g_systickCounter != 0U)
-    {
-        g_systickCounter--;
-    }
-    else 
-    {
-        g_systickCounter = 1000U;
-        g_timeOut = true;
-    }
+void SysTick_Handler(void) {
+  if (g_systickCounter != 0U) {
+    g_systickCounter--;
+  } else {
+    g_systickCounter = 1000U;
+    g_timeOut = true;
+  }
 }
 
 /*!
  * @brief Main function
  */
-int main(void)
-{
-    /* Init board hardware. */
-    /* set BOD VBAT level to 1.65V */
-    POWER_SetBodVbatLevel(kPOWER_BodVbatLevel1650mv, kPOWER_BodHystLevel50mv, false);
-    /* attach 12 MHz clock to FLEXCOMM0 (debug console) */
-    CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
-    /*!< Switch TRACE to TRACE_DIV */
-    CLOCK_AttachClk(kTRACE_DIV_to_TRACE);
+int main(void) {
+  /* Init board hardware. */
+  /* set BOD VBAT level to 1.65V */
+  POWER_SetBodVbatLevel(kPOWER_BodVbatLevel1650mv, kPOWER_BodHystLevel50mv, false);
+  /* attach 12 MHz clock to FLEXCOMM0 (debug console) */
+  CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
+  /*!< Switch TRACE to TRACE_DIV */
+  CLOCK_AttachClk(kTRACE_DIV_to_TRACE);
 
-    BOARD_InitBootPins();
-    BOARD_BootClockFROHF96M();
+  BOARD_InitBootPins();
+  BOARD_BootClockFROHF96M();
 
-    CLOCK_SetClkDiv(kCLOCK_DivAhbClk, 2U, false);          /* Set AHBCLKDIV divider to value 2 */
-    CLOCK_SetClkDiv(kCLOCK_DivArmTrClkDiv, 2U, false);     /* Set ARMTRCLKDIV divider to value 2 */
+  CLOCK_SetClkDiv(kCLOCK_DivAhbClk, 2U, false);          /* Set AHBCLKDIV divider to value 2 */
+  CLOCK_SetClkDiv(kCLOCK_DivArmTrClkDiv, 2U, false);     /* Set ARMTRCLKDIV divider to value 2 */
 
-    BOARD_InitDebugConsole();
-    BOARD_InitKey();
-    BOARD_InitDebugConsoleSWO(DEMO_DEBUG_CONSOLE_SWO_PORT, DEMO_DEBUG_CONSOLE_SWO_BAUDRATE);
+  BOARD_InitDebugConsole();
+  BOARD_InitKey();
+  BOARD_InitDebugConsoleSWO(DEMO_DEBUG_CONSOLE_SWO_PORT, DEMO_DEBUG_CONSOLE_SWO_BAUDRATE);
 
-    /* Set systick reload value to generate 1ms interrupt */
-    if (SysTick_Config(SystemCoreClock / 1000U))
-    {
-        while (1)
-        {
-        }
+  /* Set systick reload value to generate 1ms interrupt */
+  if (SysTick_Config(SystemCoreClock / 1000U)) {
+    while (1) {
     }
-
-    while (1)
-    {
-        if (g_userPress)
-        {
-            PRINTF("SWO: hello_world\r\n");
-            g_userPress = false;
-        }
-        if (g_timeOut)
-        {
-            PRINTF("SWO: timer_trigger\r\n");
-            g_timeOut = false;
-        }
+  }
+  while (1) {
+    if (g_userPress) {
+      DbgConsole_Printf("SWO: hello_world\r\n");
+      g_userPress = false;
     }
+    if (g_timeOut) {
+      DbgConsole_Printf("SWO: timer_trigger\r\n");
+      g_timeOut = false;
+    }
+  }
 }
