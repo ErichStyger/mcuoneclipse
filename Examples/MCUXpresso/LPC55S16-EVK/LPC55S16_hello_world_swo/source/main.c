de419da7881293c15a1f5502ac250bca4b7648fe
Erich Styger <erich.styger@hslu.ch>
2023-01-26 17:17:08 +0100

    refactoring

--- Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_hello_world_swo/source/main.c
+++ Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_hello_world_swo/source/main.c
@@ -16,38 +16,67 @@
 #include "McuUtility.h"
 #include "McuSWO.h"
 
+static void ReadLine_McuLib(unsigned char *buf, size_t bufSize) {
+  unsigned char ch;
+
+  buf[0] = '\0'; /* init buffer */
+  for(;;) { /* breaks */
+    if (McuSWO_StdIOKeyPressed()) {
+      McuSWO_stdio.stdIn(&ch);
+      if (ch!='\0') {
+        McuUtility_chcat(buf, bufSize, ch);
+        if (ch=='\n') {
+          break;
+        }
+      } /* if */
+    } /* if */
+  } /* for */
+}
+
+static void ReadLine_getc(unsigned char *buf, size_t bufSize) {
+  int c;
+  size_t i = 0;
+
+  do {
+    do {
+      c = getc(stdin);
+    } while(c==EOF);
+    if (i<bufSize) { /* avoid buffer overflow */
+      buf[i] = c;
+    }
+  } while(c!='\n');
+  buf[bufSize-1] = '\0';
+}
+
+static void TestStdOut(void) {
+  printf("Using printf() with SWO\n");
+  putc('*', stdout);
+  putc('#', stderr);
+  putchar('!');
+  putchar('\n');
+}
+
 static void SwoTest(void) {
   unsigned char ch;
   unsigned char buf[64];
   int c;
 
   McuSWO_SendStr((unsigned char*)"Application using SWO\n"); /* stop with the debugger here and configure SWO */
+  TestStdOut();
   for(;;) {
-    putc('*', stdout);
-    putc('#', stderr);
-    putchar('!');
-    printf("Using printf() with SWO\n");
-    McuSWO_SendStr((unsigned char*)"Enter some text and press ENTER:\n");
+    printf("Enter some text and press ENTER:\n");
+    ReadLine_getc(buf, sizeof(buf));
+#if 0
     // scanf("%c", &c);
     do {
       c = getc(stdin);
     } while(c==EOF);
     printf("getc(): '%c'\n", c);
-    buf[0] = '\0'; /* init buffer */
-    for(;;) { /* breaks */
-    	if (McuSWO_StdIOKeyPressed()) {
-    		McuSWO_stdio.stdIn(&ch);
-    		if (ch!='\0') {
-    		  McuUtility_chcat(buf, sizeof(buf), ch);
-    		  if (ch=='\n') {
-    			  break;
-    		  }
-    		}
-    	}
-    }
-	  McuSWO_SendStr((unsigned char*)"Received: ");
-	  McuSWO_SendStr(buf);
-	//printf("character entered:\n");
+#endif
+    McuSWO_SendStr((unsigned char*)"Enter some text and press ENTER:\n");
+	  ReadLine_McuLib(buf, sizeof(buf));
+    McuSWO_SendStr((unsigned char*)"McuLib: ");
+    McuSWO_SendStr(buf);
   }
 }
 
