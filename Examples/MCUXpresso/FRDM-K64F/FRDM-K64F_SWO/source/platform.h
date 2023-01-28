e8192647d532208a69081b3ddb603b46464e6fe9
Erich Styger <erich.styger@hslu.ch>
2023-01-28 20:02:56 +0100

    updated demo program, added McuLib

---
+++ Examples/MCUXpresso/FRDM-K64F/FRDM-K64F_SWO/source/platform.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023, Erich Styger
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+void PL_Init(void);
+
+#endif /* PLATFORM_H_ */
