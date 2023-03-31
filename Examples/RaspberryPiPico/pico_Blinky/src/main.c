919d28f64866987d289bb339c16fe57e266955ed
Erich Styger <erich.styger@hslu.ch>
2023-03-31 10:00:51 +0200

    added blinky project

---
+++ Examples/RaspberryPiPico/pico_Blinky/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023, Erich Styger
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "application.h"
+
+int main(void) {
+  APP_Run();
+  for(;;) {}
+  return 0;
+}
