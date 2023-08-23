608602843fee6283b0adfc0ba4ab501cdb5beedc
Erich Styger <erich.styger@hslu.ch>
2023-08-23 18:31:09 +0200

    streamlined CMake project

--- Examples/RaspberryPiPico/pico_Heidelberg/clean.bat
+++
@@ -1,7 +0,0 @@
-del *.elf *.bin *.dis *.hex *.uf2 *.map
-del CMakeCache.txt Makefile
-rmdir /S /Q build pioasm generated elf2uf2 pico-sdk CMakeFiles cmake_install.cmake
-
-del src\*.a 
-del src\Makefile src\cmake_install.cmake
-rmdir /S /Q src\CMakeFiles
