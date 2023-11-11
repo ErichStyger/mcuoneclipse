2742305ccdc15972606ef791224ff50b30171b4f
Erich Styger <erich.styger@hslu.ch>
2023-11-11 19:03:18 +0100

    using CMake presets

--- Examples/VisualStudioCode/LPC55S16-EVK/LPC55S16_gcov/src/platform.h
+++ Examples/VisualStudioCode/LPC55S16-EVK/LPC55S16_gcov/src/platform.h
@@ -8,8 +8,7 @@
 #define PLATFORM_H_
 
 #define PL_CONFIG_USE_SHELL_UART        (0)
-#define PL_CONFIG_USE_GCOV              (1 && McuRdimon_CONFIG_IS_ENABLED) /* if using gcov */
-
+#define PL_CONFIG_USE_GCOV              (0 && McuRdimon_CONFIG_IS_ENABLED) /* if using gcov */
 
 /*!
  * \brief Module initialization
