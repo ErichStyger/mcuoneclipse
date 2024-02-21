a36cd571e277d19aa9cc4e4cc32e381b0544a4d8
Erich Styger <erich.styger@hslu.ch>
2024-02-21 18:57:58 +0100

    updated version number

--- Examples/McuLib/McuLibVersion.h
+++ Examples/McuLib/McuLibVersion.h
@@ -1,7 +1,7 @@
 /**
  * \file
  * \brief Version information header file for MCU library
- * Copyright (c) 2020-2022, Erich Styger
+ * Copyright (c) 2020-2024, Erich Styger
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -10,15 +10,15 @@
 
 /* See
  * https://github.com/ErichStyger/McuOnEclipseLibrary
- * Date: 29-Dec-2022
- * Time: 08:12
+ * Date: 06-Feb-2024
+ * Time: 14:08
  */
 
 #define McuLib_VERSION_VERSION_MAJOR   (1)
   /*!< McuLib major version number */
 #define McuLib_VERSION_VERSION_MINOR   (0)
   /*!< McuLib minor version number */
-#define McuLib_VERSION_VERSION_BUILD   (9)
+#define McuLib_VERSION_VERSION_BUILD   (10)
   /*!< McuLib build version number */
 
 #endif /* SOURCES_MCULIBVERSION_H_ */
