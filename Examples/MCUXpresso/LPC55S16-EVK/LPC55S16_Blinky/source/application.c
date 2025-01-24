566d1f4b47c5c537022157264ecb5227882586d8
Erich Styger <erich.styger@hslu.ch>
2025-01-24 13:40:13 +0100

    increased stack size

--- Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_Blinky/source/application.c
+++ Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_Blinky/source/application.c
@@ -153,7 +153,7 @@ void APP_Run(void) {
   if (xTaskCreate(
       AppTask,  /* pointer to the task */
       "App", /* task name for kernel awareness debugging */
-      1000/sizeof(StackType_t), /* task stack size */
+      2048/sizeof(StackType_t), /* task stack size */
       (void*)NULL, /* optional task startup argument */
       tskIDLE_PRIORITY+2,  /* initial priority */
       (TaskHandle_t*)NULL /* optional task handle to create */
