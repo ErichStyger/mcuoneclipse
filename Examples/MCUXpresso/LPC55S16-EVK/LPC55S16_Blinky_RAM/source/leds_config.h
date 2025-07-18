c6f9e67fc70437a7ccd0cc905a250851c628446d
Erich Styger <erich.styger@hslu.ch>
2025-07-18 18:15:33 +0200

    added blinky

---
+++ Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_Blinky_RAM/source/leds_config.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021, Erich Styger
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef LEDS_CONFIG_H_
+#define LEDS_CONFIG_H_
+
+/* Red: P1_4 */
+#define LEDS_LED_RED_GPIO       GPIO
+#define LEDS_LED_RED_PORT       1
+#define LEDS_LED_RED_PIN        4
+
+/* Green: P1_7 */
+#define LEDS_LED_GREEN_GPIO     GPIO
+#define LEDS_LED_GREEN_PORT     1
+#define LEDS_LED_GREEN_PIN      7
+
+/* Blue: P1_6 */
+#define LEDS_LED_BLUE_GPIO      GPIO
+#define LEDS_LED_BLUE_PORT      1
+#define LEDS_LED_BLUE_PIN       6
+
+#endif /* LEDS_CONFIG_H_ */
--- Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_Blinky_RAM/source/main.c
+++ Examples/MCUXpresso/LPC55S16-EVK/LPC55S16_Blinky_RAM/source/main.c
@@ -1,48 +1,31 @@
-/*
- * Copyright 2016-2025 NXP
- * All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
 
-/**
- * @file    LPC55S16_Blinky_RAM.c
- * @brief   Application entry point.
- */
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
 
-/* TODO: insert other definitions and declarations here. */
+#include "leds.h"
+#include "McuWait.h"
 
-/*
- * @brief   Application entry point.
- */
 int main(void) {
-
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
+  /* Init board hardware. */
+  BOARD_InitBootPins();
+  BOARD_InitBootClocks();
+  BOARD_InitBootPeripherals();
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
+  /* Init FSL debug console. */
+  BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\r\n");
+  LEDS_Init();
+  for(;;) {
+    LEDS_Neg(LEDS_BLUE);
+    McuWait_Waitms(100);
+  }
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+  while(1) {
+  }
+  return 0;
 }
